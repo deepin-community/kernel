09489ed87715c70e142264b43ac97fbf2666ce42
Huacai Chen <chenhc@lemote.com>
2016-12-01 09:51:35 +0800

    AOSCOS: Staging: sb105x: Fix build and add MIPS support
    [Mingcong Bai: Resolved merge conflicts in arch/mips/include/asm/serial.h
    and drivers/staging/sb105x/Kconfig.]
    
    Signed-off-by: Huacai Chen <chenhc@lemote.com>
    Signed-off-by: Mingcong Bai <jeffbai@aosc.io>
    
    Signed-off-by: Kexy Biscuit <kexybiscuit@aosc.io>

---
+++ arch/mips/include/asm/serial.h
@@ -0,0 +1,19 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+/*
+ * Copyright (C) 2017 MIPS Tech, LLC
+ */
+#ifndef __ASM__SERIAL_H
+#define __ASM__SERIAL_H
+
+#ifdef CONFIG_MIPS_GENERIC
+/*
+ * Generic kernels cannot know a correct value for all platforms at
+ * compile time. Set it to 0 to prevent 8250_early using it
+ */
+#define BASE_BAUD 0
+#else
+#define STD_COM_FLAGS (ASYNC_BOOT_AUTOCONF | ASYNC_SKIP_TEST)
+#include <asm-generic/serial.h>
+#endif
+
+#endif /* __ASM__SERIAL_H */
