7953fa4376f8af49a7b4b302f83c8347c998284d
Xiang Yu <xiangy@lemote.com>
2016-12-01 09:51:35 +0800

    AOSCOS: add rear mic support for CX20631
    CX20631 Node1b's default config value should be 0x01A190F0 according to CX20631
    datesheet, but the true value is 0x018xxxxx, this cause PortC can't work as rear
    mic, so rewrite to default value
    
    Signed-off-by: Icenowy Zheng <uwu@icenowy.me>
    
    Signed-off-by: Xiang Yu <xiangy@lemote.com>
    Signed-off-by: Kexy Biscuit <kexybiscuit@aosc.io>

--- sound/pci/hda/patch_conexant.c
+++ sound/pci/hda/patch_conexant.c
@@ -1298,6 +1298,13 @@ static int patch_conexant_auto(struct hda_codec *codec)
 		snd_hda_pick_fixup(codec, cxt5051_fixup_models,
 				   cxt5051_fixups, cxt_fixups);
 		break;
+	/* CX20631 node1b's default value is not same as datasheet, cause rear mic not work */
+	case 0x14f15097:
+		snd_hda_codec_set_pincfg(codec, 0x1b, 0x01a190f0);
+		codec->pin_amp_workaround = 1;
+		snd_hda_pick_fixup(codec, cxt5066_fixup_models,
+				   cxt5066_fixups, cxt_fixups);
+		break;
 	case 0x14f15098:
 		codec->pin_amp_workaround = 1;
 		spec->gen.mixer_nid = 0x22;
