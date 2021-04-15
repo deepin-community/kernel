6a3f99b69283df474a3dd41e5cf950fb6c32cdda
hanliyang <hanliyang@hygon.cn>
2021-04-15 07:56:55 -0400

    KVM: SVM: Add support for rebooting CSV2 guest
    hygon inclusion
    category: feature
    CVE: NA
    
    ---------------------------
    
    Currently, reboot a CSV2 guest is unsupported because vCPU state is
    encrypted and can't be initialized when guest reboots to execute
    OVMF code.
    
    In order to support reboot a CSV2 guest, make a backup of the
    encrypted VMSA before booting the guest, and restore VMSA from the
    backup before rebooting the guest.
    
    Signed-off-by: hanliyang <hanliyang@hygon.cn>

--- arch/x86/kvm/svm/sev.c
+++ arch/x86/kvm/svm/sev.c
@@ -669,6 +669,17 @@ static int __sev_launch_update_vmsa(struct kvm *kvm, struct kvm_vcpu *vcpu,
 	vcpu->arch.guest_state_protected = true;
 
 	/*
+	 * Backup encrypted vmsa to support rebooting CSV2 guest. The
+	 * clflush_cache_range() is necessary to invalidate prefetched
+	 * memory area pointed by svm->sev_es.vmsa so that we can read
+	 * fresh memory updated by PSP.
+	 */
+	if (is_x86_vendor_hygon()) {
+		clflush_cache_range(svm->sev_es.vmsa, PAGE_SIZE);
+		csv2_sync_reset_vmsa(svm);
+	}
+
+	/*
 	 * SEV-ES guest mandates LBR Virtualization to be _always_ ON. Enable it
 	 * only after setting guest_state_protected because KVM_SET_MSRS allows
 	 * dynamic toggling of LBRV (for performance reason) on write access to
@@ -2442,6 +2453,9 @@ void sev_free_vcpu(struct kvm_vcpu *vcpu)
 
 	if (svm->sev_es.ghcb_sa_free)
 		kvfree(svm->sev_es.ghcb_sa);
+
+	if (is_x86_vendor_hygon())
+		csv2_free_reset_vmsa(svm);
 }
 
 static void dump_ghcb(struct vcpu_svm *svm)
