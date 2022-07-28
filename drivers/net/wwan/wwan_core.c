7fb2af56196c34b5de40554ab77f9b8df99bddba
Daniele Palmas <dnlplm@gmail.com>
2022-07-28 15:54:12 +0200

    wwan: add SAHARA device
    deepin-Intel-SIG: wwan: add SAHARA device.
    
    Signed-off-by: Daniele Palmas <dnlplm@gmail.com>
    Signed-off-by: Wei Qiao <wei.qiao@intel.com>
    Signed-off-by: WangYuli <wangyuli@uniontech.com>
    (cherry picked from commit 051260e8b2b06aaa090553b0adebc006ebc9f69e)
    Link: https://github.com/deepin-community/kernel/pull/64
    Signed-off-by: Wentao Guan <guanwentao@uniontech.com>
    
    Conflicts:
    	drivers/net/wwan/wwan_core.c
    	include/linux/wwan.h

--- drivers/net/wwan/wwan_core.c
+++ drivers/net/wwan/wwan_core.c
@@ -342,6 +342,10 @@ static const struct {
 		.name = "MIPC",
 		.devsuf = "mipc",
 	},
+	[WWAN_PORT_SAHARA] = {
+		.name = "SAHARA",
+		.devsuf = "sahara",
+	},
 };
 
 static ssize_t type_show(struct device *dev, struct device_attribute *attr,
