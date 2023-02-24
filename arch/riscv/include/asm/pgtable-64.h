fc5dc344813dcdab270d1c336d6b266b0c51fc2d
Icenowy Zheng <uwu@icenowy.me>
2023-02-24 17:22:38 +0800

    riscv: changing T-Head PBMT attributes
    Originall the T-Head PBMT implementation in the kernel is intended for
    D1, thus the Sharable bit is not set. In addition, the Bufferable bit
    is not set for writecombine situation.
    
    Set these bits in the T-Head PBMT attributes definition.
    
    Signed-off-by: Icenowy Zheng <uwu@icenowy.me>

--- arch/riscv/include/asm/pgtable-64.h
+++ arch/riscv/include/asm/pgtable-64.h
@@ -127,13 +127,13 @@ enum napot_cont_order {
 /*
  * [63:59] T-Head Memory Type definitions:
  *
- * 00000 - NC   Weakly-ordered, Non-cacheable, Non-bufferable, Non-shareable, Non-trustable
+ * 00010 - NC   Weakly-ordered, Non-cacheable, Non-bufferable, Shareable, Non-trustable
  * 01110 - PMA  Weakly-ordered, Cacheable, Bufferable, Shareable, Non-trustable
- * 10000 - IO   Strongly-ordered, Non-cacheable, Non-bufferable, Non-shareable, Non-trustable
+ * 10010 - IO   Strongly-ordered, Non-cacheable, Non-bufferable, Shareable, Non-trustable
  */
 #define _PAGE_PMA_THEAD		((1UL << 62) | (1UL << 61) | (1UL << 60))
-#define _PAGE_NOCACHE_THEAD	0UL
-#define _PAGE_IO_THEAD		(1UL << 63)
+#define _PAGE_NOCACHE_THEAD	((1UL << 61) | (1UL << 60))
+#define _PAGE_IO_THEAD		((1UL << 63) | (1UL << 60))
 #define _PAGE_MTMASK_THEAD	(_PAGE_PMA_THEAD | _PAGE_IO_THEAD | (1UL << 59))
 
 static inline u64 riscv_page_mtmask(void)
