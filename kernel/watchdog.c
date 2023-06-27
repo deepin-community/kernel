f8b84b17a79e49b4a09eef5b506c91c08994eeae
Joel Granados <j.granados@samsung.com>
2023-06-27 15:30:19 +0200

    kernel misc: Remove the now superfluous sentinel elements from ctl_table array
    mainline inclusion
    from mainline-v6.10-rc1
    category: performance
    
    This commit comes at the tail end of a greater effort to remove the
    empty elements at the end of the ctl_table arrays (sentinels) which
    will reduce the overall build time size of the kernel and run time
    memory bloat by ~64 bytes per sentinel (further information Link :
    https://lore.kernel.org/all/ZO5Yx5JFogGi%2FcBo@bombadil.infradead.org/)
    
    Remove the sentinel from ctl_table arrays. Reduce by one the values used
    to compare the size of the adjusted arrays.
    
    Signed-off-by: Joel Granados <j.granados@samsung.com>
    Conflicts:
    	kernel/ucount.c
    (cherry picked from commit 11a921909fea230cf7afcd6842a9452f3720b61b)
    Signed-off-by: Wentao Guan <guanwentao@uniontech.com>

--- kernel/watchdog.c
+++ kernel/watchdog.c
@@ -949,7 +949,6 @@ static struct ctl_table watchdog_sysctls[] = {
 	},
 #endif /* CONFIG_SMP */
 #endif
-	{}
 };
 
 static struct ctl_table watchdog_hardlockup_sysctl[] = {
@@ -962,7 +961,6 @@ static struct ctl_table watchdog_hardlockup_sysctl[] = {
 		.extra1		= SYSCTL_ZERO,
 		.extra2		= SYSCTL_ONE,
 	},
-	{}
 };
 
 static void __init watchdog_sysctl_init(void)
