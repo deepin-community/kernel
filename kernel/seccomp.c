c9545bb74cbc34e57035cc3f73fcc597bdb9fdc3
Joel Granados <j.granados@samsung.com>
2023-06-27 15:30:23 +0200

    seccomp: Remove the now superfluous sentinel elements from ctl_table array
    mainline inclusion
    from mainline-v6.10-rc1
    category: performance
    
    This commit comes at the tail end of a greater effort to remove the
    empty elements at the end of the ctl_table arrays (sentinels) which
    will reduce the overall build time size of the kernel and run time
    memory bloat by ~64 bytes per sentinel (further information Link :
    https://lore.kernel.org/all/ZO5Yx5JFogGi%2FcBo@bombadil.infradead.org/)
    
    Remove sentinel element from seccomp_sysctl_table.
    
    Acked-by: Kees Cook <keescook@chromium.org>
    Signed-off-by: Joel Granados <j.granados@samsung.com>
    (cherry picked from commit e822582effc6bc00da9b28cf814935a6be070504)
    Signed-off-by: Wentao Guan <guanwentao@uniontech.com>

--- kernel/seccomp.c
+++ kernel/seccomp.c
@@ -2443,7 +2443,6 @@ static struct ctl_table seccomp_sysctl_table[] = {
 		.mode		= 0644,
 		.proc_handler	= seccomp_actions_logged_handler,
 	},
-	{ }
 };
 
 static int __init seccomp_sysctl_init(void)
