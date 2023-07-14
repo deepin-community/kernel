80bd8b99dda273fa517cec56598048c6d9a595cd
Chen Zhenhua <chenzhenhua@phytium.com.cn>
2023-07-14 08:34:17 +0800

    xHCI: add XHCI_RESET_ON_RESUME quirk for Phytium xHCI host
    The resume operation of Phytium Px210 xHCI host would failed
    to restore state. Use the XHCI_RESET_ON_RESUME quirk to skip
    it and reset the controller after resume.
    
    Signed-off-by: Wang Zhimin <wangzhimin1179@phytium.com.cn>
    Signed-off-by: Chen Zhenhua <chenzhenhua@phytium.com.cn>
    Reviewed-by: Chen Baozi <chenbaozi@phytium.com.cn>
    Signed-off-by: Wang Yinfeng <wangyinfeng@phytium.com.cn>

--- drivers/usb/host/xhci-pci.c
+++ drivers/usb/host/xhci-pci.c
@@ -61,6 +61,7 @@
 #define PCI_DEVICE_ID_INTEL_MAPLE_RIDGE_XHCI		0x1138
 #define PCI_DEVICE_ID_INTEL_ALDER_LAKE_PCH_XHCI		0x51ed
 #define PCI_DEVICE_ID_INTEL_ALDER_LAKE_N_PCH_XHCI	0x54ed
+#define PCI_DEVICE_ID_PHYTIUM_XHCI			0xdc27
 
 #define PCI_DEVICE_ID_AMD_RENOIR_XHCI			0x1639
 #define PCI_DEVICE_ID_AMD_PROMONTORYA_4			0x43b9
@@ -472,6 +473,10 @@ static void xhci_pci_quirks(struct device *dev, struct xhci_hcd *xhci)
 		xhci->quirks |= XHCI_ZERO_64B_REGS;
 	}
 	if (pdev->vendor == PCI_VENDOR_ID_VIA)
+		xhci->quirks |= XHCI_RESET_ON_RESUME;
+
+	if (pdev->vendor == PCI_VENDOR_ID_PHYTIUM ||
+	    pdev->device == PCI_DEVICE_ID_PHYTIUM_XHCI)
 		xhci->quirks |= XHCI_RESET_ON_RESUME;
 
 	/* See https://bugzilla.kernel.org/show_bug.cgi?id=79511 */
