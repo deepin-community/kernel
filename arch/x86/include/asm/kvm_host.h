43b192d74bbe08045879b5f3b578fa2e7189ab3a
hanliyang <hanliyang@hygon.cn>
2023-08-08 23:47:22 +0800

    KVM: x86: Introduce control_{pre,post}_system_reset ioctl interfaces
    hygon inclusion
    category: feature
    CVE: NA
    
    ---------------------------
    
    In the upcoming patches, we will support for rebooting CSV2 guests. In
    order to support rebooting CSV2 guest, we will set
    vcpu->arch.guest_state_protected to false, before VMRUN, so that VMM
    can initialize vCPU states and VMSA, and then set
    vcpu->arch.guest_state_protected back to true to bypass unexpected
    behaviour in KVM. Besides, cache flush is necessary during rebooting a
    memory encrypted guest.
    
    Introduce control_{pre,post}_system_reset ioctl interfaces to support
    rebooting memory encrypted guests correctly.
    
    Signed-off-by: hanliyang <hanliyang@hygon.cn>

--- arch/x86/include/asm/kvm_host.h
+++ arch/x86/include/asm/kvm_host.h
@@ -1753,9 +1753,11 @@ struct kvm_x86_ops {
 	unsigned long (*vcpu_get_apicv_inhibit_reasons)(struct kvm_vcpu *vcpu);
 
 	/*
-	 * Attestation interface for HYGON CSV guest
+	 * Interfaces for HYGON CSV guest
 	 */
 	int (*vm_attestation)(struct kvm *kvm, unsigned long gpa, unsigned long len);
+	int (*control_pre_system_reset)(struct kvm *kvm);
+	int (*control_post_system_reset)(struct kvm *kvm);
 };
 
 struct kvm_x86_nested_ops {
