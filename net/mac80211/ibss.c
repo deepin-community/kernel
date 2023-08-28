8b60a1742027971f030ab9c86d1b37c6036a7767
Johannes Berg <johannes.berg@intel.com>
2023-08-28 14:00:06 +0200

    wifi: mac80211: reduce iflist_mtx
    We now hold the wiphy mutex everywhere that we use or
    needed the iflist_mtx, so we don't need this mutex any
    more in mac80211. However, drivers may also iterate,
    and in some cases (e.g. mt76) do so from high-priority
    contexts. Thus, keep the mutex around but remove its
    usage in mac80211 apart from those driver-visible parts
    that are still needed.
    
    Most of this change was done automatically with spatch,
    with the parts that are still needed as described above
    reverted manually.
    
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>

--- net/mac80211/ibss.c
+++ net/mac80211/ibss.c
@@ -1744,7 +1744,8 @@ void ieee80211_ibss_notify_scan_completed(struct ieee80211_local *local)
 {
 	struct ieee80211_sub_if_data *sdata;
 
-	mutex_lock(&local->iflist_mtx);
+	lockdep_assert_wiphy(local->hw.wiphy);
+
 	list_for_each_entry(sdata, &local->interfaces, list) {
 		if (!ieee80211_sdata_running(sdata))
 			continue;
@@ -1752,7 +1753,6 @@ void ieee80211_ibss_notify_scan_completed(struct ieee80211_local *local)
 			continue;
 		sdata->u.ibss.last_scan_completed = jiffies;
 	}
-	mutex_unlock(&local->iflist_mtx);
 }
 
 int ieee80211_ibss_join(struct ieee80211_sub_if_data *sdata,
