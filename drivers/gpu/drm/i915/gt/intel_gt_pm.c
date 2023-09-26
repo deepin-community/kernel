1d731747d6037a0dfdeab59a19bf9a3f54cd016a
Nirmoy Das <nirmoy.das@intel.com>
2023-09-26 10:37:41 +0200

    drm/i915: Toggle binder context ready status
    commit a2ae29629230588d50dfcba306decad7e4f690f3 ("drm/i915: Toggle binder context ready status") upstream.
    
    Toggle binder context ready status when needed.
    
    To issue gpu commands, the driver must be primed to receive
    requests. Maintain binder-based GGTT update disablement until driver
    probing completes. Moreover, implement a temporary disablement
    of blitter prior to entering suspend, followed by re-enablement
    post-resume. This is acceptable as those transition periods are
    mostly single threaded.
    
    v2: move changes to lower levels from i915_driver.c(Jani).
        use new function for setting context ready status.
    
    deepin-Intel-SIG: commit a2ae29629230 ("drm/i915: Toggle binder context ready status").
    
    Signed-off-by: Nirmoy Das <nirmoy.das@intel.com>
    Signed-off-by: Oak Zeng <oak.zeng@intel.com>
    Reviewed-by: Andi Shyti <andi.shyti@linux.intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20230926083742.14740-7-nirmoy.das@intel.com
    [ Junxiao Chang: amend commit log ]
    Signed-off-by: Junxiao Chang <junxiao.chang@intel.com>
    Signed-off-by: WangYuli <wangyuli@uniontech.com>

--- drivers/gpu/drm/i915/gt/intel_gt_pm.c
+++ drivers/gpu/drm/i915/gt/intel_gt_pm.c
@@ -280,6 +280,7 @@ int intel_gt_resume(struct intel_gt *gt)
 out_fw:
 	intel_uncore_forcewake_put(gt->uncore, FORCEWAKE_ALL);
 	intel_gt_pm_put(gt);
+	intel_gt_bind_context_set_ready(gt);
 	return err;
 
 err_wedged:
@@ -306,6 +307,7 @@ static void wait_for_suspend(struct intel_gt *gt)
 
 void intel_gt_suspend_prepare(struct intel_gt *gt)
 {
+	intel_gt_bind_context_set_unready(gt);
 	user_forcewake(gt, true);
 	wait_for_suspend(gt);
 }
@@ -359,6 +361,7 @@ void intel_gt_suspend_late(struct intel_gt *gt)
 
 void intel_gt_runtime_suspend(struct intel_gt *gt)
 {
+	intel_gt_bind_context_set_unready(gt);
 	intel_uc_runtime_suspend(&gt->uc);
 
 	GT_TRACE(gt, "\n");
@@ -376,6 +379,7 @@ int intel_gt_runtime_resume(struct intel_gt *gt)
 	if (ret)
 		return ret;
 
+	intel_gt_bind_context_set_ready(gt);
 	return 0;
 }
 
