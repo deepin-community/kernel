80698e3b0566f59a07447eb3564642b7a9af1418
Dr. Chang Liu, PhD <cl91tp@gmail.com>
2023-09-29 13:06:36 +0800

    gsgpu: Remove unused functions

--- drivers/gpu/drm/gsgpu/include/gsgpu_dc_hdmi.h
+++ drivers/gpu/drm/gsgpu/include/gsgpu_dc_hdmi.h
@@ -4,7 +4,6 @@
 int gsgpu_hdmi_init(struct gsgpu_device *adev);
 void gsgpu_hdmi_suspend(struct gsgpu_device *adev);
 int gsgpu_hdmi_resume(struct gsgpu_device *adev);
-void dc_hdmi_encoder_enable(struct drm_encoder *encoder);
 void dc_hdmi_encoder_mode_set(struct drm_encoder *encoder,
 			      struct drm_display_mode *mode,
 			      struct drm_display_mode *adjusted_mode);
