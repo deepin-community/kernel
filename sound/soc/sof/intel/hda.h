9acc52d9bfde0d745c64b73e914129b3ff90f8a7
Maarten Lankhorst <maarten.lankhorst@linux.intel.com>
2023-10-09 13:54:36 +0200

    BACKPORT: ASoC: SOF: Intel: Move binding to display driver outside of deferred probe
    Now that we can use -EPROBE_DEFER, it's no longer required to spin off
    the snd_hdac_i915_init into a workqueue.
    
    Use the -EPROBE_DEFER mechanism instead, which must be returned in the
    probe function.
    
    The previously added probe_early can be used for this,
    and we also use the newly added remove_late for unbinding afterwards.
    
    Signed-off-by: Maarten Lankhorst <maarten.lankhorst@linux.intel.com>
    Cc: Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>
    Link: https://lore.kernel.org/r/20231009115437.99976-13-maarten.lankhorst@linux.intel.com
    Signed-off-by: Takashi Iwai <tiwai@suse.de>
    (cherry picked from commit 3d1a0558136150448c91f9dd915dec843de8eacf)
    
    Conflicts:
    	sound/soc/sof/intel/hda.h
    (Changes related to this file is present in merge commit
    "39cd06e3f7b75b629f2987aa51ab26fb820d167b hence changes are made locally.)
    
    BUG=b:326869955
    TEST=Test Audio use cases.
    
    Change-Id: I9181066767e5c239308892d82a2828d2f51634cd
    Signed-off-by: Debi sahoo <debix.prasad.sahoo@intel.com>
    Signed-off-by: Hubert Mazur <hmazur@google.com>
    (cherry picked from commit f4cd343129caabaa648f8a5d8c0c24b8706fb8f4)
    [(BACKPORT of upstream 3d1a055813615) left a 'return 0;'
    at the end of hda_dsp_remove(), which already returns void in this
    tree (e4d09de3919bb equivalent). Fix the build.]
    Signed-off-by: Wentao Guan <guanwentao@uniontech.com>

--- sound/soc/sof/intel/hda.h
+++ sound/soc/sof/intel/hda.h
@@ -583,6 +583,7 @@ bool hda_is_chain_dma_supported(struct snd_sof_dev *sdev, u32 dai_type);
 int hda_dsp_probe_early(struct snd_sof_dev *sdev);
 int hda_dsp_probe(struct snd_sof_dev *sdev);
 void hda_dsp_remove(struct snd_sof_dev *sdev);
+int hda_dsp_remove_late(struct snd_sof_dev *sdev);
 int hda_dsp_core_power_up(struct snd_sof_dev *sdev, unsigned int core_mask);
 int hda_dsp_core_run(struct snd_sof_dev *sdev, unsigned int core_mask);
 int hda_dsp_enable_core(struct snd_sof_dev *sdev, unsigned int core_mask);
