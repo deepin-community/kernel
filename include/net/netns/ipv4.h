02c59003a5683fb806f911b80be4bdc117950970
Haiyang Zhang <haiyangz@microsoft.com>
2023-10-11 13:30:44 -0700

    tcp: Set pingpong threshold via sysctl
    [ Upstream commit 562b1fdf061bff9394ccd884456ed1173c224fdc ]
    
    TCP pingpong threshold is 1 by default. But some applications, like SQL DB
    may prefer a higher pingpong threshold to activate delayed acks in quick
    ack mode for better performance.
    
    The pingpong threshold and related code were changed to 3 in the year
    2019 in:
      commit 4a41f453bedf ("tcp: change pingpong threshold to 3")
    And reverted to 1 in the year 2022 in:
      commit 4d8f24eeedc5 ("Revert "tcp: change pingpong threshold to 3"")
    
    There is no single value that fits all applications.
    Add net.ipv4.tcp_pingpong_thresh sysctl tunable, so it can be tuned for
    optimal performance based on the application needs.
    
    Signed-off-by: Haiyang Zhang <haiyangz@microsoft.com>
    Reviewed-by: Simon Horman <horms@kernel.org>
    Reviewed-by: Eric Dumazet <edumazet@google.com>
    Acked-by: Neal Cardwell <ncardwell@google.com>
    Reviewed-by: Kuniyuki Iwashima <kuniyu@amazon.com>
    Link: https://lore.kernel.org/r/1697056244-21888-1-git-send-email-haiyangz@microsoft.com
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    Stable-dep-of: 87b08913a9ae ("inet: move icmp_global_{credit,stamp} to a separate cache line")
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    [ Conflict: remove // reserved  in our ("netns-ipv4: reorganize netns_ipv4 fast path variables") ]
    (cherry picked from commit 4ec8a98b3dc3cdb0344bcda12847597a141d61d6)
    Signed-off-by: Wentao Guan <guanwentao@uniontech.com>
    
    Conflicts:
    	include/net/netns/ipv4.h

--- include/net/netns/ipv4.h
+++ include/net/netns/ipv4.h
@@ -165,7 +165,7 @@ struct netns_ipv4 {
 	u8 sysctl_tcp_migrate_req;
 	u8 sysctl_tcp_comp_sack_nr;
 	u8 sysctl_tcp_backlog_ack_defer;
-	u8 sysctl_tcp_pingpong_thresh; // reserved
+	u8 sysctl_tcp_pingpong_thresh;
 
 	u8 sysctl_tcp_retries1;
 	u8 sysctl_tcp_retries2;
