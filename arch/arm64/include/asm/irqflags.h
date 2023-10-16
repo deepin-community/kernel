f21eef9cfe86be322b3a9410532fbe0fd74f9ddd
Mark Rutland <mark.rutland@arm.com>
2023-10-16 11:24:43 +0100

    arm64: Avoid cpus_have_const_cap() for ARM64_HAS_GIC_PRIO_MASKING
    mainline inclusion
    from mainline-v6.7-rc1
    category: feature
    
    In system_uses_irq_prio_masking() we use cpus_have_const_cap() to check
    for ARM64_HAS_GIC_PRIO_MASKING, but this is not necessary and
    alternative_has_cap_*() would be preferable.
    
    For historical reasons, cpus_have_const_cap() is more complicated than
    it needs to be. Before cpucaps are finalized, it will perform a bitmap
    test of the system_cpucaps bitmap, and once cpucaps are finalized it
    will use an alternative branch. This used to be necessary to handle some
    race conditions in the window between cpucap detection and the
    subsequent patching of alternatives and static branches, where different
    branches could be out-of-sync with one another (or w.r.t. alternative
    sequences). Now that we use alternative branches instead of static
    branches, these are all patched atomically w.r.t. one another, and there
    are only a handful of cases that need special care in the window between
    cpucap detection and alternative patching.
    
    Due to the above, it would be nice to remove cpus_have_const_cap(), and
    migrate callers over to alternative_has_cap_*(), cpus_have_final_cap(),
    or cpus_have_cap() depending on when their requirements. This will
    remove redundant instructions and improve code generation, and will make
    it easier to determine how each callsite will behave before, during, and
    after alternative patching.
    
    When CONFIG_ARM64_PSEUDO_NMI=y the ARM64_HAS_GIC_PRIO_MASKING cpucap is
    a strict boot cpu feature which is detected and patched early on the
    boot cpu, which both happen in smp_prepare_boot_cpu(). In the window
    between the ARM64_HAS_GIC_PRIO_MASKING cpucap is detected and
    alternatives are patched we don't run any code that depends upon the
    ARM64_HAS_GIC_PRIO_MASKING cpucap:
    
    * We leave DAIF.IF set until after boot alternatives are patched, and
      interrupts are unmasked later in init_IRQ(), so we cannot reach
      IRQ/FIQ entry code and will not use irqs_priority_unmasked().
    
    * We don't call any code which uses arm_cpuidle_save_irq_context() and
      arm_cpuidle_restore_irq_context() during this window.
    
    * We don't call start_thread_common() during this window.
    
    * The local_irq_*() code in <asm/irqflags.h> depends solely on an
      alternative branch since commit:
    
      a5f61cc636f48bdf ("arm64: irqflags: use alternative branches for pseudo-NMI logic")
    
      ... and hence will use the default (DAIF-only) masking behaviour until
      alternatives are patched.
    
    * Secondary CPUs are brought up later after alternatives are patched,
      and alternatives are patched on the boot CPU immediately prior to
      calling init_gic_priority_masking(), so we'll correctly initialize
      interrupt masking regardless.
    
    This patch replaces the use of cpus_have_const_cap() with
    alternative_has_cap_unlikely(), which avoid generating code to test the
    system_cpucaps bitmap and should be better for all subsequent calls at
    runtime. As this makes system_uses_irq_prio_masking() equivalent to
    __irqflags_uses_pmr(), the latter is removed and replaced with the
    former for consistency.
    
    Signed-off-by: Mark Rutland <mark.rutland@arm.com>
    Cc: Ard Biesheuvel <ardb@kernel.org>
    Cc: Mark Brown <broonie@kernel.org>
    Cc: Suzuki K Poulose <suzuki.poulose@arm.com>
    Cc: Will Deacon <will@kernel.org>
    Signed-off-by: Catalin Marinas <catalin.marinas@arm.com>
    (cherry picked from commit 20af807d806d13f4ab7795943587cf41257c649a)
    Signed-off-by: Wentao Guan <guanwentao@uniontech.com>

--- arch/arm64/include/asm/irqflags.h
+++ arch/arm64/include/asm/irqflags.h
@@ -31,12 +31,6 @@
  * so.
  */
 
-static __always_inline bool __irqflags_uses_pmr(void)
-{
-	return IS_ENABLED(CONFIG_ARM64_PSEUDO_NMI) &&
-	       alternative_has_cap_unlikely(ARM64_HAS_GIC_PRIO_MASKING);
-}
-
 static __always_inline void __daif_local_irq_enable(void)
 {
 	barrier();
@@ -59,7 +53,7 @@ static __always_inline void __pmr_local_irq_enable(void)
 
 static inline void arch_local_irq_enable(void)
 {
-	if (__irqflags_uses_pmr()) {
+	if (system_uses_irq_prio_masking()) {
 		__pmr_local_irq_enable();
 	} else {
 		__daif_local_irq_enable();
@@ -87,7 +81,7 @@ static __always_inline void __pmr_local_irq_disable(void)
 
 static inline void arch_local_irq_disable(void)
 {
-	if (__irqflags_uses_pmr()) {
+	if (system_uses_irq_prio_masking()) {
 		__pmr_local_irq_disable();
 	} else {
 		__daif_local_irq_disable();
@@ -109,7 +103,7 @@ static __always_inline unsigned long __pmr_local_save_flags(void)
  */
 static inline unsigned long arch_local_save_flags(void)
 {
-	if (__irqflags_uses_pmr()) {
+	if (system_uses_irq_prio_masking()) {
 		return __pmr_local_save_flags();
 	} else {
 		return __daif_local_save_flags();
@@ -128,7 +122,7 @@ static __always_inline bool __pmr_irqs_disabled_flags(unsigned long flags)
 
 static inline bool arch_irqs_disabled_flags(unsigned long flags)
 {
-	if (__irqflags_uses_pmr()) {
+	if (system_uses_irq_prio_masking()) {
 		return __pmr_irqs_disabled_flags(flags);
 	} else {
 		return __daif_irqs_disabled_flags(flags);
@@ -147,7 +141,7 @@ static __always_inline bool __pmr_irqs_disabled(void)
 
 static inline bool arch_irqs_disabled(void)
 {
-	if (__irqflags_uses_pmr()) {
+	if (system_uses_irq_prio_masking()) {
 		return __pmr_irqs_disabled();
 	} else {
 		return __daif_irqs_disabled();
@@ -179,7 +173,7 @@ static __always_inline unsigned long __pmr_local_irq_save(void)
 
 static inline unsigned long arch_local_irq_save(void)
 {
-	if (__irqflags_uses_pmr()) {
+	if (system_uses_irq_prio_masking()) {
 		return __pmr_local_irq_save();
 	} else {
 		return __daif_local_irq_save();
@@ -206,7 +200,7 @@ static __always_inline void __pmr_local_irq_restore(unsigned long flags)
  */
 static inline void arch_local_irq_restore(unsigned long flags)
 {
-	if (__irqflags_uses_pmr()) {
+	if (system_uses_irq_prio_masking()) {
 		__pmr_local_irq_restore(flags);
 	} else {
 		__daif_local_irq_restore(flags);
