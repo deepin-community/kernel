79a5c19c7f9adcb25fa549b43b0d5f08bca4191e
Mark Rutland <mark.rutland@arm.com>
2023-10-16 11:24:30 +0100

    arm64: Fixup user features at boot time
    mainline inclusion
    from mainline-v6.7-rc1
    category: feature
    
    For ARM64_WORKAROUND_2658417, we use a cpu_enable() callback to hide the
    ID_AA64ISAR1_EL1.BF16 ID register field. This is a little awkward as
    CPUs may attempt to apply the workaround concurrently, requiring that we
    protect the bulk of the callback with a raw_spinlock, and requiring some
    pointless work every time a CPU is subsequently hotplugged in.
    
    This patch makes this a little simpler by handling the masking once at
    boot time. A new user_feature_fixup() function is called at the start of
    setup_user_features() to mask the feature, matching the style of
    elf_hwcap_fixup(). The ARM64_WORKAROUND_2658417 cpucap is added to
    cpucap_is_possible() so that code can be elided entirely when this is
    not possible.
    
    Note that the ARM64_WORKAROUND_2658417 capability is matched with
    ERRATA_MIDR_RANGE(), which implicitly gives the capability a
    ARM64_CPUCAP_LOCAL_CPU_ERRATUM type, which forbids the late onlining of
    a CPU with the erratum if the erratum was not present at boot time.
    Therefore this patch doesn't change the behaviour for late onlining.
    
    Signed-off-by: Mark Rutland <mark.rutland@arm.com>
    Cc: James Morse <james.morse@arm.com>
    Cc: Mark Brown <broonie@kernel.org>
    Cc: Suzuki K Poulose <suzuki.poulose@arm.com>
    Cc: Will Deacon <will@kernel.org>
    Signed-off-by: Catalin Marinas <catalin.marinas@arm.com>
    [conflict because of 2721be39ec17df42450dabf3c1a1dc573524f303 merged before]
    Conflicts:
    	arch/arm64/kernel/cpufeature.c
    (cherry picked from commit 7f632d331d4706cdbfcaa217b98e1df9b5b5719b)
    Signed-off-by: Wentao Guan <guanwentao@uniontech.com>

--- arch/arm64/kernel/cpufeature.c
+++ arch/arm64/kernel/cpufeature.c
@@ -2328,6 +2328,14 @@ static void cpu_enable_mte(struct arm64_cpu_capabilities const *cap)
 
 static void user_feature_fixup(void)
 {
+	if (cpus_have_cap(ARM64_WORKAROUND_2658417)) {
+		struct arm64_ftr_reg *regp;
+
+		regp = get_arm64_ftr_reg(SYS_ID_AA64ISAR1_EL1);
+		if (regp)
+			regp->user_mask &= ~ID_AA64ISAR1_EL1_BF16_MASK;
+	}
+
 	if (cpus_have_cap(ARM64_WORKAROUND_SPECULATIVE_SSBS)) {
 		struct arm64_ftr_reg *regp;
 
@@ -3599,6 +3607,7 @@ void __init setup_system_features(void)
 void __init setup_user_features(void)
 {
 	user_feature_fixup();
+
 	setup_elf_hwcaps(arm64_elf_hwcaps);
 
 	if (system_supports_32bit_el0()) {
