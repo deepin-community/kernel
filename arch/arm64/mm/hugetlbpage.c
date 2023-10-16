2d80d2a87bc49e7e6acf1f8c10cb0dbe048a5a1a
Mark Rutland <mark.rutland@arm.com>
2023-10-16 11:24:57 +0100

    arm64: Avoid cpus_have_const_cap() for ARM64_WORKAROUND_2645198
    mainline inclusion
    from mainline-v6.7-rc1
    category: feature
    
    We use cpus_have_const_cap() to check for ARM64_WORKAROUND_2645198 but
    this is not necessary and alternative_has_cap() would be preferable.
    
    For historical reasons, cpus_have_const_cap() is more complicated than
    it needs to be. Before cpucaps are finalized, it will perform a bitmap
    test of the system_cpucaps bitmap, and once cpucaps are finalized it
    will use an alternative branch. This used to be necessary to handle some
    race conditions in the window between cpucap detection and the
    subsequent patching of alternatives and static branches, where different
    branches could be out-of-sync with one another (or w.r.t. alternative
    sequences). Now that we use alternative branches instead of static
    branches, these are all patched atomically w.r.t. one another, and there
    are only a handful of cases that need special care in the window between
    cpucap detection and alternative patching.
    
    Due to the above, it would be nice to remove cpus_have_const_cap(), and
    migrate callers over to alternative_has_cap_*(), cpus_have_final_cap(),
    or cpus_have_cap() depending on when their requirements. This will
    remove redundant instructions and improve code generation, and will make
    it easier to determine how each callsite will behave before, during, and
    after alternative patching.
    
    The ARM64_WORKAROUND_2645198 cpucap is detected and patched before any
    userspace translation table exist, and the workaround is only necessary
    when manipulating usrspace translation tables which are in use. Thus it
    is not necessary to use cpus_have_const_cap(), and alternative_has_cap()
    is equivalent.
    
    This patch replaces the use of cpus_have_const_cap() with
    alternative_has_cap_unlikely(), which will avoid generating code to test
    the system_cpucaps bitmap and should be better for all subsequent calls
    at runtime.  The ARM64_WORKAROUND_2645198 cpucap is added to
    cpucap_is_possible() so that code can be elided entirely when this is
    not possible, and redundant IS_ENABLED() checks are removed.
    
    Signed-off-by: Mark Rutland <mark.rutland@arm.com>
    Cc: Suzuki K Poulose <suzuki.poulose@arm.com>
    Cc: Will Deacon <will@kernel.org>
    Signed-off-by: Catalin Marinas <catalin.marinas@arm.com>
    Conflicts:
    	arch/arm64/mm/hugetlbpage.c
    (cherry picked from commit 412cb3801dfac4703032f438fa538e850cc14b6d)
    Signed-off-by: Wentao Guan <guanwentao@uniontech.com>

--- arch/arm64/mm/hugetlbpage.c
+++ arch/arm64/mm/hugetlbpage.c
@@ -545,8 +545,7 @@ pte_t huge_ptep_modify_prot_start(struct vm_area_struct *vma, unsigned long addr
 {
 	unsigned long psize = huge_page_size(hstate_vma(vma));
 
-	if (IS_ENABLED(CONFIG_ARM64_ERRATUM_2645198) &&
-	    cpus_have_const_cap(ARM64_WORKAROUND_2645198)) {
+	if (alternative_has_cap_unlikely(ARM64_WORKAROUND_2645198)) {
 		/*
 		 * Break-before-make (BBM) is required for all user space mappings
 		 * when the permission changes from executable to non-executable
