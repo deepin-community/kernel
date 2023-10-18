7f037b2f2e07d88dcbc0eb975b73e7c4b98a4130
Chuyi Zhou <zhouchuyi@bytedance.com>
2023-10-18 14:17:39 +0800

    cgroup: Prepare for using css_task_iter_*() in BPF
    commit 6da88306811b40a207c94c9da9faf07bdb20776e upstream.
    
    This patch makes some preparations for using css_task_iter_*() in BPF
    Program.
    
    1. Flags CSS_TASK_ITER_* are #define-s and it's not easy for bpf prog to
    use them. Convert them to enum so bpf prog can take them from vmlinux.h.
    
    2. In the next patch we will add css_task_iter_*() in common kfuncs which
    is not safe. Since css_task_iter_*() does spin_unlock_irq() which might
    screw up irq flags depending on the context where bpf prog is running.
    So we should use irqsave/irqrestore here and the switching is harmless.
    
    Suggested-by: Alexei Starovoitov <ast@kernel.org>
    Signed-off-by: Chuyi Zhou <zhouchuyi@bytedance.com>
    Acked-by: Tejun Heo <tj@kernel.org>
    Link: https://lore.kernel.org/r/20231018061746.111364-2-zhouchuyi@bytedance.com
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Signed-off-by: Xuchun Shang <xuchun.shang@linux.alibaba.com>

--- kernel/cgroup/cgroup.c
+++ kernel/cgroup/cgroup.c
@@ -4914,9 +4914,11 @@ repeat:
 void css_task_iter_start(struct cgroup_subsys_state *css, unsigned int flags,
 			 struct css_task_iter *it)
 {
+	unsigned long irqflags;
+
 	memset(it, 0, sizeof(*it));
 
-	spin_lock_irq(&css_set_lock);
+	spin_lock_irqsave(&css_set_lock, irqflags);
 
 	it->ss = css->ss;
 	it->flags = flags;
@@ -4930,7 +4932,7 @@ void css_task_iter_start(struct cgroup_subsys_state *css, unsigned int flags,
 
 	css_task_iter_advance(it);
 
-	spin_unlock_irq(&css_set_lock);
+	spin_unlock_irqrestore(&css_set_lock, irqflags);
 }
 
 /**
@@ -4943,12 +4945,14 @@ void css_task_iter_start(struct cgroup_subsys_state *css, unsigned int flags,
  */
 struct task_struct *css_task_iter_next(struct css_task_iter *it)
 {
+	unsigned long irqflags;
+
 	if (it->cur_task) {
 		put_task_struct(it->cur_task);
 		it->cur_task = NULL;
 	}
 
-	spin_lock_irq(&css_set_lock);
+	spin_lock_irqsave(&css_set_lock, irqflags);
 
 	/* @it may be half-advanced by skips, finish advancing */
 	if (it->flags & CSS_TASK_ITER_SKIPPED)
@@ -4961,7 +4965,7 @@ struct task_struct *css_task_iter_next(struct css_task_iter *it)
 		css_task_iter_advance(it);
 	}
 
-	spin_unlock_irq(&css_set_lock);
+	spin_unlock_irqrestore(&css_set_lock, irqflags);
 
 	return it->cur_task;
 }
@@ -4974,11 +4978,13 @@ struct task_struct *css_task_iter_next(struct css_task_iter *it)
  */
 void css_task_iter_end(struct css_task_iter *it)
 {
+	unsigned long irqflags;
+
 	if (it->cur_cset) {
-		spin_lock_irq(&css_set_lock);
+		spin_lock_irqsave(&css_set_lock, irqflags);
 		list_del(&it->iters_node);
 		put_css_set_locked(it->cur_cset);
-		spin_unlock_irq(&css_set_lock);
+		spin_unlock_irqrestore(&css_set_lock, irqflags);
 	}
 
 	if (it->cur_dcset)
