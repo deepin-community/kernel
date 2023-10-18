cc4ca213a8002004d70cc6c0f8dfc081bc7e9c32
Sean Christopherson <seanjc@google.com>
2023-10-18 13:46:22 -0700

    KVM: Set file_operations.owner appropriately for all such structures
    mainline inclusion
    from mainline-v6.7
    category: bugfix
    
    Set .owner for all KVM-owned filed types so that the KVM module is pinned
    until any files with callbacks back into KVM are completely freed.  Using
    "struct kvm" as a proxy for the module, i.e. keeping KVM-the-module alive
    while there are active VMs, doesn't provide full protection.
    
    Userspace can invoke delete_module() the instant the last reference to KVM
    is put.  If KVM itself puts the last reference, e.g. via kvm_destroy_vm(),
    then it's possible for KVM to be preempted and deleted/unloaded before KVM
    fully exits, e.g. when the task running kvm_destroy_vm() is scheduled back
    in, it will jump to a code page that is no longer mapped.
    
    Note, file types that can call into sub-module code, e.g. kvm-intel.ko or
    kvm-amd.ko on x86, must use the module pointer passed to kvm_init(), not
    THIS_MODULE (which points at kvm.ko).  KVM assumes that if /dev/kvm is
    reachable, e.g. VMs are active, then the vendor module is loaded.
    
    To reduce the probability of forgetting to set .owner entirely, use
    THIS_MODULE for stats files where KVM does not call back into vendor code.
    
    This reverts commit 70375c2d8fa3fb9b0b59207a9c5df1e2e1205c10, and fixes
    several other file types that have been buggy since their introduction.
    
    Fixes: 70375c2d8fa3 ("Revert "KVM: set owner of cpu and vm file operations"")
    Fixes: 3bcd0662d66f ("KVM: X86: Introduce mmu_rmaps_stat per-vm debugfs file")
    Reported-by: Al Viro <viro@zeniv.linux.org.uk>
    Link: https://lore.kernel.org/all/20231010003746.GN800259@ZenIV
    Link: https://lore.kernel.org/r/20231018204624.1905300-2-seanjc@google.com
    Signed-off-by: Sean Christopherson <seanjc@google.com>
    (cherry picked from commit 087e15206d6ac0d46734e2b0ab34370c0fdca481)
    Signed-off-by: Wentao Guan <guanwentao@uniontech.com>

--- virt/kvm/kvm_main.c
+++ virt/kvm/kvm_main.c
@@ -4210,7 +4210,7 @@ static int kvm_vcpu_release(struct inode *inode, struct file *filp)
 	return 0;
 }
 
-static const struct file_operations kvm_vcpu_fops = {
+static struct file_operations kvm_vcpu_fops = {
 	.release        = kvm_vcpu_release,
 	.unlocked_ioctl = kvm_vcpu_ioctl,
 	.mmap           = kvm_vcpu_mmap,
@@ -4404,6 +4404,7 @@ static int kvm_vcpu_stats_release(struct inode *inode, struct file *file)
 }
 
 static const struct file_operations kvm_vcpu_stats_fops = {
+	.owner = THIS_MODULE,
 	.read = kvm_vcpu_stats_read,
 	.release = kvm_vcpu_stats_release,
 	.llseek = noop_llseek,
@@ -4760,7 +4761,7 @@ static int kvm_device_release(struct inode *inode, struct file *filp)
 	return 0;
 }
 
-static const struct file_operations kvm_device_fops = {
+static struct file_operations kvm_device_fops = {
 	.unlocked_ioctl = kvm_device_ioctl,
 	.release = kvm_device_release,
 	KVM_COMPAT(kvm_device_ioctl),
@@ -5099,6 +5100,7 @@ static int kvm_vm_stats_release(struct inode *inode, struct file *file)
 }
 
 static const struct file_operations kvm_vm_stats_fops = {
+	.owner = THIS_MODULE,
 	.read = kvm_vm_stats_read,
 	.release = kvm_vm_stats_release,
 	.llseek = noop_llseek,
@@ -5456,7 +5458,7 @@ static long kvm_vm_compat_ioctl(struct file *filp,
 }
 #endif
 
-static const struct file_operations kvm_vm_fops = {
+static struct file_operations kvm_vm_fops = {
 	.release        = kvm_vm_release,
 	.unlocked_ioctl = kvm_vm_ioctl,
 	.llseek		= noop_llseek,
@@ -6604,6 +6606,9 @@ int kvm_init(unsigned vcpu_size, unsigned vcpu_align, struct module *module)
 		goto err_async_pf;
 
 	kvm_chardev_ops.owner = module;
+	kvm_vm_fops.owner = module;
+	kvm_vcpu_fops.owner = module;
+	kvm_device_fops.owner = module;
 
 	kvm_preempt_ops.sched_in = kvm_sched_in;
 	kvm_preempt_ops.sched_out = kvm_sched_out;
