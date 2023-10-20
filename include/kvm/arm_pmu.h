6e51965f33688a0563ae6d9589a6af4dc9f3e7bc
Reiji Watanabe <reijiw@google.com>
2023-10-20 21:40:42 +0000

    KVM: arm64: Select default PMU in KVM_ARM_VCPU_INIT handler
    mainline inclusion
    from mainline-v6.7-rc1
    commit 427733579744ef22ee6d0da9907560d79d937458
    category: bugfix
    bugzilla: https://gitee.com/openeuler/kernel/issues/ICWPIF
    Reference: https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit/?id=427733579744ef22ee6d0da9907560d79d937458
    
    --------------------------------
    
    Future changes to KVM's sysreg emulation will rely on having a valid PMU
    instance to determine the number of implemented counters (PMCR_EL0.N).
    This is earlier than when userspace is expected to modify the vPMU
    device attributes, where the default is selected today.
    
    Select the default PMU when handling KVM_ARM_VCPU_INIT such that it is
    available in time for sysreg emulation.
    
    Reviewed-by: Sebastian Ott <sebott@redhat.com>
    Co-developed-by: Marc Zyngier <maz@kernel.org>
    Signed-off-by: Marc Zyngier <maz@kernel.org>
    Signed-off-by: Reiji Watanabe <reijiw@google.com>
    Signed-off-by: Raghavendra Rao Ananta <rananta@google.com>
    Link: https://lore.kernel.org/r/20231020214053.2144305-3-rananta@google.com
    [Oliver: rewrite changelog]
    Signed-off-by: Oliver Upton <oliver.upton@linux.dev>
    Signed-off-by: Xu Raoqing <xuraoqing@huawei.com>
    Signed-off-by: WangYuli <wangyl5933@chinaunicom.cn>

--- include/kvm/arm_pmu.h
+++ include/kvm/arm_pmu.h
@@ -103,6 +103,7 @@ void kvm_vcpu_pmu_resync_el0(void);
 })
 
 u8 kvm_arm_pmu_get_pmuver_limit(void);
+int kvm_arm_set_default_pmu(struct kvm *kvm);
 u8 kvm_arm_pmu_get_max_counters(struct kvm *kvm);
 
 u64 kvm_vcpu_read_pmcr(struct kvm_vcpu *vcpu);
@@ -179,6 +180,10 @@ static inline u8 kvm_arm_pmu_get_pmuver_limit(void)
 	return 0;
 }
 static inline void kvm_vcpu_pmu_resync_el0(void) {}
+static inline int kvm_arm_set_default_pmu(struct kvm *kvm)
+{
+	return -ENODEV;
+}
 
 static inline u8 kvm_arm_pmu_get_max_counters(struct kvm *kvm)
 {
