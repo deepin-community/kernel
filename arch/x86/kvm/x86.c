f60623d43f1d910c6d40cb125dae5cd1c657067b
Sean Christopherson <seanjc@google.com>
2023-10-27 11:22:05 -0700

    KVM: x86: Add support for "protected VMs" that can utilize private memory
    mainline inclusion
    from mainline-v6.8-rc1
    category: performance
    
    Add a new x86 VM type, KVM_X86_SW_PROTECTED_VM, to serve as a development
    and testing vehicle for Confidential (CoCo) VMs, and potentially to even
    become a "real" product in the distant future, e.g. a la pKVM.
    
    The private memory support in KVM x86 is aimed at AMD's SEV-SNP and
    Intel's TDX, but those technologies are extremely complex (understatement),
    difficult to debug, don't support running as nested guests, and require
    hardware that's isn't universally accessible.  I.e. relying SEV-SNP or TDX
    for maintaining guest private memory isn't a realistic option.
    
    At the very least, KVM_X86_SW_PROTECTED_VM will enable a variety of
    selftests for guest_memfd and private memory support without requiring
    unique hardware.
    
    Signed-off-by: Sean Christopherson <seanjc@google.com>
    Reviewed-by: Paolo Bonzini <pbonzini@redhat.com>
    Message-Id: <20231027182217.3615211-24-seanjc@google.com>
    Reviewed-by: Fuad Tabba <tabba@google.com>
    Tested-by: Fuad Tabba <tabba@google.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>
    Conflicts:
    	arch/x86/kvm/x86.c
    (cherry picked from commit 89ea60c2c7b5838bf192c50062d5720cd6ab8662)
    Signed-off-by: Wentao Guan <guanwentao@uniontech.com>

--- arch/x86/kvm/x86.c
+++ arch/x86/kvm/x86.c
@@ -4453,6 +4453,13 @@ static int kvm_ioctl_get_supported_hv_cpuid(struct kvm_vcpu *vcpu,
 	return 0;
 }
 
+static bool kvm_is_vm_type_supported(unsigned long type)
+{
+	return type == KVM_X86_DEFAULT_VM ||
+	       (type == KVM_X86_SW_PROTECTED_VM &&
+		IS_ENABLED(CONFIG_KVM_SW_PROTECTED_VM) && tdp_enabled);
+}
+
 int kvm_vm_ioctl_check_extension(struct kvm *kvm, long ext)
 {
 	int r = 0;
@@ -4666,6 +4673,11 @@ int kvm_vm_ioctl_check_extension(struct kvm *kvm, long ext)
 		 */
 		if (is_x86_vendor_hygon() && kvm_x86_ops.get_hygon_coco_extension)
 			r = static_call(kvm_x86_get_hygon_coco_extension)(kvm);
+		break;
+	case KVM_CAP_VM_TYPES:
+		r = BIT(KVM_X86_DEFAULT_VM);
+		if (kvm_is_vm_type_supported(KVM_X86_SW_PROTECTED_VM))
+			r |= BIT(KVM_X86_SW_PROTECTED_VM);
 		break;
 	default:
 		break;
@@ -12533,8 +12545,10 @@ int kvm_arch_init_vm(struct kvm *kvm, unsigned long type)
 	int ret;
 	unsigned long flags;
 
-	if (type)
+	if (!kvm_is_vm_type_supported(type))
 		return -EINVAL;
+
+	kvm->arch.vm_type = type;
 
 	ret = kvm_page_track_init(kvm);
 	if (ret)
