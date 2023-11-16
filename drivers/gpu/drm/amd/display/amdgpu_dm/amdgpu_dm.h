ef79f6d35e9f376504d94ab67a63ca418ca217de
Joshua Ashton <joshua@froggi.es>
2023-11-16 18:57:59 -0100

    drm/amd/display: add plane degamma TF and LUT support
    Set DC plane with user degamma LUT or predefined TF from driver-specific
    plane color properties. If plane and CRTC degamma are set in the same
    time, plane degamma has priority.  That means, we only set CRTC degamma
    if we don't have plane degamma LUT or TF to configure. We return -EINVAL
    if we don't have plane degamma settings, so we can continue and check
    CRTC degamma.
    
    Reviewed-by: Harry Wentland <harry.wentland@amd.com>
    Signed-off-by: Joshua Ashton <joshua@froggi.es>
    Signed-off-by: Melissa Wen <mwen@igalia.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm.h
+++ drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm.h
@@ -820,6 +820,7 @@ void amdgpu_dm_init_color_mod(void);
 int amdgpu_dm_verify_lut_sizes(const struct drm_crtc_state *crtc_state);
 int amdgpu_dm_update_crtc_color_mgmt(struct dm_crtc_state *crtc);
 int amdgpu_dm_update_plane_color_mgmt(struct dm_crtc_state *crtc,
+				      struct drm_plane_state *plane_state,
 				      struct dc_plane_state *dc_plane_state);
 
 void amdgpu_dm_update_connector_after_detect(
