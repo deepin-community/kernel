fd1ac56150427e25d73681450f6a2b8542bea73d
liuyun <liuyun@loongson.cn>
2023-12-02 10:08:32 +0800

    LoongArch: Fix virtual machine startup error
    Upstream: no
    
    Signed-off-by: liuyun <liuyun@loongson.cn>
    Signed-off-by: maobibo <maobibo@loongson.cn>
    Signed-off-by: Ming Wang <wangming01@loongson.cn>
    Signed-off-by: Xianglai Li <lixianglai@loongson.cn>

--- drivers/irqchip/irq-loongson-eiointc.c
+++ drivers/irqchip/irq-loongson-eiointc.c
@@ -80,7 +80,9 @@ static void eiointc_enable(void)
 
 static int cpu_to_eio_node(int cpu)
 {
-	return cpu_logical_map(cpu) / CORES_PER_EIO_NODE;
+	int cores = (cpu_has_hypervisor ? MAX_CORES_PER_EIO_NODE : CORES_PER_EIO_NODE);
+
+	return cpu_logical_map(cpu) / cores;
 }
 
 static void eiointc_set_irq_route(int pos, unsigned int cpu, unsigned int mnode, nodemask_t *node_map)
@@ -113,6 +115,11 @@ static void eiointc_set_irq_route(int pos, unsigned int cpu, unsigned int mnode,
 
 static DEFINE_RAW_SPINLOCK(affinity_lock);
 
+static void virt_extioi_set_irq_route(int irq, unsigned int cpu)
+{
+	iocsr_write8(cpu_logical_map(cpu), EIOINTC_REG_ROUTE + irq);
+}
+
 static int eiointc_set_irq_affinity(struct irq_data *d, const struct cpumask *affinity, bool force)
 {
 	unsigned int cpu, i;
@@ -140,16 +147,22 @@ static int eiointc_set_irq_affinity(struct irq_data *d, const struct cpumask *af
 			break;
 	}
 
-	/* Mask target vector */
-	csr_any_send(regaddr, EIOINTC_ALL_ENABLE & (~BIT(vector & 0x1F)),
-		     0x0, cpu_logical_map(i));
+	if (cpu_has_hypervisor) {
+		iocsr_write32(EIOINTC_ALL_ENABLE & ~BIT(vector & 0x1F), regaddr);
+		virt_extioi_set_irq_route(vector, cpu);
+		iocsr_write32(EIOINTC_ALL_ENABLE, regaddr);
+	} else {
+		/* Mask target vector */
+		csr_any_send(regaddr, EIOINTC_ALL_ENABLE & (~BIT(vector & 0x1F)),
+			     0x0, cpu_logical_map(i));
 
-	/* Set route for target vector */
-	eiointc_set_irq_route(vector, cpu, priv->node, &priv->node_map);
+		/* Set route for target vector */
+		eiointc_set_irq_route(vector, cpu, priv->node, &priv->node_map);
 
-	/* Unmask target vector */
-	csr_any_send(regaddr, EIOINTC_ALL_ENABLE,
-		     0x0, cpu_logical_map(i));
+		/* Unmask target vector */
+		csr_any_send(regaddr, EIOINTC_ALL_ENABLE,
+		     		0x0, cpu_logical_map(i));
+	}
 
 	irq_data_update_effective_affinity(d, cpumask_of(cpu));
 
@@ -176,13 +189,14 @@ static int eiointc_router_init(unsigned int cpu)
 	uint32_t data;
 	uint32_t node = cpu_to_eio_node(cpu);
 	int index = eiointc_index(node);
+	int cores = (cpu_has_hypervisor ? MAX_CORES_PER_EIO_NODE : CORES_PER_EIO_NODE);
 
 	if (index < 0) {
 		pr_err("Error: invalid nodemap!\n");
 		return -1;
 	}
 
-	if ((cpu_logical_map(cpu) % CORES_PER_EIO_NODE) == 0) {
+	if ((cpu_logical_map(cpu) % cores) == 0) {
 		eiointc_enable();
 
 		for (i = 0; i < eiointc_priv[0]->vec_count / 32; i++) {
@@ -199,7 +213,7 @@ static int eiointc_router_init(unsigned int cpu)
 		for (i = 0; i < eiointc_priv[0]->vec_count / 4; i++) {
 			/* Route to Node-0 Core-0 */
 			if (index == 0)
-				bit = BIT(cpu_logical_map(0));
+				bit = (cpu_has_hypervisor ? cpu_logical_map(0) : BIT(cpu_logical_map(0)));
 			else
 				bit = (eiointc_priv[index]->node << 4) |
 				BIT(cpu_logical_map(smp_processor_id()) %
