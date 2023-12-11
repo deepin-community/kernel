2bd329c711db2a7e7b8867552fc0a4e608cdf033
Kent Overstreet <kent.overstreet@linux.dev>
2023-12-11 13:53:34 -0500

    timers: Split out timer_types.h
    mainline inclusion
    from mainline-v6.8-rc1
    category: performace
    
    Cutting down on sched.h dependencies: this is going to be used in
    workqueue_types.h in the next patch, so we can kill the sched.h
    dependency on workqueue.h.
    
    Signed-off-by: Kent Overstreet <kent.overstreet@linux.dev>
    Conflicts:
    	include/linux/timer.h
    [add timer_types.h to the code]
    (cherry picked from commit dff0fd233a5104337069603d201f8cad74bc0e5a)
    Signed-off-by: Wentao Guan <guanwentao@uniontech.com>

--- include/linux/timer.h
+++ include/linux/timer.h
@@ -7,25 +7,7 @@
 #include <linux/stddef.h>
 #include <linux/debugobjects.h>
 #include <linux/stringify.h>
-#include <linux/deepin_kabi.h>
-
-struct timer_list {
-	/*
-	 * All fields that change during normal runtime grouped to the
-	 * same cacheline
-	 */
-	struct hlist_node	entry;
-	unsigned long		expires;
-	void			(*function)(struct timer_list *);
-	u32			flags;
-
-#ifdef CONFIG_LOCKDEP
-	struct lockdep_map	lockdep_map;
-#endif
-
-	DEEPIN_KABI_RESERVE(1)
-	DEEPIN_KABI_RESERVE(2)
-};
+#include <linux/timer_types.h>
 
 #ifdef CONFIG_LOCKDEP
 /*
