a208e517519bd04488b644fd68eaa877d2efca32
Lorenzo Bianconi <lorenzo@kernel.org>
2023-12-11 13:07:29 +0100

    UPSTREAM: wifi: mt76: move wed common utilities in wed.c
    Introduce wed.c in order to collect mt76 wed common codebase used by
    mt7915 and mt7996 drivers.
    
    Signed-off-by: Lorenzo Bianconi <lorenzo@kernel.org>
    Signed-off-by: Felix Fietkau <nbd@nbd.name>
    (cherry picked from commit 8a7386e787ef4d9649930bb09cdf55ccd2102346
     https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git master)
    
    BUG=b:178754244, b:337131500
    TEST=Build against all mt76 drivers successfully
    
    Change-Id: I589cd23e91d13ffa1fbb0e29490a018ccb58aae5
    Signed-off-by: Sean Wang <objelf@gmail.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/5486403
    Reviewed-by: Jintao Lin <jintaolin@chromium.org>
    Tested-by: Jintao Lin <jintaolin@chromium.org>
    Commit-Queue: David Ruth <druth@chromium.org>
    Reviewed-by: Sean Paul <sean@poorly.run>
    Reviewed-by: David Ruth <druth@chromium.org>
    Signed-off-by: goutongchen <goutongchen@uniontech.com>

--- drivers/net/wireless/mediatek/mt76/mt76.h
+++ drivers/net/wireless/mediatek/mt76/mt76.h
@@ -1084,12 +1084,6 @@ void mt76_mmio_init(struct mt76_dev *dev, void __iomem *regs);
 void mt76_pci_disable_aspm(struct pci_dev *pdev);
 bool mt76_pci_aspm_supported(struct pci_dev *pdev);
 
-#ifdef CONFIG_NET_MEDIATEK_SOC_WED
-int mt76_net_setup_tc(struct ieee80211_hw *hw, struct ieee80211_vif *vif,
-		      struct net_device *netdev, enum tc_setup_type type,
-		      void *type_data);
-#endif /*CONFIG_NET_MEDIATEK_SOC_WED */
-
 static inline u16 mt76_chip(struct mt76_dev *dev)
 {
 	return dev->rev >> 16;
@@ -1100,13 +1094,34 @@ static inline u16 mt76_rev(struct mt76_dev *dev)
 	return dev->rev & 0xffff;
 }
 
+void mt76_wed_release_rx_buf(struct mtk_wed_device *wed);
+void mt76_wed_offload_disable(struct mtk_wed_device *wed);
+void mt76_wed_reset_complete(struct mtk_wed_device *wed);
+void mt76_wed_dma_reset(struct mt76_dev *dev);
+int mt76_wed_net_setup_tc(struct ieee80211_hw *hw, struct ieee80211_vif *vif,
+			  struct net_device *netdev, enum tc_setup_type type,
+			  void *type_data);
 #ifdef CONFIG_NET_MEDIATEK_SOC_WED
-u32 mt76_mmio_wed_init_rx_buf(struct mtk_wed_device *wed, int size);
-void mt76_mmio_wed_release_rx_buf(struct mtk_wed_device *wed);
-int mt76_mmio_wed_offload_enable(struct mtk_wed_device *wed);
-void mt76_mmio_wed_offload_disable(struct mtk_wed_device *wed);
-void mt76_mmio_wed_reset_complete(struct mtk_wed_device *wed);
-#endif /*CONFIG_NET_MEDIATEK_SOC_WED */
+u32 mt76_wed_init_rx_buf(struct mtk_wed_device *wed, int size);
+int mt76_wed_offload_enable(struct mtk_wed_device *wed);
+int mt76_wed_dma_setup(struct mt76_dev *dev, struct mt76_queue *q, bool reset);
+#else
+static inline u32 mt76_wed_init_rx_buf(struct mtk_wed_device *wed, int size)
+{
+	return 0;
+}
+
+static inline int mt76_wed_offload_enable(struct mtk_wed_device *wed)
+{
+	return 0;
+}
+
+static inline int mt76_wed_dma_setup(struct mt76_dev *dev, struct mt76_queue *q,
+				     bool reset)
+{
+	return 0;
+}
+#endif /* CONFIG_NET_MEDIATEK_SOC_WED */
 
 #define mt76xx_chip(dev) mt76_chip(&((dev)->mt76))
 #define mt76xx_rev(dev) mt76_rev(&((dev)->mt76))
