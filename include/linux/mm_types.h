55680a261d2a2d6a610f987b049ce778511cc5fb
Kinsey Ho <kinseyho@google.com>
2023-12-27 14:12:03 +0000

    mm/mglru: remove CONFIG_MEMCG
    Remove CONFIG_MEMCG in a refactoring to improve code readability at
    the cost of a few bytes in struct lru_gen_folio per node when
    CONFIG_MEMCG=n.
    
    Link: https://lkml.kernel.org/r/20231227141205.2200125-4-kinseyho@google.com
    Signed-off-by: Kinsey Ho <kinseyho@google.com>
    Co-developed-by: Aneesh Kumar K.V <aneesh.kumar@linux.ibm.com>
    Signed-off-by: Aneesh Kumar K.V <aneesh.kumar@linux.ibm.com>
    Tested-by: Donet Tom <donettom@linux.vnet.ibm.com>
    Acked-by: Yu Zhao <yuzhao@google.com>
    Cc: kernel test robot <lkp@intel.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- include/linux/mm_types.h
+++ include/linux/mm_types.h
@@ -1020,9 +1020,7 @@ struct lru_gen_mm_list {
 
 void lru_gen_add_mm(struct mm_struct *mm);
 void lru_gen_del_mm(struct mm_struct *mm);
-#ifdef CONFIG_MEMCG
 void lru_gen_migrate_mm(struct mm_struct *mm);
-#endif
 
 static inline void lru_gen_init_mm(struct mm_struct *mm)
 {
@@ -1053,11 +1051,9 @@ static inline void lru_gen_del_mm(struct mm_struct *mm)
 {
 }
 
-#ifdef CONFIG_MEMCG
 static inline void lru_gen_migrate_mm(struct mm_struct *mm)
 {
 }
-#endif
 
 static inline void lru_gen_init_mm(struct mm_struct *mm)
 {
