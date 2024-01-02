6e4e3a67d9c60c85f81baa6dfa12077d7c1778ab
Min Fanlei <minfanlei@wxiat.com>
2024-01-02 10:51:13 +0800

    sw64: kvm: fix clock sync of CORE4 live migration
    Move "migration_mark" and "shtclock" from struct kvm_vcpu_arch to
    struct vcpucb, so that they can be synced between the src and dst
    guest.
    
    Signed-off-by: Min Fanlei <minfanlei@wxiat.com>
    Reviewed-by: He Sheng <hesheng@wxiat.com>
    Signed-off-by: Gu Zitao <guzitao@wxiat.com>
    (cherry picked from commit b4bc0a45984ce3c424cae65625ae91b127c4bdc2)

--- arch/sw_64/kvm/mmu.c
+++ arch/sw_64/kvm/mmu.c
@@ -544,7 +544,7 @@ void kvm_mark_migration(struct kvm *kvm, int mark)
 	unsigned long cpu;
 
 	kvm_for_each_vcpu(cpu, vcpu, kvm)
-		vcpu->arch.migration_mark = mark;
+		vcpu->arch.vcb.migration_mark = mark;
 }
 
 void kvm_arch_commit_memory_region(struct kvm *kvm,
