13f8e7927c8819985946f87b1bff2a1bf5ff6f7c
Felix Fietkau <nbd@nbd.name>
2024-01-18 15:51:53 +0100

    UPSTREAM: wifi: mt76: mt7915: fix error recovery with WED enabled
    Do not clear the interrupt mask register on MT7915, since that prevents
    MCU_CMD interrupts from being reported, thus causing timeouts during the
    reset sequence. Defer stopping WED until tx/rx processing activity has
    stopped.
    
    Signed-off-by: Felix Fietkau <nbd@nbd.name>
    (cherry picked from commit 350f63c9624b8fd407805c7dfb652a1c3d619862
     https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git master)
    
    BUG=b:178754244, b:337131500
    TEST=Build against all mt76 drivers successfully
    
    Change-Id: Iada05543183a7d1a21477693ddd5fa8ff01fbc89
    Signed-off-by: Sean Wang <objelf@gmail.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/5486380
    Reviewed-by: David Ruth <druth@chromium.org>
    Tested-by: Jintao Lin <jintaolin@chromium.org>
    Reviewed-by: Sean Paul <sean@poorly.run>
    Reviewed-by: Jintao Lin <jintaolin@chromium.org>
    Commit-Queue: David Ruth <druth@chromium.org>
    Signed-off-by: goutongchen <goutongchen@uniontech.com>

--- drivers/net/wireless/mediatek/mt76/mt7915/mac.c
+++ drivers/net/wireless/mediatek/mt76/mt7915/mac.c
@@ -1515,12 +1515,6 @@ void mt7915_mac_reset_work(struct work_struct *work)
 	if (!(READ_ONCE(dev->recovery.state) & MT_MCU_CMD_STOP_DMA))
 		return;
 
-	if (mtk_wed_device_active(&dev->mt76.mmio.wed)) {
-		mtk_wed_device_stop(&dev->mt76.mmio.wed);
-		if (!is_mt798x(&dev->mt76))
-			mt76_wr(dev, MT_INT_WED_MASK_CSR, 0);
-	}
-
 	ieee80211_stop_queues(mt76_hw(dev));
 	if (ext_phy)
 		ieee80211_stop_queues(ext_phy->hw);
@@ -1541,6 +1535,8 @@ void mt7915_mac_reset_work(struct work_struct *work)
 		napi_disable(&dev->mt76.napi[i]);
 	napi_disable(&dev->mt76.tx_napi);
 
+	if (mtk_wed_device_active(&dev->mt76.mmio.wed))
+		mtk_wed_device_stop(&dev->mt76.mmio.wed);
 
 	mt76_wr(dev, MT_MCU_INT_EVENT, MT_MCU_INT_EVENT_DMA_STOPPED);
 
