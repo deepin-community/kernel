d65923f89ff630d25732537c7dccfa9acf31d277
Breno Leitao <leitao@debian.org>
2024-01-30 02:42:43 -0800

    wifi: fill in MODULE_DESCRIPTION()s for mt76 drivers
    W=1 builds now warn if module is built without a MODULE_DESCRIPTION().
    Add descriptions to the MediaTek mt76 drivers.
    
    Here is a sorted list of descriptions. It might make the reviewing
    process easier.
    
    	MODULE_DESCRIPTION("MediaTek MT7603E and MT76x8 wireless driver");
    	MODULE_DESCRIPTION("MediaTek MT7615E and MT7663E wireless driver");
    	MODULE_DESCRIPTION("MediaTek MT7615E MMIO helpers");
    	MODULE_DESCRIPTION("MediaTek MT7663 SDIO/USB helpers");
    	MODULE_DESCRIPTION("MediaTek MT7663S (SDIO) wireless driver");
    	MODULE_DESCRIPTION("MediaTek MT7663U (USB) wireless driver");
    	MODULE_DESCRIPTION("MediaTek MT76x02 helpers");
    	MODULE_DESCRIPTION("MediaTek MT76x02 MCU helpers");
    	MODULE_DESCRIPTION("MediaTek MT76x0E (PCIe) wireless driver");
    	MODULE_DESCRIPTION("MediaTek MT76x0U (USB) wireless driver");
    	MODULE_DESCRIPTION("MediaTek MT76x2 EEPROM helpers");
    	MODULE_DESCRIPTION("MediaTek MT76x2E (PCIe) wireless driver");
    	MODULE_DESCRIPTION("MediaTek MT76x2U (USB) wireless driver");
    	MODULE_DESCRIPTION("MediaTek MT76x connac layer helpers");
    	MODULE_DESCRIPTION("MediaTek MT76x EEPROM helpers");
    	MODULE_DESCRIPTION("MediaTek MT76x helpers");
    	MODULE_DESCRIPTION("MediaTek MT76x SDIO helpers");
    	MODULE_DESCRIPTION("MediaTek MT76x USB helpers");
    	MODULE_DESCRIPTION("MediaTek MT7915E MMIO helpers");
    	MODULE_DESCRIPTION("MediaTek MT7921 core driver");
    	MODULE_DESCRIPTION("MediaTek MT7921E (PCIe) wireless driver");
    	MODULE_DESCRIPTION("MediaTek MT7921S (SDIO) wireless driver");
    	MODULE_DESCRIPTION("MediaTek MT7921U (USB) wireless driver");
    	MODULE_DESCRIPTION("MediaTek MT7925 core driver");
    	MODULE_DESCRIPTION("MediaTek MT7925E (PCIe) wireless driver");
    	MODULE_DESCRIPTION("MediaTek MT7925U (USB) wireless driver");
    	MODULE_DESCRIPTION("MediaTek MT792x core driver");
    	MODULE_DESCRIPTION("MediaTek MT792x USB helpers");
    	MODULE_DESCRIPTION("MediaTek MT7996 MMIO helpers");
    
    Signed-off-by: Breno Leitao <leitao@debian.org>
    Signed-off-by: Kalle Valo <kvalo@kernel.org>
    Link: https://msgid.link/20240130104243.3025393-10-leitao@debian.org
    (cherry picked from commit f3f8f050316893fe2da523458ff7f5f6d61fb1a6)

--- drivers/net/wireless/mediatek/mt76/mt7925/main.c
+++ drivers/net/wireless/mediatek/mt76/mt7925/main.c
@@ -1471,4 +1471,5 @@ const struct ieee80211_ops mt7925_ops = {
 EXPORT_SYMBOL_GPL(mt7925_ops);
 
 MODULE_AUTHOR("Deren Wu <deren.wu@mediatek.com>");
+MODULE_DESCRIPTION("MediaTek MT7925 core driver");
 MODULE_LICENSE("Dual BSD/GPL");
