7f4c86c442381e79a9d579dfecc30a838dcda6d5
Zeng Heng <zengheng4@huawei.com>
2024-02-02 17:17:20 +0800

    deepin: KABI:kabi: reserve space for workqueue subsystem related structure
    hulk inclusion
    category: feature
    bugzilla: https://gitee.com/openeuler/kernel/issues/I9089C
    CVE: NA
    
    -------------------------------
    
    Reserve space for workqueue subsystem.
    
    Signed-off-by: Zeng Heng <zengheng4@huawei.com>
    Signed-off-by: yunjianshijie <dailingjia@uniontech.com>

--- include/linux/workqueue.h
+++ include/linux/workqueue.h
@@ -6,6 +6,7 @@
 #ifndef _LINUX_WORKQUEUE_H
 #define _LINUX_WORKQUEUE_H
 
+#include <linux/deepin_kabi.h>
 #include <linux/timer.h>
 #include <linux/linkage.h>
 #include <linux/bitops.h>
@@ -102,6 +103,9 @@ struct work_struct {
 #ifdef CONFIG_LOCKDEP
 	struct lockdep_map lockdep_map;
 #endif
+
+	DEEPIN_KABI_RESERVE(1)
+	DEEPIN_KABI_RESERVE(2)
 };
 
 #define WORK_DATA_INIT()	ATOMIC_LONG_INIT((unsigned long)WORK_STRUCT_NO_POOL)
@@ -201,6 +205,11 @@ struct workqueue_attrs {
 	 * @ordered: work items must be executed one by one in queueing order
 	 */
 	bool ordered;
+
+	DEEPIN_KABI_RESERVE(1)
+	DEEPIN_KABI_RESERVE(2)
+	DEEPIN_KABI_RESERVE(3)
+	DEEPIN_KABI_RESERVE(4)
 };
 
 static inline struct delayed_work *to_delayed_work(struct work_struct *work)
