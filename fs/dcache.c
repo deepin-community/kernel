f70a734dbbafa310b61175c0c122ee2d79507bbe
Al Viro <viro@zeniv.linux.org.uk>
2024-02-03 23:53:05 -0500

    Revert "get rid of DCACHE_GENOCIDE"
    This reverts commit 57851607326a2beef21e67f83f4f53a90df8445a.
    
    Unfortunately, while we only call that thing once, the callback
    *can* be called more than once for the same dentry - all it
    takes is rename_lock being touched while we are in d_walk().
    For now let's revert it.
    
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- fs/dcache.c
+++ fs/dcache.c
@@ -3131,7 +3131,10 @@ static enum d_walk_ret d_genocide_kill(void *data, struct dentry *dentry)
 		if (d_unhashed(dentry) || !dentry->d_inode)
 			return D_WALK_SKIP;
 
-		dentry->d_lockref.count--;
+		if (!(dentry->d_flags & DCACHE_GENOCIDE)) {
+			dentry->d_flags |= DCACHE_GENOCIDE;
+			dentry->d_lockref.count--;
+		}
 	}
 	return D_WALK_CONTINUE;
 }
