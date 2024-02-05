7364ffbe2168e5aa0b53f57cf27d5b4c3c4b8446
Yi Yang <yiyang13@huawei.com>
2024-02-05 09:36:00 +0000

    deepin: KABI: tty: KABI reservation for tty
    hulk inclusion
    category: feature
    bugzilla: https://gitee.com/openeuler/kernel/issues/I8T231
    
    --------------------------------
    
    Reserve space for tty module in advance to prepare for merging new
    feature in the future.
    
    Signed-off-by: Yi Yang <yiyang13@huawei.com>
    Signed-off-by: Wentao Guan <guanwentao@uniontech.com>

--- include/linux/tty_buffer.h
+++ include/linux/tty_buffer.h
@@ -6,6 +6,7 @@
 #include <linux/llist.h>
 #include <linux/mutex.h>
 #include <linux/workqueue.h>
+#include <linux/deepin_kabi.h>
 
 struct tty_buffer {
 	union {
@@ -18,6 +19,8 @@ struct tty_buffer {
 	unsigned int lookahead;		/* Lazy update on recv, can become less than "read" */
 	unsigned int read;
 	bool flags;
+	DEEPIN_KABI_RESERVE(0)
+	DEEPIN_KABI_RESERVE(1)
 	/* Data points here */
 	u8 data[] __aligned(sizeof(unsigned long));
 };
@@ -42,6 +45,8 @@ struct tty_bufhead {
 	atomic_t	   mem_used;    /* In-use buffers excluding free list */
 	int		   mem_limit;
 	struct tty_buffer *tail;	/* Active buffer */
+	DEEPIN_KABI_RESERVE(0)
+	DEEPIN_KABI_RESERVE(1)
 };
 
 /*
