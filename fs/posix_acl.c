97bda0b41ef012e059baf0682c502ffef760d33b
Roberto Sassu <roberto.sassu@huawei.com>
2024-02-15 11:31:10 +0100

    evm: Move to LSM infrastructure
    mainline inclusion
    from mainline-v6.9-rc1
    category: feature
    
    As for IMA, move hardcoded EVM function calls from various places in the
    kernel to the LSM infrastructure, by introducing a new LSM named 'evm'
    (last and always enabled like 'ima'). The order in the Makefile ensures
    that 'evm' hooks are executed after 'ima' ones.
    
    Make EVM functions as static (except for evm_inode_init_security(), which
    is exported), and register them as hook implementations in init_evm_lsm().
    Also move the inline functions evm_inode_remove_acl(),
    evm_inode_post_remove_acl(), and evm_inode_post_set_acl() from the public
    evm.h header to evm_main.c.
    
    Unlike before (see commit to move IMA to the LSM infrastructure),
    evm_inode_post_setattr(), evm_inode_post_set_acl(),
    evm_inode_post_remove_acl(), and evm_inode_post_removexattr() are not
    executed for private inodes.
    
    Finally, add the LSM_ID_EVM case in lsm_list_modules_test.c
    
    Signed-off-by: Roberto Sassu <roberto.sassu@huawei.com>
    Reviewed-by: Casey Schaufler <casey@schaufler-ca.com>
    Acked-by: Christian Brauner <brauner@kernel.org>
    Reviewed-by: Stefan Berger <stefanb@linux.ibm.com>
    Reviewed-by: Mimi Zohar <zohar@linux.ibm.com>
    Acked-by: Mimi Zohar <zohar@linux.ibm.com>
    Signed-off-by: Paul Moore <paul@paul-moore.com>
    Conflicts: security/security.c
               include/uapi/linux/lsm.h
               tools/testing/selftests/lsm/lsm_list_modules_test.c
    (cherry picked from commit 9238311176115aac1b1a86e8e968c04ebec747a1)
    Signed-off-by: Wentao Guan <guanwentao@uniontech.com>

--- fs/posix_acl.c
+++ fs/posix_acl.c
@@ -26,7 +26,6 @@
 #include <linux/mnt_idmapping.h>
 #include <linux/iversion.h>
 #include <linux/security.h>
-#include <linux/evm.h>
 #include <linux/fsnotify.h>
 #include <linux/filelock.h>
 
@@ -1145,7 +1144,6 @@ retry_deleg:
 	if (!error) {
 		fsnotify_xattr(dentry);
 		security_inode_post_set_acl(dentry, acl_name, kacl);
-		evm_inode_post_set_acl(dentry, acl_name, kacl);
 	}
 
 out_inode_unlock:
@@ -1254,7 +1252,6 @@ retry_deleg:
 	if (!error) {
 		fsnotify_xattr(dentry);
 		security_inode_post_remove_acl(idmap, dentry, acl_name);
-		evm_inode_post_remove_acl(idmap, dentry, acl_name);
 	}
 
 out_inode_unlock:
