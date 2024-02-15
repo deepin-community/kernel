f3cc09cc5a7d5648ca8af6cd0fbbfbde75087d5b
Roberto Sassu <roberto.sassu@huawei.com>
2024-02-15 11:30:58 +0100

    security: Introduce inode_post_setattr hook
    mainline inclusion
    from mainline-v6.9-rc1
    category: feature
    
    In preparation for moving IMA and EVM to the LSM infrastructure, introduce
    the inode_post_setattr hook.
    
    At inode_setattr hook, EVM verifies the file's existing HMAC value. At
    inode_post_setattr, EVM re-calculates the file's HMAC based on the modified
    file attributes and other file metadata.
    
    Other LSMs could similarly take some action after successful file attribute
    change.
    
    The new hook cannot return an error and cannot cause the operation to be
    reverted.
    
    Signed-off-by: Roberto Sassu <roberto.sassu@huawei.com>
    Reviewed-by: Stefan Berger <stefanb@linux.ibm.com>
    Reviewed-by: Mimi Zohar <zohar@linux.ibm.com>
    Acked-by: Casey Schaufler <casey@schaufler-ca.com>
    Acked-by: Christian Brauner <brauner@kernel.org>
    Signed-off-by: Paul Moore <paul@paul-moore.com>
    (cherry picked from commit 77fa6f314f0376176ef6bf3d84403e0d8b54ce28)
    Signed-off-by: Wentao Guan <guanwentao@uniontech.com>

--- include/linux/security.h
+++ include/linux/security.h
@@ -357,6 +357,8 @@ int security_inode_follow_link(struct dentry *dentry, struct inode *inode,
 int security_inode_permission(struct inode *inode, int mask);
 int security_inode_setattr(struct mnt_idmap *idmap,
 			   struct dentry *dentry, struct iattr *attr);
+void security_inode_post_setattr(struct mnt_idmap *idmap, struct dentry *dentry,
+				 int ia_valid);
 int security_inode_getattr(const struct path *path);
 int security_inode_setxattr(struct mnt_idmap *idmap,
 			    struct dentry *dentry, const char *name,
@@ -871,6 +873,11 @@ static inline int security_inode_setattr(struct mnt_idmap *idmap,
 {
 	return 0;
 }
+
+static inline void
+security_inode_post_setattr(struct mnt_idmap *idmap, struct dentry *dentry,
+			    int ia_valid)
+{ }
 
 static inline int security_inode_getattr(const struct path *path)
 {
