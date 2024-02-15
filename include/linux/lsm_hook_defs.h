22d0b57866733ae03f685084ad365cc66a522dc3
Roberto Sassu <roberto.sassu@huawei.com>
2024-02-15 11:31:01 +0100

    security: Introduce file_release hook
    mainline inclusion
    from mainline-v6.9-rc1
    category: feature
    
    In preparation for moving IMA and EVM to the LSM infrastructure, introduce
    the file_release hook.
    
    IMA calculates at file close the new digest of the file content and writes
    it to security.ima, so that appraisal at next file access succeeds.
    
    The new hook cannot return an error and cannot cause the operation to be
    reverted.
    
    Signed-off-by: Roberto Sassu <roberto.sassu@huawei.com>
    Acked-by: Christian Brauner <brauner@kernel.org>
    Reviewed-by: Stefan Berger <stefanb@linux.ibm.com>
    Reviewed-by: Mimi Zohar <zohar@linux.ibm.com>
    Signed-off-by: Paul Moore <paul@paul-moore.com>
    (cherry picked from commit f09068b5a114ed28d2df2e82a7d30dde0145dc69)
    Signed-off-by: Wentao Guan <guanwentao@uniontech.com>

--- include/linux/lsm_hook_defs.h
+++ include/linux/lsm_hook_defs.h
@@ -173,6 +173,7 @@ LSM_HOOK(int, 0, kernfs_init_security, struct kernfs_node *kn_dir,
 	 struct kernfs_node *kn)
 LSM_HOOK(int, 0, file_permission, struct file *file, int mask)
 LSM_HOOK(int, 0, file_alloc_security, struct file *file)
+LSM_HOOK(void, LSM_RET_VOID, file_release, struct file *file)
 LSM_HOOK(void, LSM_RET_VOID, file_free_security, struct file *file)
 LSM_HOOK(int, 0, backing_file_alloc, struct file *backing_file,
 	 const struct file *user_file)
--- include/linux/security.h
+++ include/linux/security.h
@@ -391,6 +391,7 @@ int security_kernfs_init_security(struct kernfs_node *kn_dir,
 				  struct kernfs_node *kn);
 int security_file_permission(struct file *file, int mask);
 int security_file_alloc(struct file *file);
+void security_file_release(struct file *file);
 void security_file_free(struct file *file);
 int security_backing_file_alloc(struct file *backing_file,
 				const struct file *user_file);
@@ -1000,6 +1001,9 @@ static inline int security_file_alloc(struct file *file)
 {
 	return 0;
 }
+
+static inline void security_file_release(struct file *file)
+{ }
 
 static inline void security_file_free(struct file *file)
 { }
