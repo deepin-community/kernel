159d8d4b7c55f4cbea493de556e84345884b9fbf
Roberto Sassu <roberto.sassu@huawei.com>
2024-02-15 11:31:03 +0100

    security: Introduce inode_post_create_tmpfile hook
    mainline inclusion
    from mainline-v6.9-rc1
    category: feature
    
    In preparation for moving IMA and EVM to the LSM infrastructure, introduce
    the inode_post_create_tmpfile hook.
    
    As temp files can be made persistent, treat new temp files like other new
    files, so that the file hash is calculated and stored in the security
    xattr.
    
    LSMs could also take some action after temp files have been created.
    
    The new hook cannot return an error and cannot cause the operation to be
    canceled.
    
    Signed-off-by: Roberto Sassu <roberto.sassu@huawei.com>
    Acked-by: Casey Schaufler <casey@schaufler-ca.com>
    Reviewed-by: Mimi Zohar <zohar@linux.ibm.com>
    Acked-by: Christian Brauner <brauner@kernel.org>
    Reviewed-by: Stefan Berger <stefanb@linux.ibm.com>
    Signed-off-by: Paul Moore <paul@paul-moore.com>
    (cherry picked from commit a7811e34d100acf24870eb949c5ae3e49dde18b9)
    Signed-off-by: Wentao Guan <guanwentao@uniontech.com>

--- fs/namei.c
+++ fs/namei.c
@@ -3737,6 +3737,7 @@ static int vfs_tmpfile(struct mnt_idmap *idmap,
 		inode->i_state |= I_LINKABLE;
 		spin_unlock(&inode->i_lock);
 	}
+	security_inode_post_create_tmpfile(idmap, inode);
 	ima_post_create_tmpfile(idmap, inode);
 	return 0;
 }
--- include/linux/security.h
+++ include/linux/security.h
@@ -340,6 +340,8 @@ int security_inode_init_security_anon(struct inode *inode,
 				      const struct qstr *name,
 				      const struct inode *context_inode);
 int security_inode_create(struct inode *dir, struct dentry *dentry, umode_t mode);
+void security_inode_post_create_tmpfile(struct mnt_idmap *idmap,
+					struct inode *inode);
 int security_inode_link(struct dentry *old_dentry, struct inode *dir,
 			 struct dentry *new_dentry);
 int security_inode_unlink(struct inode *dir, struct dentry *dentry);
@@ -803,6 +805,10 @@ static inline int security_inode_create(struct inode *dir,
 {
 	return 0;
 }
+
+static inline void
+security_inode_post_create_tmpfile(struct mnt_idmap *idmap, struct inode *inode)
+{ }
 
 static inline int security_inode_link(struct dentry *old_dentry,
 				       struct inode *dir,
