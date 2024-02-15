5855fcb7c613a5351d848a047f59da0aab98b459
Roberto Sassu <roberto.sassu@huawei.com>
2024-02-15 11:31:04 +0100

    security: Introduce inode_post_set_acl hook
    mainline inclusion
    from mainline-v6.9-rc1
    category: feature
    
    In preparation for moving IMA and EVM to the LSM infrastructure, introduce
    the inode_post_set_acl hook.
    
    At inode_set_acl hook, EVM verifies the file's existing HMAC value. At
    inode_post_set_acl, EVM re-calculates the file's HMAC based on the modified
    POSIX ACL and other file metadata.
    
    Other LSMs could similarly take some action after successful POSIX ACL
    change.
    
    The new hook cannot return an error and cannot cause the operation to be
    reverted.
    
    Signed-off-by: Roberto Sassu <roberto.sassu@huawei.com>
    Reviewed-by: Stefan Berger <stefanb@linux.ibm.com>
    Acked-by: Casey Schaufler <casey@schaufler-ca.com>
    Reviewed-by: Mimi Zohar <zohar@linux.ibm.com>
    Acked-by: Christian Brauner <brauner@kernel.org>
    Signed-off-by: Paul Moore <paul@paul-moore.com>
    (cherry picked from commit 8b9d0b825c6573d654c8b8039ea79920926305c2)
    Signed-off-by: Wentao Guan <guanwentao@uniontech.com>

--- include/linux/security.h
+++ include/linux/security.h
@@ -368,6 +368,8 @@ int security_inode_setxattr(struct mnt_idmap *idmap,
 int security_inode_set_acl(struct mnt_idmap *idmap,
 			   struct dentry *dentry, const char *acl_name,
 			   struct posix_acl *kacl);
+void security_inode_post_set_acl(struct dentry *dentry, const char *acl_name,
+				 struct posix_acl *kacl);
 int security_inode_get_acl(struct mnt_idmap *idmap,
 			   struct dentry *dentry, const char *acl_name);
 int security_inode_remove_acl(struct mnt_idmap *idmap,
@@ -907,6 +909,11 @@ static inline int security_inode_set_acl(struct mnt_idmap *idmap,
 {
 	return 0;
 }
+
+static inline void security_inode_post_set_acl(struct dentry *dentry,
+					       const char *acl_name,
+					       struct posix_acl *kacl)
+{ }
 
 static inline int security_inode_get_acl(struct mnt_idmap *idmap,
 					 struct dentry *dentry,
