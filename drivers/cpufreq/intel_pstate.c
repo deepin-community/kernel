65b4db9c872fe3d7f4b72d1f7f1779d635a4f2a2
Rafael J. Wysocki <rafael.j.wysocki@intel.com>
2024-03-21 20:29:43 +0100

    cpufreq: intel_pstate: Drop redundant locking from intel_pstate_driver_cleanup()
    commit f186b2dace86f36cc08872b693185eaf71128898 upstream.
    
    Remove the spinlock locking from intel_pstate_driver_cleanup() as it is
    not necessary because no other code accessing all_cpu_data[] can run in
    parallel with that function.
    
    Had the locking been necessary, though, it would have been incorrect
    because the lock in question is acquired from a hardirq handler and
    it cannot be acquired from thread context without disabling interrupts.
    
    Intel-SIG: commit f186b2dace86 cpufreq: intel_pstate: Drop redundant locking from.
    Backport intel_pstate driver update for 6.6 from 6.11
    
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    [ Yingbao Jia: amend commit log ]
    Signed-off-by: Yingbao Jia <yingbao.jia@intel.com>

--- drivers/cpufreq/intel_pstate.c
+++ drivers/cpufreq/intel_pstate.c
@@ -3134,10 +3134,8 @@ static void intel_pstate_driver_cleanup(void)
 			if (intel_pstate_driver == &intel_pstate)
 				intel_pstate_clear_update_util_hook(cpu);
 
-			raw_spin_lock(&hwp_notify_lock);
 			kfree(all_cpu_data[cpu]);
 			WRITE_ONCE(all_cpu_data[cpu], NULL);
-			raw_spin_unlock(&hwp_notify_lock);
 		}
 	}
 	cpus_read_unlock();
