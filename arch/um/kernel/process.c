80653a077cf5c63cc86851b8c3a8c5ffa5c9d978
Johannes Berg <johannes.berg@intel.com>
2024-03-28 10:06:38 +0100

    um: process: remove unused 'n' variable
    mainline inclusion
    from mainline-v6.10-rc1
    category: bugfix
    
    The return value of fn() wasn't used for a long time,
    so no need to assign it to a variable, addressing a
    W=1 warning.
    
    This seems to be - with patches from others posted to
    the list before - the last W=1 warning in arch/um/.
    
    Fixes: 22e2430d60db ("x86, um: convert to saner kernel_execve() semantics")
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>
    Reviewed-by: Tiwei Bie <tiwei.btw@antgroup.com>
    Signed-off-by: Richard Weinberger <richard@nod.at>
    (cherry picked from commit dac847ae2b718d41b72bd68eb911ca2862ecfb38)
    Signed-off-by: Wentao Guan <guanwentao@uniontech.com>

--- arch/um/kernel/process.c
+++ arch/um/kernel/process.c
@@ -123,7 +123,7 @@ int get_current_pid(void)
  */
 void new_thread_handler(void)
 {
-	int (*fn)(void *), n;
+	int (*fn)(void *);
 	void *arg;
 
 	if (current->thread.prev_sched != NULL)
@@ -136,7 +136,7 @@ void new_thread_handler(void)
 	/*
 	 * callback returns only if the kernel thread execs a process
 	 */
-	n = fn(arg);
+	fn(arg);
 	userspace(&current->thread.regs.regs, current_thread_info()->aux_fp_regs);
 }
 
