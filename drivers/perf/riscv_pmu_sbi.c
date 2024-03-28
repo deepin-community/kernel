caee98dd645173f153191ea567f2cc35e1d0e793
Joel Granados <j.granados@samsung.com>
2024-03-28 16:57:54 +0100

    drivers: perf: Remove the now superfluous sentinel elements from ctl_table array
    mainline inclusion
    from mainline-v6.10-rc1
    category: performance
    
    This commit comes at the tail end of a greater effort to remove the
    empty elements at the end of the ctl_table arrays (sentinels) which will
    reduce the overall build time size of the kernel and run time memory
    bloat by ~64 bytes per sentinel (further information Link :
    https://lore.kernel.org/all/ZO5Yx5JFogGi%2FcBo@bombadil.infradead.org/)
    
    Remove sentinel from sbi_pmu_sysctl_table
    
    Signed-off-by: Joel Granados <j.granados@samsung.com>
    Link: https://lore.kernel.org/r/20240328-jag-sysctl_remset_misc-v1-7-47c1463b3af2@samsung.com
    Signed-off-by: Will Deacon <will@kernel.org>
    (cherry picked from commit f66ae597411cb0d11ed3c281ef2ae809c2e25cb0)
    Signed-off-by: Wentao Guan <guanwentao@uniontech.com>

--- drivers/perf/riscv_pmu_sbi.c
+++ drivers/perf/riscv_pmu_sbi.c
@@ -1016,7 +1016,6 @@ static struct ctl_table sbi_pmu_sysctl_table[] = {
 		.extra1		= SYSCTL_ZERO,
 		.extra2		= SYSCTL_TWO,
 	},
-	{ }
 };
 
 static int pmu_sbi_device_probe(struct platform_device *pdev)
