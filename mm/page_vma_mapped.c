cd7e0c72a9d0eaaeb45989428ea7076d821ce09b
Matthew Wilcox (Oracle) <willy@infradead.org>
2024-04-12 20:35:01 +0100

    mm: make page_mapped_in_vma conditional on CONFIG_MEMORY_FAILURE
    This function is only currently used by the memory-failure code, so we can
    omit it if we're not compiling in the memory-failure code.
    
    Link: https://lkml.kernel.org/r/20240412193510.2356957-5-willy@infradead.org
    Signed-off-by: Matthew Wilcox (Oracle) <willy@infradead.org>
    Suggested-by: Miaohe Lin <linmiaohe@huawei.com>
    Reviewed-by: Jane Chu <jane.chu@oracle.com>
    Reviewed-by: Oscar Salvador <osalvador@suse.de>
    Acked-by: Miaohe Lin <linmiaohe@huawei.com>
    Cc: Dan Williams <dan.j.williams@intel.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    (cherry picked from commit b87f978dc77519943b659dc8b753f544772e7c85)

--- mm/page_vma_mapped.c
+++ mm/page_vma_mapped.c
@@ -318,6 +318,7 @@ next_pte:
 	return false;
 }
 
+#ifdef CONFIG_MEMORY_FAILURE
 /**
  * page_mapped_in_vma - check whether a page is really mapped in a VMA
  * @page: the page to test
@@ -348,3 +349,4 @@ unsigned long page_mapped_in_vma(struct page *page, struct vm_area_struct *vma)
 out:
 	return pvmw.address;
 }
+#endif
