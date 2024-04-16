dd939931b67eae521d367a1f301b0631c9cf1f1f
Gu Zitao <guzitao@wxiat.com>
2024-04-16 08:51:51 +0800

    sw64: add arch-specific SYM_END definition
    Redefine SYM_END to make low version compiler work.
    
    Signed-off-by: Gu Zitao <guzitao@wxiat.com>

--- arch/sw_64/include/asm/linkage.h
+++ arch/sw_64/include/asm/linkage.h
@@ -6,4 +6,8 @@
 #define SYSCALL_ALIAS(alias, name)                                      \
 	asm (#alias " = " #name "\n\t.globl " #alias)
 
+#define SYM_END(name, sym_type)				\
+       .type name sym_type ASM_NL			\
+       .size name, .-name
+
 #endif /* _ASM_SW64_LINKAGE_H */
