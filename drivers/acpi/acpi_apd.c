0f7d6c7934e5873585ce153bdb8f033c66f56695
Xu Yiwei <xuyiwei@wxiat.com>
2024-04-19 14:17:17 +0000

    sw64: match platform device by acpi method
    Signed-off-by: Xu Yiwei <xuyiwei@wxiat.com>
    Reviewed-by: He Sheng <hesheng@wxiat.com>
    Signed-off-by: Gu Zitao <guzitao@wxiat.com>
    (cherry picked from commit af87c0ae0c6a090721c0e97d721261d5c8893eb3)

--- drivers/acpi/acpi_apd.c
+++ drivers/acpi/acpi_apd.c
@@ -267,8 +267,8 @@ static const struct acpi_device_id acpi_apd_device_ids[] = {
 	{ "PHYT0003", APD_ADDR(phytium_i2c_desc) },
 #endif
 #ifdef CONFIG_SW64
-	{ "HISI02A1", APD_ADDR(sunway_i2c_desc) },
-	{ "HISI0173", APD_ADDR(sunway_spi_desc) },
+	{ "SUNW0005", APD_ADDR(sunway_i2c_desc) },
+	{ "SUNW0008", APD_ADDR(sunway_spi_desc) },
 #endif
 	{ }
 };
