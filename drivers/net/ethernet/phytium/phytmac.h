33d15bdd1533913afbde698e24bf69bbd60dbdb7
Li Wencheng <liwencheng@phytium.com.cn>
2024-04-26 15:04:49 +0800

    net/phytmac: Roll back rx_clean version
    Roll back rx_clean version to not use batching.
    
    Mainline: NA
    Signed-off-by: Li Wencheng <liwencheng@phytium.com.cn>
    Signed-off-by: Wang Yinfeng <wangyinfeng@phytium.com.cn>
    Change-Id: Ie6e7585304ac6a7b9101e04c5af5219b3a361996
    (cherry picked from commit fe5384d8442bf523038db01b8ad5c59b2357a2db)
    Signed-off-by: Wentao Guan <guanwentao@uniontech.com>

--- drivers/net/ethernet/phytium/phytmac.h
+++ drivers/net/ethernet/phytium/phytmac.h
@@ -38,7 +38,6 @@
 #define MIN_TX_RING_SIZE		64
 #define MIN_RX_RING_SIZE		64
 #define DEFAULT_TX_DESC_MIN_FREE	64
-#define DEFAULT_RX_DESC_MIN_FREE	64
 
 #define MEMORY_SIZE			4096
 #define MHU_SIZE			0x20
@@ -499,7 +498,6 @@ struct phytmac_hw_if {
 			       struct packet_info *packet);
 	void (*init_rx_map)(struct phytmac_queue *queue, u32 index);
 	unsigned int (*rx_map)(struct phytmac_queue *queue, u32 index, dma_addr_t addr);
-	unsigned int (*rx_clean)(struct phytmac_queue *queue, u32 cleaned_count);
 	void (*transmit)(struct phytmac_queue *queue);
 	void (*restart)(struct phytmac *pdata);
 	int (*tx_complete)(const struct phytmac_dma_desc *desc);
