827fc8b2df9c6db6a6a06988cc8be58ac5ab0021
Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>
2024-05-03 08:32:51 -0500

    ASoC: SOF: Intel: discard SoundWire configuration if HDaudio codec is reported
    mainline inclusion
    from mainline-v6.10-rc1
    
    The machine driver and topology selection starts with I2S, then
    SoundWire and last uses HDaudio as a fallback. That assumes that the
    ACPI information is correct but there are of course exceptions to the
    rule.
    
    On a Lenovo platform, an external HDaudio codec is detected, but the
    ACPI tables expose TWO RT711 jack codecs. This patch skips the
    SoundWire selection in case an external HDaudio codec is detected -
    which only works with the additional assumption that no one will mix
    HDaudio and SoundWire.
    
    Closes: https://github.com/thesofproject/linux/issues/4962
    Reviewed-by: Bard Liao <yung-chuan.liao@linux.intel.com>
    Reviewed-by: Péter Ujfalusi <peter.ujfalusi@linux.intel.com>
    Reviewed-by: Kai Vehmanen <kai.vehmanen@linux.intel.com>
    Reviewed-by: Ranjani Sridharan <ranjani.sridharan@linux.intel.com>
    Signed-off-by: Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>
    Link: https://lore.kernel.org/r/20240503133253.108201-2-pierre-louis.bossart@linux.intel.com
    Signed-off-by: Mark Brown <broonie@kernel.org>
    (cherry picked from commit 5a7543d0ca01d68d992f480d151efd693807e0ce)

--- sound/soc/sof/intel/hda.c
+++ sound/soc/sof/intel/hda.c
@@ -1666,6 +1666,7 @@ struct snd_soc_acpi_mach *hda_machine_select(struct snd_sof_dev *sdev)
 	u32 interface_mask = hda_get_interface_mask(sdev);
 	struct snd_sof_pdata *sof_pdata = sdev->pdata;
 	const struct sof_dev_desc *desc = sof_pdata->desc;
+	struct hdac_bus *bus = sof_to_bus(sdev);
 	struct snd_soc_acpi_mach *mach = NULL;
 	const char *tplg_filename;
 
@@ -1779,8 +1780,12 @@ struct snd_soc_acpi_mach *hda_machine_select(struct snd_sof_dev *sdev)
 		}
 	}
 
-	/* If I2S fails, try SoundWire if it is supported */
-	if (!mach && (interface_mask & BIT(SOF_DAI_INTEL_ALH)))
+	/*
+	 * If I2S fails and no external HDaudio codec is detected,
+	 * try SoundWire if it is supported
+	 */
+	if (!mach && !HDA_EXT_CODEC(bus->codec_mask) &&
+	    (interface_mask & BIT(SOF_DAI_INTEL_ALH)))
 		mach = hda_sdw_machine_select(sdev);
 
 	/*
