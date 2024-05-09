27fa80f4af7506c3a23ed5973b96bceedf90e753
wenlunpeng <wenlunpeng@uniontech.com>
2024-05-09 11:19:11 +0800

    printk: show build commitid in the front of dmesg
    This can help us find source code version when troubleshooting.
    
    Signed-off-by: wenlunpeng <wenlunpeng@uniontech.com>
    Signed-off-by: WangYuli <wangyuli@uniontech.com>

--- include/linux/printk.h
+++ include/linux/printk.h
@@ -11,6 +11,7 @@
 
 extern const char linux_banner[];
 extern const char linux_proc_banner[];
+extern const char linux_commitid_banner[];
 
 extern int oops_in_progress;	/* If set, an oops, panic(), BUG() or die() is in progress */
 
