cd4418c0ab8a36870532b1ce2b62a5e35cee4709
Liao Xuan <liaoxuan@hygon.cn>
2024-05-14 03:09:03 -0400

    ALSA: hda: Add support for Hygon family 18h model 5h HD-Audio
    Add the new PCI ID 0x1d94 0x14a9 for Hygon family 18h model 5h
    HDA controller.
    
    Signed-off-by: Liao Xuan <liaoxuan@hygon.cn>
    (cherry picked from commit f5910501cdb957d6c0c963943d1d508502edcd0a)
    Signed-off-by: Wentao Guan <guanwentao@uniontech.com>
    
    Conflicts:
    	include/linux/pci_ids.h
    	sound/hda/controllers/intel.c

--- sound/hda/controllers/intel.c
+++ sound/hda/controllers/intel.c
@@ -241,6 +241,7 @@ enum {
 	AZX_DRIVER_ZHAOXIN,
 	AZX_DRIVER_ZHAOXINHDMI,
 	AZX_DRIVER_LOONGSON,
+	AZX_DRIVER_HYGON,
 	AZX_DRIVER_GENERIC,
 	AZX_NUM_DRIVERS, /* keep this as last entry */
 };
@@ -360,6 +361,7 @@ static const char * const driver_short_names[] = {
 	[AZX_DRIVER_ZHAOXIN] = "HDA Zhaoxin",
 	[AZX_DRIVER_ZHAOXINHDMI] = "HDA Zhaoxin HDMI",
 	[AZX_DRIVER_LOONGSON] = "HDA Loongson",
+	[AZX_DRIVER_HYGON] = "HDA Hygon",
 	[AZX_DRIVER_GENERIC] = "HD-Audio Generic",
 };
 
@@ -2838,6 +2840,9 @@ static const struct pci_device_id azx_ids[] = {
 	  .driver_data = AZX_DRIVER_LOONGSON | AZX_DCAPS_NO_TCSEL },
 	{ PCI_VDEVICE(LOONGSON, PCI_DEVICE_ID_LOONGSON_HDMI),
 	  .driver_data = AZX_DRIVER_LOONGSON | AZX_DCAPS_NO_TCSEL },
+	/* Hygon HDAudio */
+	{ PCI_VDEVICE(HYGON, PCI_DEVICE_ID_HYGON_18H_M05H_HDA),
+	  .driver_data = AZX_DRIVER_HYGON | AZX_DCAPS_POSFIX_LPIB | AZX_DCAPS_NO_MSI },
 	{ 0, }
 };
 MODULE_DEVICE_TABLE(pci, azx_ids);
