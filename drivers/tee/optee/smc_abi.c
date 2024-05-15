be35c499930dc421c7bc1c0d3ecf138c4cde0b06
tianwei <tianwei@phytium.com.cn>
2024-05-15 08:51:12 +0000

    crypto: x86/sm2 -add Zhaoxin SM2 algorithm implementation
    zhaoxin inclusion
    category: feature
    
    -------------------
    
    Add support for SM2 (ShangMi 2) public key algorithm by Zhaoxin GMI
    Instruction. The purpose of this driver is to ensure that the application
    has both high performance and high security.
    
    Signed-off-by: leoliu-oc <leoliu-oc@zhaoxin.com>

--- drivers/tee/optee/smc_abi.c
+++ drivers/tee/optee/smc_abi.c
@@ -6,6 +6,7 @@
 
 #define pr_fmt(fmt) KBUILD_MODNAME ": " fmt
 
+#include <linux/acpi.h>
 #include <linux/arm-smccc.h>
 #include <linux/cpuhotplug.h>
 #include <linux/errno.h>
@@ -1424,6 +1425,14 @@ static void optee_smccc_hvc(unsigned long a0, unsigned long a1,
 	arm_smccc_hvc(a0, a1, a2, a3, a4, a5, a6, a7, res);
 }
 
+#if defined(CONFIG_OPTEE_DEFAULT_METHOD_HVC)
+#define DEFAULT_CONDUIT_METHOD optee_smccc_hvc
+#elif defined(CONFIG_OPTEE_DEFAULT_METHOD_SMC)
+#define DEFAULT_CONDUIT_METHOD optee_smccc_smc
+#else
+#define DEFAULT_CONDUIT_METHOD ERR_PTR(-ENXIO)
+#endif
+
 static optee_invoke_fn *get_invoke_func(struct device *dev)
 {
 	const char *method;
@@ -1432,7 +1441,7 @@ static optee_invoke_fn *get_invoke_func(struct device *dev)
 
 	if (device_property_read_string(dev, "method", &method)) {
 		pr_warn("missing \"method\" property\n");
-		return ERR_PTR(-ENXIO);
+		return DEFAULT_CONDUIT_METHOD;
 	}
 
 	if (!strcmp("hvc", method))
@@ -1820,6 +1829,14 @@ static const struct of_device_id optee_dt_match[] = {
 };
 MODULE_DEVICE_TABLE(of, optee_dt_match);
 
+#ifdef CONFIG_ACPI
+static const struct acpi_device_id optee_acpi_match[] = {
+	{ "PHYT8003" },
+	{ }
+};
+MODULE_DEVICE_TABLE(acpi, optee_acpi_match);
+#endif
+
 static struct platform_driver optee_driver = {
 	.probe  = optee_probe,
 	.remove = optee_smc_remove,
@@ -1827,6 +1844,7 @@ static struct platform_driver optee_driver = {
 	.driver = {
 		.name = "optee",
 		.of_match_table = optee_dt_match,
+		.acpi_match_table = ACPI_PTR(optee_acpi_match),
 	},
 };
 
