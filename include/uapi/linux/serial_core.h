a15567fee8ee9a52ffd83925fd3c7fe21a51fde1
xu yan <xuyan1481@phytium.com>
2024-05-15 16:00:48 +0800

    Phytium PCI serial port support
    This driver supports the Phytium UART controller on PCI/PCIe adapters.
    
    Signed-off-by: xu yan <xuyan1481@phytium.com>
    Signed-off-by: Chen Baozi <chenbaozi@phytium.com.cn>
    Signed-off-by: Lan Hengyu <lanhengyu1395@phytium.com.cn>
    Signed-off-by: Wang Yinfeng <wangyinfeng@phytium.com.cn>
    Link: https://github.com/deepin-community/kernel/pull/150
    (cherry picked from commit ff51061d59e6c875a8d49cfb3e842bf4d33d88f8)
    Signed-off-by: Wentao Guan <guanwentao@uniontech.com>
    
    Conflicts:
    	drivers/tty/serial/Makefile
    	include/uapi/linux/serial_core.h

--- include/uapi/linux/serial_core.h
+++ include/uapi/linux/serial_core.h
@@ -231,6 +231,9 @@
 /* Sunplus UART */
 #define PORT_SUNPLUS	123
 
+/* Phytium PCI UART */
+#define PORT_PHYTIUM	124
+
 /* Generic type identifier for ports which type is not important to userspace. */
 #define PORT_GENERIC	(-1)
 
