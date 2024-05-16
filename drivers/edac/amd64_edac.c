6af820e12045c9029da65b1d0b97dda20b9bd053
Liao Xuan <liaoxuan@hygon.cn>
2024-05-16 03:44:30 -0400

    EDAC/amd64: Add support for Hygon family 18h model 10h
    Add Hygon family 18h model 10h processor support for amd64_edac.
    
    Signed-off-by: Liao Xuan <liaoxuan@hygon.cn>

--- drivers/edac/amd64_edac.c
+++ drivers/edac/amd64_edac.c
@@ -4185,6 +4185,9 @@ static int per_family_init(struct amd64_pvt *pvt)
 		} else if (pvt->model == 0x7) {
 			pvt->ctl_name			= "F18h_M07h";
 			break;
+		} else if (pvt->model == 0x10) {
+			pvt->ctl_name			= "F18h_M10h";
+			break;
 		}
 		pvt->ctl_name				= "F18h";
 		break;
