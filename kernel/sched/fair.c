411e3f7301742dcf57b461ac9d03cc0c461e6ef0
Peter Zijlstra <peterz@infradead.org>
2024-05-23 11:00:10 +0200

    sched/fair: Assert {set_next,put_prev}_entity() are properly balanced
    mainline inclusion
    from mainline-v6.12-rc1
    category: feature
    note: patch series end in ("sched/eevdf: Fix se->slice being set to U64_MAX and resulting
    crash"), in middle may boot crash!
    
    Just a little sanity test..
    
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Reviewed-by: Valentin Schneider <vschneid@redhat.com>
    Tested-by: Valentin Schneider <vschneid@redhat.com>
    Link: https://lkml.kernel.org/r/20240727105029.486423066@infradead.org
    (cherry picked from commit e28b5f8bda01720b5ce8456b48cf4b963f9a80a1)
    Signed-off-by: Wentao Guan <guanwentao@uniontech.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -5411,6 +5411,7 @@ set_next_entity(struct cfs_rq *cfs_rq, struct sched_entity *se)
 	}
 
 	update_stats_curr_start(cfs_rq, se);
+	SCHED_WARN_ON(cfs_rq->curr);
 	cfs_rq->curr = se;
 
 	/*
@@ -5472,6 +5473,7 @@ static void put_prev_entity(struct cfs_rq *cfs_rq, struct sched_entity *prev)
 		/* in !on_rq case, update occurred at dequeue */
 		update_load_avg(cfs_rq, prev, 0);
 	}
+	SCHED_WARN_ON(cfs_rq->curr != prev);
 	cfs_rq->curr = NULL;
 }
 
