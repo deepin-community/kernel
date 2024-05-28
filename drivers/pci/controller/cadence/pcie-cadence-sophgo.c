c0fe5277aa97eb8e9ff441e8c6acf35b50ecb8de
Felix Yan <felixonmars@archlinux.org>
2024-05-28 08:43:41 +0300

    pcie: whitelist and support mellanox connectx-2
    Basic functionalities have been tested to work fine on pioneer board.

--- drivers/pci/controller/cadence/pcie-cadence-sophgo.c
+++ drivers/pci/controller/cadence/pcie-cadence-sophgo.c
@@ -465,6 +465,7 @@ struct vendor_id_list vendor_id_list[] = {
 	{"Inter I40E", 0x8086, 0x1572},
 	//{"WangXun RP1000", 0x8088},
 	{"Switchtec", 0x11f8,0x4052},
+	{"Mellanox ConnectX-2", 0x15b3, 0x6750}
 };
 
 size_t vendor_id_list_num = ARRAY_SIZE(vendor_id_list);
