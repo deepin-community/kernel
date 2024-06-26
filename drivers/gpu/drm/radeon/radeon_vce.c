0747c8279f26f113890f8c5b4e96fc53c5fa881c
Zheng Chongzhen <zhengchongzhen@wxiat.com>
2024-06-26 14:15:07 +0000

    sw64: amdgpu/radeon: correct low-level mmio memset/memcpy direct calls
    Driver codes of the direct calls, via the SIMD-optimized memset
    and memcpy functions, may raise default when using RX580/R7 under
    sw64 architecture, so work around 'memset' references to
    'memset_io' and 'memcpy' to 'memcpy_fromio/toio'.
    
    Signed-off-by: Zheng Chongzhen <zhengchongzhen@wxiat.com>
    Reviewed-by: He Sheng <hesheng@wxiat.com>
    Signed-off-by: Gu Zitao <guzitao@wxiat.com>
    (cherry picked from commit 530172fe6472a660709801ae19b5b72c9bcd59ab)

--- drivers/gpu/drm/radeon/radeon_vce.c
+++ drivers/gpu/drm/radeon/radeon_vce.c
@@ -238,7 +238,11 @@ int radeon_vce_resume(struct radeon_device *rdev)
 		return r;
 	}
 
+#if IS_ENABLED(CONFIG_SW64)
+	memset_io(cpu_addr, 0, radeon_bo_size(rdev->vce.vcpu_bo));
+#else
 	memset(cpu_addr, 0, radeon_bo_size(rdev->vce.vcpu_bo));
+#endif
 	if (rdev->family < CHIP_BONAIRE)
 		r = vce_v1_0_load_fw(rdev, cpu_addr);
 	else
