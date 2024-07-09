73781a8d304a45d31349ffe9dde049c466c1af5e
Gao Chen <gaochen@wxiat.com>
2024-07-09 09:54:32 +0800

    sw64: add virt_to_bus() and bus_to_virt()
    This patch add virt_to_bus() and bus_to_virt() for sw64.
    
    Signed-off-by: Gao Chen <gaochen@wxiat.com>

--- arch/sw_64/include/asm/io.h
+++ arch/sw_64/include/asm/io.h
@@ -94,32 +94,32 @@ extern void outsl(unsigned long port, const void *src, unsigned long count);
 #include <asm-generic/io.h>
 #undef PCI_IOBASE
 
-///*
-// * Change addresses as seen by the kernel (virtual) to addresses as
-// * seen by a device (bus), and vice versa.
-// *
-// * Note that this only works for a limited range of kernel addresses,
-// * and very well may not span all memory.  Consider this interface
-// * deprecated in favour of the DMA-mapping API.
-// */
-//static inline unsigned long __deprecated virt_to_bus(void *address)
-//{
-//	return virt_to_phys(address);
-//}
-//#define isa_virt_to_bus virt_to_bus
-//
-//static inline void * __deprecated bus_to_virt(unsigned long address)
-//{
-//	void *virt;
-//
-//	/* This check is a sanity check but also ensures that bus address 0
-//	 * maps to virtual address 0 which is useful to detect null pointers
-//	 * (the NCR driver is much simpler if NULL pointers are preserved).
-//	 */
-//	virt = phys_to_virt(address);
-//	return (long)address <= 0 ? NULL : virt;
-//}
-//#define isa_bus_to_virt bus_to_virt
+/*
+ * Change addresses as seen by the kernel (virtual) to addresses as
+ * seen by a device (bus), and vice versa.
+ *
+ * Note that this only works for a limited range of kernel addresses,
+ * and very well may not span all memory.  Consider this interface
+ * deprecated in favour of the DMA-mapping API.
+ */
+static inline unsigned long __deprecated virt_to_bus(void *address)
+{
+	return virt_to_phys(address);
+}
+#define isa_virt_to_bus virt_to_bus
+
+static inline void * __deprecated bus_to_virt(unsigned long address)
+{
+	void *virt;
+
+	/* This check is a sanity check but also ensures that bus address 0
+	 * maps to virtual address 0 which is useful to detect null pointers
+	 * (the NCR driver is much simpler if NULL pointers are preserved).
+	 */
+	virt = phys_to_virt(address);
+	return (long)address <= 0 ? NULL : virt;
+}
+#define isa_bus_to_virt bus_to_virt
 
 #endif /* __KERNEL__ */
 
