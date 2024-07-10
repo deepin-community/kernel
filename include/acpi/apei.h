d99ba912e08a452bb07aadca49384704133e1ddb
leoliu-oc <leoliu-oc@zhaoxin.com>
2024-07-10 16:35:20 +0800

    x86/mce/zhaoxin: Enable mcelog to decode PCIE, ZDI/ZPI, and DRAM errors
    zhaoxin inclusion
    category: bugfix
    
    -------------------
    
    The mcelog cannot decode PCIE, ZDI/ZPI, and DRAM errors in the FFM
    (Firmware First Mode).
    The purpose of this patch is to enable mcelog to decode PCIE, ZDI/ZPI, and
    DRAM errors that occur on Zhaoxin processors, so that the cause of these
    errors can be quickly located.
    
    Signed-off-by: leoliu-oc <leoliu-oc@zhaoxin.com>
    Link: https://github.com/deepin-community/kernel/pull/282
    (cherry picked from commit 743ad5c138a9b2b2aadce43ad7f26f77a691d7d3)
    Signed-off-by: Wentao Guan <guanwentao@uniontech.com>
    
    Conflicts:
    	drivers/acpi/apei/ghes.c

--- include/acpi/apei.h
+++ include/acpi/apei.h
@@ -52,6 +52,8 @@ int erst_clear(u64 record_id);
 
 int arch_apei_enable_cmcff(struct acpi_hest_header *hest_hdr, void *data);
 void arch_apei_report_mem_error(int sev, struct cper_sec_mem_err *mem_err);
+void arch_apei_report_pcie_error(int sev, struct cper_sec_pcie *pcie_err);
+void arch_apei_report_zdi_error(int sev, struct cper_sec_proc_generic *zdi_err);
 
 #endif
 #endif
