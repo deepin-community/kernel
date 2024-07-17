7dd4807e4e2c4fae018fc4f6ef1a2de65afe504e
Miao Wang <shankerwangmiao@gmail.com>
2024-07-17 09:03:32 +0800

    loongarch: basic boot support for legacy firmware
    The addresses passed from legacy firmware in efi system tables, the
    initrd table, the efi system memory mapping table are virtual addresses.
    This patch converts all that addresses back to physical addresses, to
    make sure all the following booting processes can run smoothly like on
    modern firmwares. The conversion happens unconditionally, since physical
    addresses passed in by modern firmwares remain unchanged after the
    conversion.
    
    In the EFI Stub, the mapping entries given by GetMemoryMap() on legacy
    firmwares contain virtual addresses in the phys_addr fields and 0x1xxxx
    addresses in the virt_addr fields, causing the later call to
    SetVirtualAddressMap() fails. This patch fixes this by correcting the
    addresses in the virt_addr fields. This patch detects the existence of
    the legacy firmware by reading DMW1 CSR, as done in the legacy loongarch
    GRUB port. Only if legacy firmwares detected, this correction happens.
    
    With this patch, the linux kernel is basically able to boot.
    
    Link: https://github.com/deepin-community/kernel/pull/356
    (cherry picked from commit ec43d52070cf3dbdbdb3368400ac321c151756da)
    Signed-off-by: Wentao Guan <guanwentao@uniontech.com>

--- drivers/firmware/efi/libstub/loongarch.c
+++ drivers/firmware/efi/libstub/loongarch.c
@@ -23,6 +23,8 @@ struct exit_boot_struct {
 	int			runtime_entry_count;
 };
 
+static int is_oldworld = 0;
+
 static efi_status_t exit_boot_func(struct efi_boot_memmap *map, void *priv)
 {
 	struct exit_boot_struct *p = priv;
@@ -36,6 +38,15 @@ static efi_status_t exit_boot_func(struct efi_boot_memmap *map, void *priv)
 			p->runtime_map, &p->runtime_entry_count);
 
 	return EFI_SUCCESS;
+}
+
+static void detect_oldworld(void)
+{
+	is_oldworld = !!(csr_read64(LOONGARCH_CSR_DMWIN1) & CSR_DMW1_PLV0);
+	efi_debug("is_oldworld: %d\n", is_oldworld);
+	if(is_oldworld) {
+		efi_info("Booting on OldWorld firmware\n");
+	}
 }
 
 unsigned long __weak kernel_entry_address(unsigned long kernel_addr,
@@ -53,6 +64,8 @@ efi_status_t efi_boot_kernel(void *handle, efi_loaded_image_t *image,
 	efi_status_t status;
 	u32 desc_ver;
 
+	detect_oldworld();
+
 	status = efi_alloc_virtmap(&priv.runtime_map, &desc_size, &desc_ver);
 	if (status != EFI_SUCCESS) {
 		efi_err("Unable to retrieve UEFI memory map.\n");
@@ -66,11 +79,16 @@ efi_status_t efi_boot_kernel(void *handle, efi_loaded_image_t *image,
 	if (status != EFI_SUCCESS)
 		return status;
 
+	if (is_oldworld) {
+		goto skip_set_virtual_address_map;
+	}
+
 	/* Install the new virtual address map */
 	efi_rt_call(set_virtual_address_map,
 		    priv.runtime_entry_count * desc_size, desc_size,
 		    desc_ver, priv.runtime_map);
 
+skip_set_virtual_address_map:
 	/* Config Direct Mapping */
 	csr_write64(CSR_DMW0_INIT, LOONGARCH_CSR_DMWIN0);
 	csr_write64(CSR_DMW1_INIT, LOONGARCH_CSR_DMWIN1);
