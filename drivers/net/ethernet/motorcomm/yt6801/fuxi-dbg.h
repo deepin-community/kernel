cc38a06e5a50300f8f4c844c1d86ddb3964b26a8
Yanteng Si <siyanteng@loongson.cn>
2024-07-17 18:25:37 +0800

    ethernet: Add motorcomm yt6801 support
    - The Asus XC-LS3A6M motherboard(Loongson 3A6000),CE720Z2,CE720Z... uses
      a yt6801 controller.
    - This patch introduces an out of tree module to provide support for this
      NIC. [^1]
    - Refactor module tree to make it work with Kconfig. [^2]
    - Drop unneeded installation script (yt_nic_install.sh).
    - Drop README. some day,we can write a yt6801.rst in kenenl doc.
    - enable YT6801 for x86 arm64 and loongarch as module
    - Refactor according to the kernel code style
    
    [^1]: Ref: https://www.motor-comm.com/Public/Uploads/uploadfile/files/20240104/yt6801-linux-driver-1.0.27.zip
    [^2]: CONFIG_NET_VENDOR_MOTORCOMM =(y)=> CONFIG_YT8601 (tristate).
    
    Co-authored-by: Mingcong Bai <jeffbai@aosc.io>
    Co-authored-by: Xiaotian Wu <wuxiaotian@loongson.cn>
    Signed-off-by: Yanteng Si <siyanteng@loongson.cn>
    (cherry picked from commit 21b918a62ff1b964cae2249fae41f1841b0e1536)
    Signed-off-by: Wentao Guan <guanwentao@uniontech.com>

---
+++ drivers/net/ethernet/motorcomm/yt6801/fuxi-dbg.h
@@ -0,0 +1,15 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/* Copyright (c) 2021 Motorcomm Corporation. */
+
+#ifndef _MP_DBG_H
+#define _MP_DBG_H
+
+/* Message verbosity: lower values indicate higher urgency */
+#define MP_OFF          0
+#define MP_ERROR        1
+#define MP_WARN         2
+#define MP_TRACE        3
+#define MP_INFO         4
+#define MP_LOUD         5
+
+#endif	/* _MP_DBG_H */
