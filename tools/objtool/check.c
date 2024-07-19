ceb917600771196d10aec6acff490b6c979e9f4a
Mingcong Bai <baimingcong@uniontech.com>
2024-07-19 13:41:46 +0800

    Revert "objtool: Check local label in read_unwind_hints()"
    This reverts commit d29378e405cff2799131e0d59fae4853787c4fd4.

--- tools/objtool/check.c
+++ tools/objtool/check.c
@@ -2211,7 +2211,6 @@ static int read_unwind_hints(struct objtool_file *file)
 	struct unwind_hint *hint;
 	struct instruction *insn;
 	struct reloc *reloc;
-	unsigned long offset;
 	int i;
 
 	sec = find_section_by_name(file->elf, ".discard.unwind_hints");
@@ -2239,16 +2238,7 @@ static int read_unwind_hints(struct objtool_file *file)
 			return -1;
 		}
 
-		if (reloc->sym->type == STT_SECTION) {
-			offset = reloc_addend(reloc);
-		} else if (reloc->sym->local_label) {
-			offset = reloc->sym->offset;
-		} else {
-			WARN("unexpected relocation symbol type in %s", sec->rsec->name);
-			return -1;
-		}
-
-		insn = find_insn(file, reloc->sym->sec, offset);
+		insn = find_insn(file, reloc->sym->sec, reloc_addend(reloc));
 		if (!insn) {
 			WARN("can't find insn for unwind_hints[%d]", i);
 			return -1;
