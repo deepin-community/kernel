d8114a4622474e79dc388cfc5475ccd5e7bc4932
Peter Zijlstra <peterz@infradead.org>
2024-08-14 00:25:52 +0200

    sched: Split up put_prev_task_balance()
    commit 260598f142c34811d226fdde5ab0346b48181439 upstream.
    
    With the goal of pushing put_prev_task() after pick_task() / into
    pick_next_task().
    
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Link: https://lore.kernel.org/r/20240813224015.943143811@infradead.org
    Signed-off-by: Xuchun Shang <xuchun.shang@linux.alibaba.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -5821,8 +5821,8 @@ static inline void schedule_debug(struct task_struct *prev, bool preempt)
 	schedstat_inc(this_rq()->sched_count);
 }
 
-static void put_prev_task_balance(struct rq *rq, struct task_struct *prev,
-				  struct rq_flags *rf)
+static void prev_balance(struct rq *rq, struct task_struct *prev,
+			 struct rq_flags *rf)
 {
 	const struct sched_class *start_class = prev->sched_class;
 	const struct sched_class *class;
@@ -5849,8 +5849,6 @@ static void put_prev_task_balance(struct rq *rq, struct task_struct *prev,
 		if (class->balance && class->balance(rq, prev, rf))
 			break;
 	}
-
-	put_prev_task(rq, prev);
 }
 
 /*
@@ -5888,7 +5886,8 @@ __pick_next_task(struct rq *rq, struct task_struct *prev, struct rq_flags *rf)
 	}
 
 restart:
-	put_prev_task_balance(rq, prev, rf);
+	prev_balance(rq, prev, rf);
+	put_prev_task(rq, prev);
 
 	for_each_active_class(class) {
 		p = class->pick_next_task(rq);
@@ -5993,7 +5992,8 @@ pick_next_task(struct rq *rq, struct task_struct *prev, struct rq_flags *rf)
 		goto out;
 	}
 
-	put_prev_task_balance(rq, prev, rf);
+	prev_balance(rq, prev, rf);
+	put_prev_task(rq, prev);
 
 	smt_mask = cpu_smt_mask(cpu);
 	need_sync = !!rq->core->core_cookie;
