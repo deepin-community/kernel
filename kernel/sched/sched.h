430e9e7e089035b7d847e160bb3f65d59cb209f6
Peter Zijlstra <peterz@infradead.org>
2024-08-14 00:25:56 +0200

    sched: Add put_prev_task(.next)
    commit b2d70222dbf2a2ff7a972a685d249a5d75afa87f upstream.
    
    In order to tell the previous sched_class what the next task is, add
    put_prev_task(.next).
    
    Notable SCX will use this to:
    
     1) determine the next task will leave the SCX sched class and push
        the current task to another CPU if possible.
     2) statistics on how often and which other classes preempt it
    
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Link: https://lore.kernel.org/r/20240813224016.367421076@infradead.org
    Signed-off-by: Xuchun Shang <xuchun.shang@linux.alibaba.com>

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -2371,7 +2371,7 @@ struct sched_class {
 	 */
 	struct task_struct *(*pick_next_task)(struct rq *rq, struct task_struct *prev);
 
-	void (*put_prev_task)(struct rq *rq, struct task_struct *p);
+	void (*put_prev_task)(struct rq *rq, struct task_struct *p, struct task_struct *next);
 	void (*set_next_task)(struct rq *rq, struct task_struct *p, bool first);
 
 	void (*switch_class)(struct rq *rq, struct task_struct *next);
@@ -2427,7 +2427,7 @@ struct sched_class {
 static inline void put_prev_task(struct rq *rq, struct task_struct *prev)
 {
 	WARN_ON_ONCE(rq->curr != prev);
-	prev->sched_class->put_prev_task(rq, prev);
+	prev->sched_class->put_prev_task(rq, prev, NULL);
 }
 
 static inline void set_next_task(struct rq *rq, struct task_struct *next)
@@ -2673,7 +2673,7 @@ static inline void put_prev_set_next_task(struct rq *rq,
 	if (next == prev)
 		return;
 
-	prev->sched_class->put_prev_task(rq, prev);
+	prev->sched_class->put_prev_task(rq, prev, next);
 	next->sched_class->set_next_task(rq, next, true);
 }
 
