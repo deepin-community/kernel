1a97f0dc6abe111310f89ffde1ee215729356acd
Bibo Mao <maobibo@loongson.cn>
2024-09-11 23:26:32 +0800

    LoongArch: KVM: Add Binary Translation extension support
    commit 59d8372bfc46 ("LoongArch: KVM: Add Binary Translation extension support")
    Conflict: none
    Backport-reason: Synchronize upstream linux loongarch kvm
    patch to support loongarch virtualization.
    Checkpatch: no, to be consistent with upstream commit.
    
    Loongson Binary Translation (LBT) is used to accelerate binary translation,
    which contains 4 scratch registers (scr0 to scr3), x86/ARM eflags (eflags)
    and x87 fpu stack pointer (ftop).
    
    Like FPU extension, here a lazy enabling method is used for LBT. the LBT
    context is saved/restored on the vcpu context switch path.
    
    Signed-off-by: Bibo Mao <maobibo@loongson.cn>
    Signed-off-by: Huacai Chen <chenhuacai@loongson.cn>
    Signed-off-by: Xianglai Li <lixianglai@loongson.cn>

--- arch/loongarch/kvm/exit.c
+++ arch/loongarch/kvm/exit.c
@@ -761,6 +761,14 @@ static int kvm_handle_lasx_disabled(struct kvm_vcpu *vcpu)
 	return RESUME_GUEST;
 }
 
+static int kvm_handle_lbt_disabled(struct kvm_vcpu *vcpu)
+{
+	if (kvm_own_lbt(vcpu))
+		kvm_queue_exception(vcpu, EXCCODE_INE, 0);
+
+	return RESUME_GUEST;
+}
+
 static int kvm_pv_send_ipi(struct kvm_vcpu *vcpu)
 {
 	unsigned long ipi_bitmap;
@@ -881,6 +889,7 @@ static exit_handle_fn kvm_fault_tables[EXCCODE_INT_START] = {
 	[EXCCODE_FPDIS]			= kvm_handle_fpu_disabled,
 	[EXCCODE_LSXDIS]		= kvm_handle_lsx_disabled,
 	[EXCCODE_LASXDIS]		= kvm_handle_lasx_disabled,
+	[EXCCODE_BTDIS]			= kvm_handle_lbt_disabled,
 	[EXCCODE_GSPR]			= kvm_handle_gspr,
 	[EXCCODE_HVC]			= kvm_handle_hypercall,
 };
