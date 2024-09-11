eefee1ede5b2393c2b9eb87a5ac70e065b5b24c2
Philo Lu <lulie@linux.alibaba.com>
2024-09-11 11:37:15 +0800

    bpf: Support __nullable argument suffix for tp_btf
    Pointers passed to tp_btf were trusted to be valid, but some tracepoints
    do take NULL pointer as input, such as trace_tcp_send_reset(). Then the
    invalid memory access cannot be detected by verifier.
    
    This patch fix it by add a suffix "__nullable" to the unreliable
    argument. The suffix is shown in btf, and PTR_MAYBE_NULL will be added
    to nullable arguments. Then users must check the pointer before use it.
    
    A problem here is that we use "btf_trace_##call" to search func_proto.
    As it is a typedef, argument names as well as the suffix are not
    recorded. To solve this, I use bpf_raw_event_map to find
    "__bpf_trace##template" from "btf_trace_##call", and then we can see the
    suffix.
    
    Suggested-by: Alexei Starovoitov <ast@kernel.org>
    Signed-off-by: Philo Lu <lulie@linux.alibaba.com>
    Signed-off-by: xulang <xulang@uniontech.com>

--- kernel/bpf/btf.c
+++ kernel/bpf/btf.c
@@ -5931,6 +5931,12 @@ static bool prog_args_trusted(const struct bpf_prog *prog)
 	}
 }
 
+static bool prog_arg_maybe_null(const struct bpf_prog *prog, const struct btf *btf,
+				const struct btf_param *arg)
+{
+	return btf_param_match_suffix(btf, arg, "__nullable");
+}
+
 bool btf_ctx_access(int off, int size, enum bpf_access_type type,
 		    const struct bpf_prog *prog,
 		    struct bpf_insn_access_aux *info)
@@ -6082,6 +6088,9 @@ bool btf_ctx_access(int off, int size, enum bpf_access_type type,
 	info->reg_type = PTR_TO_BTF_ID;
 	if (prog_args_trusted(prog))
 		info->reg_type |= PTR_TRUSTED;
+
+	if (prog_arg_maybe_null(prog, btf, &args[arg]))
+		info->reg_type |= PTR_MAYBE_NULL;
 
 	if (tgt_prog) {
 		enum bpf_prog_type tgt_type;
