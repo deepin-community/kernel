2756b6d3404bcf67806c079b476e82445e1f7c63
忘怀 <guanwentao@uniontech.com>
2024-09-16 23:46:39 +0800

    deepin: Use BFQ as the elevator for SQ devices
    For hdd optimize.
    Question: 
    1.How to test QUEUE_FLAG_NONROT?
    2.It is correct time to get the flag?？
    3.If we can get the flag,so we can only use BFQ elevator for hdd.
    
    Link:https://github.com/zen-kernel/zen-kernel/commit/c614dbbfd3480cf18c90fd51bb52abd53339b790

--- block/elevator.c
+++ block/elevator.c
@@ -575,8 +575,11 @@ static struct elevator_type *elevator_get_default(struct request_queue *q)
 	if (q->nr_hw_queues != 1 &&
 	    !blk_mq_is_shared_tags(q->tag_set->flags))
 		return NULL;
-
+#if defined(CONFIG_IOSCHED_BFQ)
+	return elevator_find_get(q, "bfq");
+#else
 	return elevator_find_get(q, "mq-deadline");
+#endif
 }
 
 /*
