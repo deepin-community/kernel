c1706c6131e15e3425423897643cdc3b977da540
Aboorva Devarajan <aboorvad@linux.ibm.com>
2024-10-26 00:20:20 +0530

    sched: Pass correct scheduling policy to __setscheduler_class
    commit 5db91545ef8150c45a526675ef99e8998b648a41 upstream.
    
    Commit 98442f0ccd82 ("sched: Fix delayed_dequeue vs
    switched_from_fair()") overlooked that __setscheduler_prio(), now
    __setscheduler_class() relies on p->policy for task_should_scx(), and
    moved the call before __setscheduler_params() updates it, causing it
    to be using the old p->policy value.
    
    Resolve this by changing task_should_scx() to take the policy itself
    instead of a task pointer, such that __sched_setscheduler() can pass
    in the updated policy.
    
    Fixes: 98442f0ccd82 ("sched: Fix delayed_dequeue vs switched_from_fair()")
    Signed-off-by: Aboorva Devarajan <aboorvad@linux.ibm.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Acked-by: Tejun Heo <tj@kernel.org>
    Signed-off-by: Xuchun Shang <xuchun.shang@linux.alibaba.com>

--- kernel/sched/ext.c
+++ kernel/sched/ext.c
@@ -4265,14 +4265,14 @@ static const struct kset_uevent_ops scx_uevent_ops = {
  * Used by sched_fork() and __setscheduler_class() to pick the matching
  * sched_class. dl/rt are already handled.
  */
-bool task_should_scx(struct task_struct *p)
+bool task_should_scx(int policy)
 {
 	if (!scx_enabled() ||
 	    unlikely(scx_ops_enable_state() == SCX_OPS_DISABLING))
 		return false;
 	if (READ_ONCE(scx_switching_all))
 		return true;
-	return p->policy == SCHED_EXT;
+	return policy == SCHED_EXT;
 }
 
 /**
@@ -4502,7 +4502,7 @@ static void scx_ops_disable_workfn(struct kthread_work *work)
 
 		sched_deq_and_put_task(p, DEQUEUE_SAVE | DEQUEUE_MOVE, &ctx);
 
-		p->sched_class = __setscheduler_class(p, p->prio);
+		p->sched_class = __setscheduler_class(p->policy, p->prio);
 		check_class_changing(task_rq(p), p, old_class);
 
 		sched_enq_and_set_task(&ctx);
@@ -5213,7 +5213,7 @@ static int scx_ops_enable(struct sched_ext_ops *ops)
 		sched_deq_and_put_task(p, DEQUEUE_SAVE | DEQUEUE_MOVE, &ctx);
 
 		p->scx.slice = SCX_SLICE_DFL;
-		p->sched_class = __setscheduler_class(p, p->prio);
+		p->sched_class = __setscheduler_class(p->policy, p->prio);
 		check_class_changing(task_rq(p), p, old_class);
 
 		sched_enq_and_set_task(&ctx);
