155044fd281a29786039c6d2019e7be163b220dd
WangYuli <wangyuli@uniontech.com>
2024-11-11 06:07:40 +0100

    ARM: 9435/1: ARM/nommu: Fix typo "absence"
    commit fb5af7d5405bf89a848819d1af007dfc73e9fb57 upstram.
    
    There is a spelling mistake of 'absense' in comments which
    should be 'absence'.
    
    Link: https://lore.kernel.org/all/fca25741-c89f-43ea-95af-5e3232d513fc@arm.com/
    Signed-off-by: WangYuli <wangyuli@uniontech.com>
    Reviewed-by: Vladimir Murzin <vladimir.murzin@arm.com>
    Signed-off-by: Russell King (Oracle) <rmk+kernel@armlinux.org.uk>
    Signed-off-by: WangYuli <wangyuli@uniontech.com>

--- arch/arm/mm/dma-mapping-nommu.c
+++ arch/arm/mm/dma-mapping-nommu.c
@@ -40,7 +40,7 @@ void arch_setup_dma_ops(struct device *dev, u64 dma_base, u64 size,
 		/*
 		 * Cache support for v7m is optional, so can be treated as
 		 * coherent if no cache has been detected. Note that it is not
-		 * enough to check if MPU is in use or not since in absense of
+		 * enough to check if MPU is in use or not since in absence of
 		 * MPU system memory map is used.
 		 */
 		dev->dma_coherent = cacheid ? coherent : true;
