acf8f84d911da110cf54958ecdc86006eac31ec7
Chih-Kang Chang <gary.chang@realtek.com>
2024-11-28 13:54:31 +0800

    wifi: rtw89: 8922a: use RSSI from PHY report in RX descriptor
    The PPDU status of probe response will fail to parse the IE due to being
    filtered by the to_self check. Therefore, we parse RSSI from PHY report in
    RX descriptor.
    
    Change-Id: If92768bd3330225476e66f7f8ae0a60572a39be3
    Signed-off-by: Chih-Kang Chang <gary.chang@realtek.com>
    Signed-off-by: Ping-Ke Shih <pkshih@realtek.com>
    Link: https://patch.msgid.link/20241128055433.11851-5-pkshih@realtek.com

--- drivers/net/wireless/realtek/rtw89/core.h
+++ drivers/net/wireless/realtek/rtw89/core.h
@@ -1137,6 +1137,7 @@ struct rtw89_rx_desc_info {
 	u16 offset;
 	u16 rxd_len;
 	bool ready;
+	u16 rssi;
 };
 
 struct rtw89_rxdesc_short {
@@ -1177,6 +1178,11 @@ struct rtw89_rxdesc_long_v2 {
 	__le32 dword7;
 	__le32 dword8;
 	__le32 dword9;
+} __packed;
+
+struct rtw89_rxdesc_phy_rpt_v2 {
+	__le32 dword0;
+	__le32 dword1;
 } __packed;
 
 struct rtw89_tx_desc_info {
@@ -3677,6 +3683,9 @@ struct rtw89_chip_ops {
 			   struct ieee80211_rx_status *status);
 	void (*convert_rpl_to_rssi)(struct rtw89_dev *rtwdev,
 				    struct rtw89_rx_phy_ppdu *phy_ppdu);
+	void (*phy_rpt_to_rssi)(struct rtw89_dev *rtwdev,
+				struct rtw89_rx_desc_info *desc_info,
+				struct ieee80211_rx_status *rx_status);
 	void (*ctrl_nbtg_bt_tx)(struct rtw89_dev *rtwdev, bool en,
 				enum rtw89_phy_idx phy_idx);
 	void (*cfg_txrx_path)(struct rtw89_dev *rtwdev);
@@ -6759,6 +6768,16 @@ static inline void rtw89_chip_convert_rpl_to_rssi(struct rtw89_dev *rtwdev,
 
 	if (chip->ops->convert_rpl_to_rssi)
 		chip->ops->convert_rpl_to_rssi(rtwdev, phy_ppdu);
+}
+
+static inline void rtw89_chip_phy_rpt_to_rssi(struct rtw89_dev *rtwdev,
+					      struct rtw89_rx_desc_info *desc_info,
+					      struct ieee80211_rx_status *rx_status)
+{
+	const struct rtw89_chip_info *chip = rtwdev->chip;
+
+	if (chip->ops->phy_rpt_to_rssi)
+		chip->ops->phy_rpt_to_rssi(rtwdev, desc_info, rx_status);
 }
 
 static inline void rtw89_ctrl_nbtg_bt_tx(struct rtw89_dev *rtwdev, bool en,
