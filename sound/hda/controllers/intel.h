86858d4871e34273c2b2b6dc76319ad07f9347aa
jasontao <jasontao@glenfly.com>
2024-12-03 15:35:29 +0800

    add gf hdaudio 001 patch in deepin kernel 6.6
    Signed-off-by: jasontao <jasontao@glenfly.com>
    Change-Id: Ief9a58e29ec73540da4d05e0fa79bf75e0f0da6f
    (cherry picked from commit b7e8bf15860fe2033100182dee657be2add75211)
    Signed-off-by: Wentao Guan <guanwentao@uniontech.com>
    
    Conflicts:
    	sound/hda/common/controller.c
    	sound/hda/controllers/intel.c
    	sound/pci/hda/patch_hdmi.c

--- sound/hda/controllers/intel.h
+++ sound/hda/controllers/intel.h
@@ -44,4 +44,15 @@ struct hda_intel_stream {
 #define azx_dev_to_istream(azx_dev) \
 	container_of(azx_dev, struct hda_intel_stream, azx_dev)
 
+struct gf_private {
+	struct hda_intel hda;
+
+	phys_addr_t diu_fb_stream_ofs[2];
+	void __iomem *diu_fb_stream_vaddr[2];
+	unsigned int diu_fb_stream_pos[2];
+
+	phys_addr_t diu_fb_bdl_ofs[2];
+	void __iomem *diu_fb_bdl_vaddr[2];
+};
+
 #endif
