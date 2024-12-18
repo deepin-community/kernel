fead39f1975267c161554b03642e476bd56e5f5c
Chen Ridong <chenridong@huawei.com>
2024-12-18 08:11:03 +0000

    deepin: KABI:cgroup: add more reserve kabi
    hulk inclusion
    category: feature
    bugzilla: https://gitee.com/openeuler/kernel/issues/I8SA3O
    
    --------------------------------
    
    Reserve KABI for future feature development.
    
    Signed-off-by: Chen Ridong <chenridong@huawei.com>
    Signed-off-by: yunjianshijie <dailingjia@uniontech.com>

--- kernel/cgroup/cpuset.c
+++ kernel/cgroup/cpuset.c
@@ -185,11 +185,6 @@ struct cpuset {
 
 	/* Handle for cpuset.cpus.partition */
 	struct cgroup_file partition_file;
-
-	DEEPIN_KABI_RESERVE(1)
-	DEEPIN_KABI_RESERVE(2)
-	DEEPIN_KABI_RESERVE(3)
-	DEEPIN_KABI_RESERVE(4)
 };
 
 /*
