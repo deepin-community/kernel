AOSC-Tracking/linux

AOSCOS: Optimize clear_page
Signed-off-by: chenj <chenj@lemote.com>
Signed-off-by: Kexy Biscuit <kexybiscuit@aosc.io>

arch/mips/mm/page.c
@@ -206,6 +206,11 @@ static void set_prefetch_parameters(void)
 		else if (cpu_has_cache_cdex_p)
 			cache_line_size = cpu_dcache_line_size();
 	}
+
+#ifdef CONFIG_CPU_LOONGSON3
+	clear_word_size = 16;
+#endif
+
 	/*
 	 * Too much unrolling will overflow the available space in
 	 * clear_space_array / copy_page_array.
@@ -220,11 +225,15 @@ static void set_prefetch_parameters(void)
 
 static void build_clear_store(u32 **buf, int off)
 {
+#ifdef CONFIG_CPU_LOONGSON3
+	uasm_i_gssq(buf, ZERO, ZERO, off, A0);
+#else
 	if (cpu_has_64bit_gp_regs || cpu_has_64bit_zero_reg) {
 		uasm_i_sd(buf, GPR_ZERO, off, GPR_A0);
 	} else {
 		uasm_i_sw(buf, GPR_ZERO, off, GPR_A0);
 	}
+#endif
 }
 
 static inline void build_clear_pref(u32 **buf, int off)

deepin-community/kernel

arch/mips/mm/page.c
@@ -218,6 +218,11 @@ static void set_prefetch_parameters(void)
 		else if (cpu_has_cache_cdex_p)
 			cache_line_size = cpu_dcache_line_size();
 	}
+
+#ifdef CONFIG_CPU_LOONGSON64
+	clear_word_size = 16;
+#endif
+
 	/*
 	 * Too much unrolling will overflow the available space in
 	 * clear_space_array / copy_page_array.
@@ -232,11 +237,15 @@ static void set_prefetch_parameters(void)
 
 static void build_clear_store(u32 **buf, int off)
 {
+#ifdef CONFIG_CPU_LOONGSON64
+	uasm_i_gssq(buf, ZERO, ZERO, off, A0);
+#else
 	if (cpu_has_64bit_gp_regs || cpu_has_64bit_zero_reg) {
 		uasm_i_sd(buf, ZERO, off, A0);
 	} else {
 		uasm_i_sw(buf, ZERO, off, A0);
 	}
+#endif
 }
 
 static inline void build_clear_pref(u32 **buf, int off)