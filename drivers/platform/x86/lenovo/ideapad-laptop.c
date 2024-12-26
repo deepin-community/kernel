1270a0e9ad7a040180bedbaab09961345993035b
Mingcong Bai <jeffbai@aosc.io>
2024-12-26 10:13:42 +0800

    DEEPIN: platform: ideapad-laptop: fix if branch syntax
    With "AOSCOS: platform: x86: ideapad-laptop: add fixes for ThinkBook 14+
    (2024 G6+)", an if branch guarding a no-data condition was introduced:
    
      if(!data)
      	acpi_handle_info(handle, "no data");
      	return;
    
    However, it was missing braces as it's a multi-line branch.
    
    Add a pair of braces to fix this syntax error.
    
    Fixes: "AOSCOS: platform: x86: ideapad-laptop: add fixes for ThinkBook 14+ (2024 G6+)"
    Signed-off-by: Mingcong Bai <jeffbai@aosc.io>
    (cherry picked from commit 9b3b8884c29b7670338c0e288e1f176253361986)
    Signed-off-by: Wentao Guan <guanwentao@uniontech.com>

--- drivers/platform/x86/lenovo/ideapad-laptop.c
+++ drivers/platform/x86/lenovo/ideapad-laptop.c
@@ -1884,9 +1884,10 @@ static void ideapad_acpi_notify(acpi_handle handle, u32 event, void *data)
 		acpi_handle_info(handle, "event: %lu\n",
 						(unsigned long)event);
 
-		if(!data)
+		if(!data) {
 			acpi_handle_info(handle, "no data");
 			return;
+		}
 
 		if (priv->suspended)
 			return;
