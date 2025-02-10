5d2a7094b2c9973a0c7b1a53dfaaadd9814da72f
Ge Yang <yangge1116@126.com>
2025-02-10 09:56:06 +0800

    mm/cma: using per-CMA locks to improve concurrent allocation performance
    mainline inclusion
    from mainline-v6.15-rc1
    category: feature
    CVE: NA
    
    ---------------------------
    
    commit 24ac6fb6e3647fff3646b3ea1811095441380560 upstream.
    
    For different CMAs, concurrent allocation of CMA memory ideally should not
    require synchronization using locks.  Currently, a global cma_mutex lock
    is employed to synchronize all CMA allocations, which can impact the
    performance of concurrent allocations across different CMAs.
    
    To test the performance impact, follow these steps:
    1. Boot the kernel with the command line argument hugetlb_cma=30G to
       allocate a 30GB CMA area specifically for huge page allocations. (note:
       on my machine, which has 3 nodes, each node is initialized with 10G of
       CMA)
    2. Use the dd command with parameters if=/dev/zero of=/dev/shm/file bs=1G
       count=30 to fully utilize the CMA area by writing zeroes to a file in
       /dev/shm.
    3. Open three terminals and execute the following commands simultaneously:
       (Note: Each of these commands attempts to allocate 10GB [2621440 * 4KB
       pages] of CMA memory.)
       On Terminal 1: time echo 2621440 > /sys/kernel/debug/cma/hugetlb1/alloc
       On Terminal 2: time echo 2621440 > /sys/kernel/debug/cma/hugetlb2/alloc
       On Terminal 3: time echo 2621440 > /sys/kernel/debug/cma/hugetlb3/alloc
    
    We attempt to allocate pages through the CMA debug interface and use the
    time command to measure the duration of each allocation.
    Performance comparison:
                 Without this patch      With this patch
    Terminal1        ~7s                     ~7s
    Terminal2       ~14s                     ~8s
    Terminal3       ~21s                     ~7s
    
    To solve problem above, we could use per-CMA locks to improve concurrent
    allocation performance.  This would allow each CMA to be managed
    independently, reducing the need for a global lock and thus improving
    scalability and performance.
    
    Link: https://lkml.kernel.org/r/1739152566-744-1-git-send-email-yangge1116@126.com
    Signed-off-by: Ge Yang <yangge1116@126.com>
    Reviewed-by: Barry Song <baohua@kernel.org>
    Acked-by: David Hildenbrand <david@redhat.com>
    Reviewed-by: Oscar Salvador <osalvador@suse.de>
    Cc: Aisheng Dong <aisheng.dong@nxp.com>
    Cc: Baolin Wang <baolin.wang@linux.alibaba.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- mm/cma.c
+++ mm/cma.c
@@ -41,7 +41,6 @@ static unsigned int cma_areas_size = MAX_CMA_AREAS;
 struct cma *cma_areas = cma_areas_data;
 
 unsigned cma_area_count;
-static DEFINE_MUTEX(cma_mutex);
 
 phys_addr_t cma_get_base(const struct cma *cma)
 {
@@ -124,6 +123,8 @@ static void __init cma_activate_area(struct cma *cma)
 		init_cma_reserved_pageblock(pfn_to_page(pfn));
 
 	spin_lock_init(&cma->lock);
+
+	mutex_init(&cma->alloc_mutex);
 
 #ifdef CONFIG_CMA_DEBUGFS
 	INIT_HLIST_HEAD(&cma->mem_head);
@@ -492,10 +493,10 @@ struct page *cma_alloc(struct cma *cma, unsigned long count,
 		spin_unlock_irq(&cma->lock);
 
 		pfn = cma->base_pfn + (bitmap_no << cma->order_per_bit);
-		mutex_lock(&cma_mutex);
+		mutex_lock(&cma->alloc_mutex);
 		ret = alloc_contig_range(pfn, pfn + count, MIGRATE_CMA,
 				     GFP_KERNEL | (no_warn ? __GFP_NOWARN : 0));
-		mutex_unlock(&cma_mutex);
+		mutex_unlock(&cma->alloc_mutex);
 		if (ret == 0) {
 			page = pfn_to_page(pfn);
 			break;
