7eefbb588e2a632d260983b1952d04806242afe9
Elizabeth Figura <zfigura@codeweavers.com>
2025-02-20 13:23:34 -0600

    ntsync: Check wait count based on byte size.
    [ Upstream commit 92527e473911b835c2c18b0c55c337c33e85ff00 ]
    
    GCC versions below 13 incorrectly detect the copy size as being static and too
    small to fit in the "fds" array. Work around this by explicitly calculating the
    size and returning EINVAL based on that, instead of based on the object count.
    
    Reported-by: kernel test robot <lkp@intel.com>
    Closes: https://lore.kernel.org/oe-kbuild-all/202502072019.LYoCR9bF-lkp@intel.com/
    Suggested-by: Arnd Bergmann <arnd@arndb.de>
    Signed-off-by: Elizabeth Figura <zfigura@codeweavers.com>
    
    --
    
    Suggested-by as per Arnd's request, but the only thing I changed was preserving
    array_size() [as noted by Geert in the linked thread]. I tested and found no
    regressions.
    
    v2: Add missing sign-off
    Link: https://lore.kernel.org/r/20250220192334.549167-1-zfigura@codeweavers.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    [ Backport from v6.14 ]
    Signed-off-by: WangYuli <wangyuli@uniontech.com>

--- drivers/misc/ntsync.c
+++ drivers/misc/ntsync.c
@@ -874,6 +874,7 @@ static int setup_wait(struct ntsync_device *dev,
 {
 	int fds[NTSYNC_MAX_WAIT_COUNT + 1];
 	const __u32 count = args->count;
+	size_t size = array_size(count, sizeof(fds[0]));
 	struct ntsync_q *q;
 	__u32 total_count;
 	__u32 i, j;
@@ -881,15 +882,14 @@ static int setup_wait(struct ntsync_device *dev,
 	if (args->pad || (args->flags & ~NTSYNC_WAIT_REALTIME))
 		return -EINVAL;
 
-	if (args->count > NTSYNC_MAX_WAIT_COUNT)
+	if (size >= sizeof(fds))
 		return -EINVAL;
 
 	total_count = count;
 	if (args->alert)
 		total_count++;
 
-	if (copy_from_user(fds, u64_to_user_ptr(args->objs),
-			   array_size(count, sizeof(*fds))))
+	if (copy_from_user(fds, u64_to_user_ptr(args->objs), size))
 		return -EFAULT;
 	if (args->alert)
 		fds[count] = args->alert;
