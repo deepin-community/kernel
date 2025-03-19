0513dd61cb4633a003c6cd39e713fbd739a97401
Marc Herbert <Marc.Herbert@linux.intel.com>
2025-03-19 06:00:30 +0000

    mm/hugetlb: move hugetlb_sysctl_init() to the __init section
    commit 1ca77ff1837249701053a7fcbdedabc41f4ae67c upstream.
    
    hugetlb_sysctl_init() is only invoked once by an __init function and is
    merely a wrapper around another __init function so there is not reason to
    keep it.
    
    Fixes the following warning when toning down some GCC inline options:
    
     WARNING: modpost: vmlinux: section mismatch in reference:
       hugetlb_sysctl_init+0x1b (section: .text) ->
         __register_sysctl_init (section: .init.text)
    
    Link: https://lkml.kernel.org/r/20250319060041.2737320-1-marc.herbert@linux.intel.com
    Signed-off-by: Marc Herbert <Marc.Herbert@linux.intel.com>
    Reviewed-by: Anshuman Khandual <anshuman.khandual@arm.com>
    Reviewed-by: Muchun Song <muchun.song@linux.dev>
    Cc: <stable@vger.kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    (cherry picked from commit b5681a8b997eccc6a6db0eb8dc8f5b9fcf18196d)

--- mm/hugetlb.c
+++ mm/hugetlb.c
@@ -4745,7 +4745,7 @@ static struct ctl_table hugetlb_table[] = {
 	{ }
 };
 
-static void hugetlb_sysctl_init(void)
+static void __init hugetlb_sysctl_init(void)
 {
 	register_sysctl_init("vm", hugetlb_table);
 }
