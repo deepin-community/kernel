610539adddcd1294509ddd4288f1cde62eee70cb
Hu Yeqing <huyeqing2074@phytium.com.cn>
2025-04-28 09:16:50 +0800

    PCI: ep: Add phytium pcie ep driver version message
    Add phytium pcie ep driver version message
    
    Signed-off-by: Xiao Cong <xiaocong1866@phytium.com.cn>
    Signed-off-by: Wang Yinfeng <wangyinfeng@phytium.com.cn>
    Signed-off-by: Hu Yeqing <huyeqing2074@phytium.com.cn>

--- drivers/pci/controller/pcie-phytium-ep.c
+++ drivers/pci/controller/pcie-phytium-ep.c
@@ -19,6 +19,8 @@
 #include "pcie-phytium-ep.h"
 #include "pcie-phytium-register.h"
 
+#define PHYTIUM_PCIE_RP_DRIVER_VERSION		"1.1.1"
+
 #define PHYTIUM_PCIE_EP_IRQ_PCI_ADDR_NONE	0x0
 #define PHYTIUM_PCIE_EP_IRQ_PCI_ADDR_LEGACY	0x1
 
@@ -462,9 +464,10 @@ static struct platform_driver phytium_pcie_ep_driver = {
 	.probe = phytium_pcie_ep_probe,
 	.remove = phytium_pcie_ep_remove,
 };
-
+MODULE_DEVICE_TABLE(of, phytium_pcie_ep_of_match);
 module_platform_driver(phytium_pcie_ep_driver);
 
 MODULE_LICENSE("GPL");
+MODULE_VERSION(PHYTIUM_PCIE_RP_DRIVER_VERSION);
 MODULE_AUTHOR("Yang Xun <yangxun@phytium.com.cn>");
 MODULE_DESCRIPTION("Phytium PCIe Controller Endpoint driver");
