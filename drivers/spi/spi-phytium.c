6057104c3a871ba3000f42624766d47472471e8d
Cui Fulong <cuifulong2112@phytium.com.cn>
2025-04-28 10:18:16 +0800

    spi: phytium: Bugfix phytium_spi_irq panic on boot
    The root cause is that irq is triggered between request_irq
    and spi_master_set_devdata.
    
    Signed-off-by: Cui Fulong <cuifulong2112@phytium.com.cn>
    Signed-off-by: Liu Dalin <liudalin@kylinsec.com.cn>
    Signed-off-by: Peng Min <pengmin1540@phytium.com.cn>
    Signed-off-by: Wang Yinfeng <wangyinfeng@phytium.com.cn>

--- drivers/spi/spi-phytium.c
+++ drivers/spi/spi-phytium.c
@@ -420,8 +420,6 @@ int phytium_spi_add_host(struct device *dev, struct phytium_spi *fts)
 	master->dev.fwnode = dev->fwnode;
 	master->flags = SPI_CONTROLLER_GPIO_SS;
 
-	spi_hw_init(dev, fts);
-
 	if (fts->dma_ops && fts->dma_ops->dma_init) {
 		ret = fts->dma_ops->dma_init(dev, fts);
 		if (ret) {
@@ -433,6 +431,8 @@ int phytium_spi_add_host(struct device *dev, struct phytium_spi *fts)
 	}
 
 	spi_master_set_devdata(master, fts);
+
+	spi_hw_init(dev, fts);
 
 	ret = request_irq(fts->irq, phytium_spi_irq, IRQF_SHARED,
 			fts->name, master);
