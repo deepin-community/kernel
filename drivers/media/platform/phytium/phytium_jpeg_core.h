944bb80308927424a825fabadc027ca92704dd67
Li Guohui <liguohui2061@phytium.com.cn>
2025-04-29 11:09:54 +0800

    media: phytium-jpeg: Fixed some JPEG driver issues
    The commit fixed the below issues.
    1) Intialize the driver first version.
    2) Supprot the feature of set the sampling mode, such as
    V4L2_JPEG_CHROMA_SUBSAMPLING_420,V4L2_JPEG_CHROMA_SUBSAMPLING_444
    and other modes.
    3) Adjust the order of altering resolution procedures.
    
    Signed-off-by: Li Guohui <liguohui2061@phytium.com.cn>
    Signed-off-by: Wang Min <wangmin@phytium.com.cn>
    Signed-off-by: Wang Yinfeng <wangyinfeng@phytium.com.cn>

--- drivers/media/platform/phytium/phytium_jpeg_core.h
+++ drivers/media/platform/phytium/phytium_jpeg_core.h
@@ -36,6 +36,7 @@
 #include <media/videobuf2-dma-contig.h>
 
 #define PHYTIUM_JPEG_NAME       "phytium-jpeg"
+#define JPEG_DRIVER_VERSION     "1.0.0"
 #define MAX_FRAME_RATE          60
 #define MAX_HEIGHT              1080
 #define MAX_WIDTH               1920
@@ -45,7 +46,7 @@
 #define MAX_PIXEL_CLOCK         (1920 * 1080 * 60)   /* 1920 x 1080 x 60Hz */
 
 #define SOURCE_RESOLUTION_DETECT_TIMEOUT    msecs_to_jiffies(500)
-#define RESOLUTION_CHANGE_DELAY             msecs_to_jiffies(0)
+#define RESOLUTION_CHANGE_DELAY             msecs_to_jiffies(250)
 #define INVALID_RESOLUTION_DELAY            msecs_to_jiffies(250)
 #define STOP_TIMEOUT                        msecs_to_jiffies(1000)
 
@@ -128,6 +129,7 @@ struct phytium_jpeg_dev {
 	unsigned int frame_rate;
 	void __iomem *timer30_addr;
 	void __iomem *timer31_addr;
+	struct v4l2_ctrl_handler ctrl_handler;
 };
 
 struct phytium_jpeg_config {
