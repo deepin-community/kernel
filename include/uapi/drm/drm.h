4f0879ef37d634fdbbbd09c471570ba3c99bdd2d
WangYuli <wangyuli@uniontech.com>
2025-05-13 13:40:02 +0800

    drm/uapi: Fix typo "restictions"
    There is a spelling mistake of 'restictions' in comments which
    should be 'restrictions'.
    
    This typo was not listed in scripts/spelling.txt, thus it was more
    difficult to detect. Add it for convenience.
    
    This typo is reported by GitHub Copilot code review. [1]
    
    [1]. https://github.com/deepin-community/kernel/pull/796
    
    Signed-off-by: WangYuli <wangyuli@uniontech.com>
    Signed-off-by: WangYuli <wangyuli@aosc.io>

--- include/uapi/drm/drm.h
+++ include/uapi/drm/drm.h
@@ -852,7 +852,7 @@ struct drm_get_cap {
  * cursor and have correctly set hotspot properties.
  * If this client cap is not set the DRM core will hide cursor plane on
  * those virtualized drivers because not setting it implies that the
- * client is not capable of dealing with those extra restictions.
+ * client is not capable of dealing with those extra restrictions.
  * Clients which do set cursor hotspot and treat the cursor plane
  * like a mouse cursor should set this property.
  * The client must enable &DRM_CLIENT_CAP_ATOMIC first.
