8b0185da4ed959a6727be581898e1e986b4a5880
Dong Yibo <dong100@mucse.com>
2025-05-19 10:57:45 +0800

    eth: mucse: Fix rx PAGE error in csum handler
    Drivers re-use half-page (may be used but not freed) to hardware
    by mistake, this may cause mutiple skbs use one physical address.
    
    Signed-off-by: Dong Yibo <dong100@mucse.com>

--- drivers/net/ethernet/mucse/rnpgbevf/rnpgbevf_main.c
+++ drivers/net/ethernet/mucse/rnpgbevf/rnpgbevf_main.c
@@ -891,14 +891,6 @@ skip_fix:
 	if (err) {
 		u32 ntc = rx_ring->next_to_clean + 1;
 		struct rnpgbevf_rx_buffer *rx_buffer;
-#if (PAGE_SIZE < 8192)
-		unsigned int truesize = rnpgbevf_rx_pg_size(rx_ring) / 2;
-#else
-		unsigned int truesize =
-			ring_uses_build_skb(rx_ring) ?
-				SKB_DATA_ALIGN(RNPVF_SKB_PAD + size) :
-				SKB_DATA_ALIGN(size);
-#endif
 
 		if (likely(rnpgbevf_test_staterr(rx_desc, RNPGBE_RXD_STAT_EOP)))
 			*driver_drop_packets = *driver_drop_packets + 1;
@@ -911,11 +903,6 @@ skip_fix:
 		/* we should clean it since we used all info in it */
 		rx_desc->wb.cmd = 0;
 
-#if (PAGE_SIZE < 8192)
-		rx_buffer->page_offset ^= truesize;
-#else
-		rx_buffer->page_offset += truesize;
-#endif
 #ifdef OPTM_WITH_LPAGE
 		rnpgbevf_put_rx_buffer(rx_ring, rx_buffer);
 #else
