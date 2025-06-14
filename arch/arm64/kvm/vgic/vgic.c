257876c9feea42f77e50c758c492d60134d37d3d
Steven Price <steven.price@arm.com>
2025-06-14 15:32:57 +0800

    KVM: arm64: vgic: Provide helper for number of list registers
    community inclusion
    category: feature
    bugzilla: https://gitee.com/openeuler/kernel/issues/IBY08N
    Reference: https://lore.kernel.org/kvm/20250416134208.383984-1-steven.price@arm.com/T/
    
    --------------------------------
    
    Currently the number of list registers available is stored in a global
    (kvm_vgic_global_state.nr_lr). With Arm CCA the RMM is permitted to
    reserve list registers for its own use and so the number of available
    list registers can be fewer for a realm VM. Provide a wrapper function
    to fetch the global in preparation for restricting nr_lr when dealing
    with a realm VM.
    
    Signed-off-by: Steven Price <steven.price@arm.com>
    Reviewed-by: Gavin Shan <gshan@redhat.com>
    Reviewed-by: Suzuki K Poulose <suzuki.poulose@arm.com>
    Signed-off-by: Xu Raoqing <xuraoqing@huawei.com>
    Signed-off-by: WangYuli <wangyl5933@chinaunicom.cn>

--- arch/arm64/kvm/vgic/vgic.c
+++ arch/arm64/kvm/vgic/vgic.c
@@ -21,6 +21,11 @@ struct vgic_global kvm_vgic_global_state __ro_after_init = {
 	.gicv3_cpuif = STATIC_KEY_FALSE_INIT,
 };
 
+static inline int kvm_vcpu_vgic_nr_lr(struct kvm_vcpu *vcpu)
+{
+	return kvm_vgic_global_state.nr_lr;
+}
+
 /*
  * Locking order is always:
  * kvm->lock (mutex)
@@ -808,7 +813,7 @@ static void vgic_flush_lr_state(struct kvm_vcpu *vcpu)
 	lockdep_assert_held(&vgic_cpu->ap_list_lock);
 
 	count = compute_ap_list_depth(vcpu, &multi_sgi);
-	if (count > kvm_vgic_global_state.nr_lr || multi_sgi)
+	if (count > kvm_vcpu_vgic_nr_lr(vcpu) || multi_sgi)
 		vgic_sort_ap_list(vcpu);
 
 	count = 0;
@@ -837,7 +842,7 @@ static void vgic_flush_lr_state(struct kvm_vcpu *vcpu)
 
 		raw_spin_unlock(&irq->irq_lock);
 
-		if (count == kvm_vgic_global_state.nr_lr) {
+		if (count == kvm_vcpu_vgic_nr_lr(vcpu)) {
 			if (!list_is_last(&irq->ap_list,
 					  &vgic_cpu->ap_list_head))
 				vgic_set_underflow(vcpu);
@@ -846,7 +851,7 @@ static void vgic_flush_lr_state(struct kvm_vcpu *vcpu)
 	}
 
 	/* Nuke remaining LRs */
-	for (i = count ; i < kvm_vgic_global_state.nr_lr; i++)
+	for (i = count ; i < kvm_vcpu_vgic_nr_lr(vcpu); i++)
 		vgic_clear_lr(vcpu, i);
 
 	if (!static_branch_unlikely(&kvm_vgic_global_state.gicv3_cpuif))
