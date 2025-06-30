5ac79ad516f70922765975993efa9d0a5637ff3b
Min Fanlei <minfanlei@wxiat.com>
2025-06-30 16:15:44 +0800

    sw64: kvm: fix perf kvm support
    This patch:
     - Register callback for monitoring guest info to enable perf kvm
       correctly.
     - For core4, we determine the running mode of the vcpu through
       the CPU mode bit of the PC rather than PS.
    
    Signed-off-by: Min Fanlei <minfanlei@wxiat.com>
    Reviewed-by: He Sheng <hesheng@wxiat.com>
    Signed-off-by: Gu Zitao <guzitao@wxiat.com>

--- arch/sw_64/kvm/perf.c
+++ arch/sw_64/kvm/perf.c
@@ -11,17 +11,14 @@
 
 bool kvm_arch_vcpu_in_kernel(struct kvm_vcpu *vcpu)
 {
-	return (vcpu->arch.regs.ps & 8) != 0;
+	if (IS_ENABLED(CONFIG_SUBARCH_C3B))
+		return (vcpu->arch.regs.ps & 0x8) == 0;
+	else
+		return (vcpu->arch.regs.pc & 0x3) == 0x2;
+
 }
 
 unsigned long kvm_arch_vcpu_get_ip(struct kvm_vcpu *vcpu)
 {
 	return vcpu->arch.regs.pc;
-}
-
-
-
-static inline bool kvm_arch_pmi_in_guest(struct kvm_vcpu *vcpu)
-{
-	return IS_ENABLED(CONFIG_GUEST_PERF_EVENTS) && !!vcpu;
 }
