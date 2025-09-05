77cc4c207d8aba3e6c0354ba6b7ece4bf1303908
wenxue.ding <Wenxue.Ding@cixtech.com>
2025-09-05 14:59:45 +0800

    CIX: Adaption SCMI protocols to support ACPI.
    This patchset extends the CIX clock and SCMI framework to support
    ACPI-based platforms.
    
    Signed-off-by: "wenxue.ding" <Wenxue.Ding@cixtech.com>

--- include/linux/mailbox_controller.h
+++ include/linux/mailbox_controller.h
@@ -67,6 +67,8 @@ struct mbox_chan_ops {
  * @txpoll_period:	If 'txdone_poll' is in effect, the API polls for
  *			last TX's status after these many millisecs
  * @of_xlate:		Controller driver specific mapping of channel via DT
+ * @fwnode_xlate:	Controller driver specific mapping index to channel
+ *			via fwnode support DT&ACPI
  * @poll_hrt:		API private. hrtimer used to poll for TXDONE on all
  *			channels.
  * @node:		API private. To hook into list of controllers.
@@ -81,6 +83,8 @@ struct mbox_controller {
 	unsigned txpoll_period;
 	struct mbox_chan *(*of_xlate)(struct mbox_controller *mbox,
 				      const struct of_phandle_args *sp);
+	struct mbox_chan *(*fwnode_xlate)(struct mbox_controller *mbox,
+					const struct fwnode_reference_args *fwnode_args);
 	/* Internal to API */
 	struct hrtimer poll_hrt;
 	spinlock_t poll_hrt_lock;
