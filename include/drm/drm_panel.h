f14858b176c858cb53ec701e8791206f1e63073e
wenxue.ding <Wenxue.Ding@cixtech.com>
2025-09-12 10:29:24 +0800

    CIX: Add a fwonde interface for ACPI to find panel.
    This patch extends the DRM panel framework with a new fwnode-based
    lookup interface.
    
    Signed-off-by: wenxue.ding <Wenxue.Ding@cixtech.com>

--- include/drm/drm_panel.h
+++ include/drm/drm_panel.h
@@ -284,6 +284,8 @@ int drm_panel_disable(struct drm_panel *panel);
 
 int drm_panel_get_modes(struct drm_panel *panel, struct drm_connector *connector);
 
+struct drm_panel *fwnode_drm_find_panel(const struct fwnode_handle *fwnode);
+
 #if defined(CONFIG_OF) && defined(CONFIG_DRM_PANEL)
 struct drm_panel *of_drm_find_panel(const struct device_node *np);
 int of_drm_get_panel_orientation(const struct device_node *np,
