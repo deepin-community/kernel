09c9253ebaa7c0f240b084e3827d9cc2a0a0844b
leoliu-oc <leoliu-oc@zhaoxin.com>
2025-09-25 19:32:36 +0800

    ALSA: HDA: Roll back the old version for consistency with the mainline
    zhaoxin inclusion
    category: feature
    
    --------------------
    
    The official version has already been merged into the mainline. Therefore, in order to ensure the consistency of the HDAC code with the mainline, it is necessary to roll back the current version so as to merge the mainline version subsequently.
    
    Signed-off-by: leoliu-oc <leoliu-oc@zhaoxin.com>

--- sound/pci/hda/hda_controller.h
+++ sound/pci/hda/hda_controller.h
@@ -48,7 +48,6 @@
 #define AZX_DCAPS_CORBRP_SELF_CLEAR (1 << 28)	/* CORBRP clears itself after reset */
 #define AZX_DCAPS_NO_MSI64      (1 << 29)	/* Stick to 32-bit MSIs */
 #define AZX_DCAPS_SEPARATE_STREAM_TAG	(1 << 30) /* capture and playback use separate stream tag */
-#define AZX_DCAPS_RIRB_PRE_DELAY	(1 << 31) /* Put a delay before read */
 
 enum {
 	AZX_SNOOP_TYPE_NONE,
@@ -146,8 +145,6 @@ struct azx {
 	unsigned int align_buffer_size:1;
 	unsigned int disabled:1; /* disabled by vga_switcheroo */
 	unsigned int pm_prepared:1;
-
-	void __iomem *remap_diu_addr;
 
 	/* GTS present */
 	unsigned int gts_present:1;
