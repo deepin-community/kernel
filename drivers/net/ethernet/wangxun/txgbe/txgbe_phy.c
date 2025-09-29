61a4c0bca97748580ac509af6371e683bacfd72a
Duanqiang Wen <duanqiangwen@net-swift.com>
2025-09-29 14:11:41 +0800

    deepin: net: txgbe: fix i2c issues
    wangxun inclusion
    category: feature
    
    In txgbe, when ethtool -m port will return -EINVAL with DAC
    because not record i2c info when identify sfp. And If repeatly
    reinit_locked, when txgbe_down(), may read i2c error in record
    i2c info flow. So skip it when port down.
    
    Signed-off-by: Duanqiang Wen <duanqiangwen@net-swift.com>

--- drivers/net/ethernet/wangxun/txgbe/txgbe_phy.c
+++ drivers/net/ethernet/wangxun/txgbe/txgbe_phy.c
@@ -815,7 +815,7 @@ s32 txgbe_identify_sfp_module(struct txgbe_hw *hw)
 		if (cable_tech & (TXGBE_SFF_DA_PASSIVE_CABLE |
 		    TXGBE_SFF_DA_ACTIVE_CABLE)) {
 			status = 0;
-			goto out;
+			goto sp_record;
 		}
 
 		/* Verify supported 1G SFP modules */
@@ -831,50 +831,53 @@ s32 txgbe_identify_sfp_module(struct txgbe_hw *hw)
 			goto out;
 		}
 	}
-	/*record eeprom info*/
-	status = hw->phy.ops.read_i2c_eeprom(hw,
-		       TXGBE_SFF_SFF_8472_COMP,
-		       &sff8472_rev);
-	if (status != 0)
-		goto err_read_i2c_eeprom;
-
-	/* addressing mode is not supported */
-	status = hw->phy.ops.read_i2c_eeprom(hw,
-					     TXGBE_SFF_SFF_8472_SWAP,
-					     &addr_mode);
-	if (status != 0)
-		goto err_read_i2c_eeprom;
-
-	if (addr_mode & TXGBE_SFF_ADDRESSING_MODE) {
-		e_err(drv, "Address change required to access page 0xA2,");
-		e_err(drv, "but not supported. Please report the module type to the driver maintainers.\n");
-		page_swap = true;
-	}
-
-	if (sff8472_rev == TXGBE_SFF_SFF_8472_UNSUP || page_swap ||
-	    !(addr_mode & TXGBE_SFF_DDM_IMPLEMENTED)) {
-		/* We have a SFP, but it does not support SFF-8472 */
-		adapter->eeprom_type = ETH_MODULE_SFF_8079;
-		adapter->eeprom_len = ETH_MODULE_SFF_8079_LEN;
-	} else {
-		/* We have a SFP which supports a revision of SFF-8472. */
-		adapter->eeprom_type = ETH_MODULE_SFF_8472;
-		adapter->eeprom_len = ETH_MODULE_SFF_8472_LEN;
-	}
-	for (i = 0; i < adapter->eeprom_len; i++) {
-		if (i < ETH_MODULE_SFF_8079_LEN)
-			status = hw->phy.ops.read_i2c_eeprom(hw, i,
-				       &databyte);
-		else
-			status = hw->phy.ops.read_i2c_sff8472(hw, i,
-				       &databyte);
-
+sp_record:
+	if (hw->mac.type == txgbe_mac_sp &&
+	    !test_bit(__TXGBE_DOWN, &adapter->state)) {
+		/*record eeprom info*/
+		status = hw->phy.ops.read_i2c_eeprom(hw,
+				TXGBE_SFF_SFF_8472_COMP,
+				&sff8472_rev);
 		if (status != 0)
 			goto err_read_i2c_eeprom;
 
-		adapter->i2c_eeprom[i] = databyte;
-	}
+		/* addressing mode is not supported */
+		status = hw->phy.ops.read_i2c_eeprom(hw,
+							TXGBE_SFF_SFF_8472_SWAP,
+							&addr_mode);
+		if (status != 0)
+			goto err_read_i2c_eeprom;
 
+		if (addr_mode & TXGBE_SFF_ADDRESSING_MODE) {
+			e_err(drv, "Address change required to access page 0xA2,");
+			e_err(drv, "but not supported. Please report the module type to the driver maintainers.\n");
+			page_swap = true;
+		}
+
+		if (sff8472_rev == TXGBE_SFF_SFF_8472_UNSUP || page_swap ||
+		    !(addr_mode & TXGBE_SFF_DDM_IMPLEMENTED)) {
+			/* We have a SFP, but it does not support SFF-8472 */
+			adapter->eeprom_type = ETH_MODULE_SFF_8079;
+			adapter->eeprom_len = ETH_MODULE_SFF_8079_LEN;
+		} else {
+			/* We have a SFP which supports a revision of SFF-8472. */
+			adapter->eeprom_type = ETH_MODULE_SFF_8472;
+			adapter->eeprom_len = ETH_MODULE_SFF_8472_LEN;
+		}
+		for (i = 0; i < adapter->eeprom_len; i++) {
+			if (i < ETH_MODULE_SFF_8079_LEN)
+				status = hw->phy.ops.read_i2c_eeprom(hw, i,
+						&databyte);
+			else
+				status = hw->phy.ops.read_i2c_sff8472(hw, i,
+						&databyte);
+
+			if (status != 0)
+				goto err_read_i2c_eeprom;
+
+			adapter->i2c_eeprom[i] = databyte;
+		}
+	}
 out:
 	hw->mac.ops.release_swfw_sync(hw, swfw_mask);
 
