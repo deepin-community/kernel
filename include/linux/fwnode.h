6dea329d08d317370cc44c8d3126c01b95b700f0
Guomin Chen <Guomin.Chen@cixtech.com>
2025-09-30 09:51:34 +0000

    Add a property reference count interface for ACPI.
    This patch extends the property handling framework to provide a
    reference count query interface for ACPI properties.
    
    Signed-off-by: "wenxue.ding" <Wenxue.Ding@cixtech.com>
    Signed-off-by: Guomin Chen <Guomin.Chen@cixtech.com>

--- include/linux/fwnode.h
+++ include/linux/fwnode.h
@@ -171,6 +171,9 @@ struct fwnode_operations {
 	void __iomem *(*iomap)(struct fwnode_handle *fwnode, int index);
 	int (*irq_get)(const struct fwnode_handle *fwnode, unsigned int index);
 	int (*add_links)(struct fwnode_handle *fwnode);
+	int (*property_count_reference_with_args)(
+		const struct fwnode_handle *fwnode, const char *list_name,
+		const char *cells_name);
 
 	DEEPIN_KABI_RESERVE(1)
 	DEEPIN_KABI_RESERVE(2)
