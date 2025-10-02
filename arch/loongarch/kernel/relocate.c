81a93943a30a3c89d197b192b3cdc302ce466fe6
Youling Tang <tangyouling@kylinos.cn>
2025-10-02 22:39:08 +0800

    LoongArch: Automatically disable kaslr if boot from kexec_file
    commit c8168b4faf1d62cbb320a3e518ad31cdd567cb05 upstream.
    
    Automatically disable kaslr when the kernel loads from kexec_file.
    
    kexec_file loads the secondary kernel image to a non-linked address,
    inherently providing KASLR-like randomization.
    
    However, on LoongArch where System RAM may be non-contiguous, enabling
    KASLR for the second kernel may relocate it to an invalid memory region
    and cause a boot failure. Thus, we disable KASLR when "kexec_file" is
    detected in the command line.
    
    To ensure compatibility with older kernels loaded via kexec_file, this
    patch should be backported to stable branches.
    
    Cc: stable@vger.kernel.org
    Signed-off-by: Youling Tang <tangyouling@kylinos.cn>
    Signed-off-by: Huacai Chen <chenhuacai@loongson.cn>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    (cherry picked from commit 7eca645917bc1d68673536e2f100574e7b542929)
    
    Conflicts:
    	arch/loongarch/kernel/relocate.c

--- arch/loongarch/kernel/relocate.c
+++ arch/loongarch/kernel/relocate.c
@@ -149,6 +149,9 @@ static inline __init bool kaslr_disabled(void)
 	if (str == boot_command_line || (str > boot_command_line && *(str - 1) == ' '))
 		return true;
 #endif
+	str = strstr(boot_command_line, "kexec_file");
+	if (str == boot_command_line || (str > boot_command_line && *(str - 1) == ' '))
+		return true;
 
 	return false;
 }
