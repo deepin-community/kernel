0b95715c12c39495a709375c166fd81fedb95215
Chin-Yen Lee <timlee@realtek.com>
2025-10-21 21:34:01 +0800

    wifi: rtw89: restart hardware to recover firmware if power-save becomes abnormal
    Somehow power-save related functions get failure, such as failed to
    send null packet, or no response form firmware, and then WiFi will
    become unstable. Trigger SER function actively to reset firmware/driver
    to recover from abnormal states, including
    
     - firmware failed to ACK for entering PS mode
     - firmware failed to ACK for leaving PS mode
     - check PS H2C command received by firmware fail
     - failed to leave PS state
    
    Change-Id: I45f41bb87acb28034fa237bc1e52cfb8b511644e
    Signed-off-by: Chin-Yen Lee <timlee@realtek.com>
    Signed-off-by: Ping-Ke Shih <pkshih@realtek.com>
    Link: https://patch.msgid.link/20251021133402.15467-8-pkshih@realtek.com

--- drivers/net/wireless/realtek/rtw89/core.h
+++ drivers/net/wireless/realtek/rtw89/core.h
@@ -176,6 +176,7 @@ extern const struct ieee80211_ops rtw89_ops;
 #define RTW89_TRACK_WORK_PERIOD	round_jiffies_relative(HZ * 2)
 #define RTW89_TRACK_PS_WORK_PERIOD msecs_to_jiffies(100)
 #define RTW89_FORBID_BA_TIMER round_jiffies_relative(HZ * 4)
+#define RTW89_PS_HANG_MAX_CNT 3
 #define CFO_TRACK_MAX_USER 64
 #define MAX_RSSI 110
 #define RSSI_FACTOR 1
@@ -6218,6 +6219,7 @@ struct rtw89_dev {
 	struct rtw89_btc btc;
 	enum rtw89_ps_mode ps_mode;
 	bool lps_enabled;
+	u8 ps_hang_cnt;
 
 	struct rtw89_wow_param wow;
 
