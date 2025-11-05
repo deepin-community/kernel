b2f78f34b836ae8b59a7cf4dbbd2915f2983a981
Kuan-Chung Chen <damon.chen@realtek.com>
2025-11-06 00:23:21 +0800

    wifi: rtw89: mlo: fix missing TX nulldata 1 during link switch
    Fix missing nulldata 1 when switching links. The FW should send a
    nulldata 1 to notify AP before disabling the old link. Adjust the
    position of the H2C rtw89_fw_h2c_mlo_link_cfg to ensure proper FW
    behavior during link transition.
    
    Change-Id: I178fe3ea2a26b33f713fc26789224bd3335e80e0
    Signed-off-by: Kuan-Chung Chen <damon.chen@realtek.com>

--- drivers/net/wireless/realtek/rtw89/mac80211.c
+++ drivers/net/wireless/realtek/rtw89/mac80211.c
@@ -1576,12 +1576,23 @@ static int __rtw89_ops_set_vif_links(struct rtw89_dev *rtwdev,
 	return 0;
 }
 
-static void rtw89_vif_cfg_fw_links(struct rtw89_dev *rtwdev,
-				   struct rtw89_vif *rtwvif,
-				   unsigned long links, bool en)
+static void rtw89_vif_update_fw_links(struct rtw89_dev *rtwdev,
+				      struct rtw89_vif *rtwvif,
+				      u16 current_links, bool en)
 {
+	struct rtw89_vif_ml_trans *trans = &rtwvif->ml_trans;
 	struct rtw89_vif_link *rtwvif_link;
 	unsigned int link_id;
+	unsigned long links;
+
+	/* Do follow-up when all updating links exist. */
+	if (current_links != trans->mediate_links)
+		return;
+
+	if (en)
+		links = trans->links_to_add;
+	else
+		links = trans->links_to_del;
 
 	for_each_set_bit(link_id, &links, IEEE80211_MLD_MAX_NUM_LINKS) {
 		rtwvif_link = rtwvif->links[link_id];
@@ -1590,20 +1601,6 @@ static void rtw89_vif_cfg_fw_links(struct rtw89_dev *rtwdev,
 
 		rtw89_fw_h2c_mlo_link_cfg(rtwdev, rtwvif_link, en);
 	}
-}
-
-static void rtw89_vif_update_fw_links(struct rtw89_dev *rtwdev,
-				      struct rtw89_vif *rtwvif,
-				      u16 current_links)
-{
-	struct rtw89_vif_ml_trans *trans = &rtwvif->ml_trans;
-
-	/* Do follow-up when all updating links exist. */
-	if (current_links != trans->mediate_links)
-		return;
-
-	rtw89_vif_cfg_fw_links(rtwdev, rtwvif, trans->links_to_del, false);
-	rtw89_vif_cfg_fw_links(rtwdev, rtwvif, trans->links_to_add, true);
 }
 
 static
@@ -1647,7 +1644,7 @@ int rtw89_ops_change_vif_links(struct ieee80211_hw *hw,
 	if (rtwdev->scanning)
 		rtw89_hw_scan_abort(rtwdev, rtwdev->scan_info.scanning_vif);
 
-	rtw89_vif_update_fw_links(rtwdev, rtwvif, old_links);
+	rtw89_vif_update_fw_links(rtwdev, rtwvif, old_links, true);
 
 	if (!old_links)
 		__rtw89_ops_clr_vif_links(rtwdev, rtwvif,
@@ -1679,6 +1676,9 @@ int rtw89_ops_change_vif_links(struct ieee80211_hw *hw,
 			__rtw89_ops_clr_vif_links(rtwdev, rtwvif,
 						  BIT(RTW89_VIF_IDLE_LINK_ID));
 	}
+
+	if (!ret)
+		rtw89_vif_update_fw_links(rtwdev, rtwvif, new_links, false);
 
 	rtw89_enter_ips_by_hwflags(rtwdev);
 	return ret;
