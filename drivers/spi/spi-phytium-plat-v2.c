81a4aef739eb0abcbdf672b73480c9280ab751b2
Peng Min <pengmin1540@phytium.com.cn>
2025-12-04 09:18:43 +0800

    spi-v2: phytium: Add support for full-duplex transmission mode
    In order to accommodate devices such as spidev and tpm that
    support full-duplex transmission, full-duplex support has been
    added to the spi-v2 driver.
    
    Mainline: NA
    Signed-off-by: Peng Min <pengmin1540@phytium.com.cn>
    Signed-off-by: Wang Yinfeng <wangyinfeng@phytium.com.cn>
    Change-Id: I6b5a96337b8713ed5300d4c1651ea2d14c74bfdb

--- drivers/spi/spi-phytium-plat-v2.c
+++ drivers/spi/spi-phytium-plat-v2.c
@@ -26,7 +26,7 @@
 #include "spi-phytium.h"
 
 #define DRIVER_NAME_PHYT "phytium_spi_2.0"
-#define DRIVER_VERSION	"1.0.13"
+#define DRIVER_VERSION	"1.0.14"
 
 #define PHYTIUM_CPU_PART_FTC872		0x872
 
