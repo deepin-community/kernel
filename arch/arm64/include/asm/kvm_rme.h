OERV-RVCI/rvck-olk

arm64: RME: Introduce kvm_rec_pre_enter() called before entering an atomic section
community inclusion
category: bugfix
bugzilla: https://gitee.com/openeuler/kernel/issues/ICX7FX?from=project-issue

Reference: https://patchew.org/linux/20250820145606.180644-1-steven.price@arm.com/20250820145606.180644-17-steven.price@arm.com

--------------------------------

Entering a realm is done using a SMC call to the RMM. On exit the
exit-codes need to be handled slightly differently to the normal KVM
path so define our own functions for realm enter/exit and hook them
in if the guest is a realm guest.

Fixes: a9b2e8a67446 ("[v8-16-43]arm64: RME: Handle realm enter/exit")
Signed-off-by: Steven Price <steven.price@arm.com>
Reviewed-by: Gavin Shan <gshan@redhat.com>

---
Changes since v8 patch:
 * Introduce kvm_rec_pre_enter() called before entering an atomic
   section to handle operations that might require memory allocation
   (specifically completing a RIPAS change introduced in a later patch).
 * Updates to align with upstream changes to hpfar_el2 which now (ab)uses
   HPFAR_EL2_NS as a valid flag.
 * Fix exit reason when racing with PSCI shutdown to return
   KVM_EXIT_SHUTDOWN rather than KVM_EXIT_UNKNOWN.

Backport cca-v10 changes from v8 for bugfix

Signed-off-by: houmingyong <houmingyong@huawei.com>

arch/arm64/include/asm/kvm_rme.h
@@ -108,6 +108,7 @@ int _kvm_create_rec(struct kvm_vcpu *vcpu);
 void _kvm_destroy_rec(struct kvm_vcpu *vcpu);
 
 int _kvm_rec_enter(struct kvm_vcpu *vcpu);
+int _kvm_rec_pre_enter(struct kvm_vcpu *vcpu);
 int _handle_rec_exit(struct kvm_vcpu *vcpu, int rec_run_status);
 
 void kvm_realm_unmap_range(struct kvm *kvm,

arch/arm64/kvm/rme.c
@@ -1469,11 +1469,23 @@ static void kvm_complete_ripas_change(struct kvm_vcpu *vcpu)
 		base = top_ipa;
 	} while (top_ipa < top);
 }
-
-int _kvm_rec_enter(struct kvm_vcpu *vcpu)
+/*
+ * _kvm_rec_pre_enter - Complete operations before entering a REC
+ *
+ * Some operations require work to be completed before entering a realm. That
+ * work may require memory allocation so cannot be done in the kvm_rec_enter()
+ * call.
+ *
+ * Return: 1 if we should enter the guest
+ *	   0 if we should exit to userspace
+ *	   < 0 if we should exit to userspace, where the return value indicates
+ *	   an error
+ */
+int _kvm_rec_pre_enter(struct kvm_vcpu *vcpu)
 {
 	struct realm_rec *rec = vcpu->arch.rec;
-
+	if (kvm_realm_state(vcpu->kvm) != REALM_STATE_ACTIVE)
+		return -EINVAL;
 	switch (rec->run->exit.exit_reason) {
 	case RMI_EXIT_HOST_CALL:
 	case RMI_EXIT_PSCI:
@@ -1485,8 +1497,12 @@ int _kvm_rec_enter(struct kvm_vcpu *vcpu)
 		break;
 	}
 
-	if (kvm_realm_state(vcpu->kvm) != REALM_STATE_ACTIVE)
-		return -EINVAL;
+	return 1;
+}
+
+int _kvm_rec_enter(struct kvm_vcpu *vcpu)
+{
+	struct realm_rec *rec = vcpu->arch.rec;
 
 	return rmi_rec_enter(virt_to_phys(rec->rec_page),
 			     virt_to_phys(rec->run));
@@ -1707,6 +1723,7 @@ static struct cca_operations armcca_operations = {
 	.enable_cap = _kvm_realm_enable_cap,
 	.init_realm_vm = _kvm_init_realm_vm,
 	.realm_vm_enter = _kvm_rec_enter,
+	.realm_vm_pre_enter = _kvm_rec_pre_enter,
 	.realm_vm_exit = _handle_rec_exit,
 	.init_sel2_hypervisor = _kvm_init_rme,
 	.psci_complete = _realm_psci_complete,

deepin-community/kernel

Signed-off-by: Xu Raoqing <xuraoqing@huawei.com>
Signed-off-by: WangYuli <wangyl5933@chinaunicom.cn>

arch/arm64/include/asm/kvm_rme.h
@@ -108,6 +108,7 @@ int kvm_create_rec(struct kvm_vcpu *vcpu);
 void kvm_destroy_rec(struct kvm_vcpu *vcpu);
 
 int kvm_rec_enter(struct kvm_vcpu *vcpu);
+int kvm_rec_pre_enter(struct kvm_vcpu *vcpu);
 int handle_rec_exit(struct kvm_vcpu *vcpu, int rec_run_status);
 
 void kvm_realm_unmap_range(struct kvm *kvm,

arch/arm64/kvm/rme.c
@@ -1470,10 +1470,24 @@ static void kvm_complete_ripas_change(struct kvm_vcpu *vcpu)
 	} while (top_ipa < top);
 }
 
-int kvm_rec_enter(struct kvm_vcpu *vcpu)
+/*
+ * kvm_rec_pre_enter - Complete operations before entering a REC
+ *
+ * Some operations require work to be completed before entering a realm. That
+ * work may require memory allocation so cannot be done in the kvm_rec_enter()
+ * call.
+ *
+ * Return: 1 if we should enter the guest
+ *	   0 if we should exit to userspace
+ *	   < 0 if we should exit to userspace, where the return value indicates
+ *	   an error
+ */
+int kvm_rec_pre_enter(struct kvm_vcpu *vcpu)
 {
 	struct realm_rec *rec = &vcpu->arch.rec;
 
+	if (kvm_realm_state(vcpu->kvm) != REALM_STATE_ACTIVE)
+		return -EINVAL;
 	switch (rec->run->exit.exit_reason) {
 	case RMI_EXIT_HOST_CALL:
 	case RMI_EXIT_PSCI:
@@ -1485,8 +1499,12 @@ int kvm_rec_enter(struct kvm_vcpu *vcpu)
 		break;
 	}
 
-	if (kvm_realm_state(vcpu->kvm) != REALM_STATE_ACTIVE)
-		return -EINVAL;
+	return 1;
+}
+
+int kvm_rec_enter(struct kvm_vcpu *vcpu)
+{
+	struct realm_rec *rec = &vcpu->arch.rec;
 
 	return rmi_rec_enter(virt_to_phys(rec->rec_page),
 			     virt_to_phys(rec->run));