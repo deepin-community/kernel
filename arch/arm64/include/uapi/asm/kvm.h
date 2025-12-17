9b38228b2b70986f37266b7a59997e4e5b1d1da1
Steven Price <steven.price@arm.com>
2025-12-17 10:11:22 +0000

    Fromlist: KVM: arm64: Expose KVM_ARM_VCPU_REC to user space
    Increment KVM_VCPU_MAX_FEATURES to expose the new capability to user
    space.
    
    Signed-off-by: Steven Price <steven.price@arm.com>
    Reviewed-by: Gavin Shan <gshan@redhat.com>

--- arch/arm64/include/uapi/asm/kvm.h
+++ arch/arm64/include/uapi/asm/kvm.h
@@ -110,6 +110,7 @@ struct kvm_regs {
 #define KVM_ARM_VCPU_PTRAUTH_ADDRESS	5 /* VCPU uses address authentication */
 #define KVM_ARM_VCPU_PTRAUTH_GENERIC	6 /* VCPU uses generic authentication */
 #define KVM_ARM_VCPU_HAS_EL2		7 /* Support nested virtualization */
+#define KVM_ARM_VCPU_REC		8 /* Create a Realm guest */
 
 struct kvm_vcpu_init {
 	__u32 target;
