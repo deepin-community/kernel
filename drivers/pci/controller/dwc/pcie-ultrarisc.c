c5132109a68c9f56bebd174b76e8e187f5b3723b
Jia Wang <wangjia@ultrarisc.com>
2025-12-29 09:41:02 +0800

    PCI: dwc: ultrarisc: Update compatible string for DP1000 SoC
    community inclusion
    category: feature
    bugzilla: https://github.com/RVCK-Project/rvck/issues/71
    
    --------------------------------
    
    Update the compatible string for UltraRISC DP1000 SoC PCIe controllers
    from 'ultrarisc,dw-pcie' to 'ultrarisc,dp1000-pcie', 'ultrarisc,dw-pcie'
    to better distinguish the DP1000 implementation while maintaining
    backward compatibility.
    
    This change updates both the device tree nodes and the driver's
    device ID matching table to ensure proper driver binding.
    
    Signed-off-by: Jia Wang <wangjia@ultrarisc.com>

--- drivers/pci/controller/dwc/pcie-ultrarisc.c
+++ drivers/pci/controller/dwc/pcie-ultrarisc.c
@@ -183,9 +183,8 @@ int ultrarisc_pcie_resume(struct platform_device *pdev)
 }
 
 static const struct of_device_id ultrarisc_pcie_of_match[] = {
-	{
-		.compatible = "ultrarisc,dw-pcie",
-	},
+	{ .compatible = "ultrarisc,dp1000-pcie", },
+	{ .compatible = "ultrarisc,dw-pcie", },
 	{},
 };
 
