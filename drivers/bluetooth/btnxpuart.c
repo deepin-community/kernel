838f7dc0bfe90b1334ee85f92df2071a4ccaacb3
Bastien Nocera <hadess@hadess.net>
2026-01-19 13:34:59 +0100

    Bluetooth: btnxpuart: Remove unneeded CONFIG_PM ifdef
    The functions are already disabled through the use of pm_ptr() when
    CONFIG_PM is disabled, and will be removed from the final linked code
    as not needed.
    
    This increases build coverage and allows to drop an #ifdef.
    
    Signed-off-by: Bastien Nocera <hadess@hadess.net>
    Reviewed-by: Neeraj Sanjay Kale <neeraj.sanjaykale@nxp.com>
    Signed-off-by: Luiz Augusto von Dentz <luiz.von.dentz@intel.com>
    (cherry picked from commit 04a4c885fddf505872404f12ed9353fa4bc280c5)
    Signed-off-by: Wentao Guan <guanwentao@uniontech.com>

--- drivers/bluetooth/btnxpuart.c
+++ drivers/bluetooth/btnxpuart.c
@@ -1953,8 +1953,7 @@ static void nxp_serdev_remove(struct serdev_device *serdev)
 	hci_free_dev(hdev);
 }
 
-#ifdef CONFIG_PM_SLEEP
-static int nxp_serdev_suspend(struct device *dev)
+static int __maybe_unused nxp_serdev_suspend(struct device *dev)
 {
 	struct btnxpuart_dev *nxpdev = dev_get_drvdata(dev);
 	struct ps_data *psdata = &nxpdev->psdata;
@@ -1968,7 +1967,7 @@ static int nxp_serdev_suspend(struct device *dev)
 	return 0;
 }
 
-static int nxp_serdev_resume(struct device *dev)
+static int __maybe_unused nxp_serdev_resume(struct device *dev)
 {
 	struct btnxpuart_dev *nxpdev = dev_get_drvdata(dev);
 	struct ps_data *psdata = &nxpdev->psdata;
@@ -1981,7 +1980,6 @@ static int nxp_serdev_resume(struct device *dev)
 	ps_control(psdata->hdev, PS_STATE_AWAKE);
 	return 0;
 }
-#endif
 
 #ifdef CONFIG_DEV_COREDUMP
 static void nxp_serdev_coredump(struct device *dev)
