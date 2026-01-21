bfa8416c6a2ace0504523a1b1dd9e669be25608a
Wentao Guan <guanwentao@uniontech.com>
2026-01-21 09:53:20 +0800

    Revert "wifi: cfg80211: pass net_device to .set_monitor_channel"
    deepin inclusion
    category: bugix
    
    It is an API change, which cause aic8800 build failed.
    
    This reverts commit 9f92b0d39f80ceb721e898979d2dd758fbb6db78.
    
    Link: https://bbs.deepin.org.cn/post/295146
    Signed-off-by: Wentao Guan <guanwentao@uniontech.com>

--- drivers/net/wireless/microchip/wilc1000/cfg80211.c
+++ drivers/net/wireless/microchip/wilc1000/cfg80211.c
@@ -231,7 +231,6 @@ struct wilc_vif *wilc_get_wl_to_vif(struct wilc *wl)
 }
 
 static int set_channel(struct wiphy *wiphy,
-		       struct net_device *dev,
 		       struct cfg80211_chan_def *chandef)
 {
 	struct wilc *wl = wiphy_priv(wiphy);
@@ -1425,7 +1424,7 @@ static int start_ap(struct wiphy *wiphy, struct net_device *dev,
 	struct wilc_vif *vif = netdev_priv(dev);
 	int ret;
 
-	ret = set_channel(wiphy, dev, &settings->chandef);
+	ret = set_channel(wiphy, &settings->chandef);
 	if (ret != 0)
 		netdev_err(dev, "Error in setting channel\n");
 
