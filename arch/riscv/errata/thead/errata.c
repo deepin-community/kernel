01f7ae08f60f8edc478681f151a10a6d46d1846c
Guo Ren (Alibaba DAMO Academy) <guoren@kernel.org>
2026-01-25 01:39:41 -0500

    FROMLIST: riscv: errata: Add ERRATA_THEAD_WRITE_ONCE fixup
    The early version of XuanTie C910 core has a store merge buffer
    delay problem. The store merge buffer could improve the store queue
    performance by merging multi-store requests, but when there are not
    continued store requests, the prior single store request would be
    waiting in the store queue for a long time. That would cause
    significant problems for communication between multi-cores. This
    problem was found on sg2042 & th1520 platforms with the qspinlock
    lock torture test.
    
    So appending a fence w.o could immediately flush the store merge
    buffer and let other cores see the write result.
    
    This will apply the WRITE_ONCE errata to handle the non-standard
    behavior via appending a fence w.o instruction for WRITE_ONCE().
    
    This problem is only observed on the sg2042 hardware platform by
    running the lock_torture test program for half an hour. The problem
    was not found in the user space application, because interrupt can
    break the livelock.
    
    Reviewed-by: Leonardo Bras <leobras@redhat.com>
    Tested-by: Han Gao <gaohan@iscas.ac.cn>
    Cc: Yao Zi <me@ziyao.cc>
    Cc: Alexandre Ghiti <alexghiti@rivosinc.com>
    Cc: Paul Walmsley <pjw@kernel.org>
    Signed-off-by: Guo Ren (Alibaba DAMO Academy) <guoren@kernel.org>
    Link: https://lore.kernel.org/r/20260125063941.443777-1-guoren@kernel.org
    Signed-off-by: Han Gao <gaohan@iscas.ac.cn>

--- arch/riscv/errata/thead/errata.c
+++ arch/riscv/errata/thead/errata.c
@@ -168,6 +168,23 @@ static bool errata_probe_ghostwrite(unsigned int stage,
 	return true;
 }
 
+static bool errata_probe_write_once(unsigned int stage,
+				    unsigned long arch_id, unsigned long impid)
+{
+	if (!IS_ENABLED(CONFIG_ERRATA_THEAD_WRITE_ONCE))
+		return false;
+
+	/* target-c9xx cores report arch_id and impid as 0 */
+	if (arch_id != 0 || impid != 0)
+		return false;
+
+	if (stage == RISCV_ALTERNATIVES_BOOT ||
+	    stage == RISCV_ALTERNATIVES_MODULE)
+		return true;
+
+	return false;
+}
+
 static u32 thead_errata_probe(unsigned int stage,
 			      unsigned long archid, unsigned long impid)
 {
@@ -182,6 +199,9 @@ static u32 thead_errata_probe(unsigned int stage,
 		cpu_req_errata |= BIT(ERRATA_THEAD_PMU);
 
 	errata_probe_ghostwrite(stage, archid, impid);
+
+	if (errata_probe_write_once(stage, archid, impid))
+		cpu_req_errata |= BIT(ERRATA_THEAD_WRITE_ONCE);
 
 	return cpu_req_errata;
 }
