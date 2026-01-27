11d80d297ee3df092599b0aaab9ca6f24c64c840
Nicolas Pitre <npitre@baylibre.com>
2026-01-27 17:56:01 -0500

    vt: save/restore unicode screen buffer for alternate screen
    commit 5eb608319bb56464674a71b4a66ea65c6c435d64 upstream.
    
    The alternate screen support added by commit 23743ba64709 ("vt: add
    support for smput/rmput escape codes") only saves and restores the
    regular screen buffer (vc_origin), but completely ignores the corresponding
    unicode screen buffer (vc_uni_lines) creating a messed-up display.
    
    Add vc_saved_uni_lines to save the unicode screen buffer when entering
    the alternate screen, and restore it when leaving. Also ensure proper
    cleanup in reset_terminal() and vc_deallocate().
    
    Fixes: 23743ba64709 ("vt: add support for smput/rmput escape codes")
    Cc: stable <stable@kernel.org>
    Signed-off-by: Nicolas Pitre <npitre@baylibre.com>
    Link: https://patch.msgid.link/5o2p6qp3-91pq-0p17-or02-1oors4417ns7@onlyvoer.pbz
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    (cherry picked from commit c4efef65a46f610418a3f69149c51492d31c8591)
    Signed-off-by: Wentao Guan <guanwentao@uniontech.com>

--- drivers/tty/vt/vt.c
+++ drivers/tty/vt/vt.c
@@ -1370,6 +1370,8 @@ struct vc_data *vc_deallocate(unsigned int currcons)
 			kfree(vc->vc_saved_screen);
 			vc->vc_saved_screen = NULL;
 		}
+		vc_uniscr_free(vc->vc_saved_uni_lines);
+		vc->vc_saved_uni_lines = NULL;
 	}
 	return vc;
 }
@@ -1915,6 +1917,8 @@ static void enter_alt_screen(struct vc_data *vc)
 	vc->vc_saved_screen = kmemdup((u16 *)vc->vc_origin, size, GFP_KERNEL);
 	if (vc->vc_saved_screen == NULL)
 		return;
+	vc->vc_saved_uni_lines = vc->vc_uni_lines;
+	vc->vc_uni_lines = NULL;
 	vc->vc_saved_rows = vc->vc_rows;
 	vc->vc_saved_cols = vc->vc_cols;
 	save_cur(vc);
@@ -1936,6 +1940,8 @@ static void leave_alt_screen(struct vc_data *vc)
 		dest = ((u16 *)vc->vc_origin) + r * vc->vc_cols;
 		memcpy(dest, src, 2 * cols);
 	}
+	vc_uniscr_set(vc, vc->vc_saved_uni_lines);
+	vc->vc_saved_uni_lines = NULL;
 	restore_cur(vc);
 	/* Update the entire screen */
 	if (con_should_update(vc))
@@ -2258,6 +2264,8 @@ static void reset_terminal(struct vc_data *vc, int do_clear)
 	if (vc->vc_saved_screen != NULL) {
 		kfree(vc->vc_saved_screen);
 		vc->vc_saved_screen = NULL;
+		vc_uniscr_free(vc->vc_saved_uni_lines);
+		vc->vc_saved_uni_lines = NULL;
 		vc->vc_saved_rows = 0;
 		vc->vc_saved_cols = 0;
 	}
