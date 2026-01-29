af8fab92f197a5ab7f7956bac5d3226f4914d4be
qiuzhiteng <qiuzhiteng@hygon.cn>
2026-01-29 09:35:03 +0800

    x86: Resolve FPU state corruption between atomic and non-atomic contexts
    hygon inclusion
    category: bugfix
    bugzilla: https://atomgit.com/openeuler/kernel/issues/8438
    CVE: NA
    
    ---------------------------
    
    Hygon-SIG: commit none hygon x86: Resolve FPU state
    corruption between atomic and non-atomic contexts
    
    Fix data inconsistency caused by race condition between
    kernel_fpu_begin_nonatomic (used for NT copy) and kernel_fpu_begin.
    Introduce separate FPU context flags to properly isolate atomic and
    non-atomic FPU usage paths, ensuring state integrity during preemption
    and context switches.
    
    Fixes: 48fb9af1d048 ("mm: Enhanced copy capabilities for Hygon processor")
    Signed-off-by: Zhiteng Qiu <qiuzhiteng@hygon.cn>
    (cherry picked from commit 32afeccc02179d5153c4493c736ccd1a8ba9e0b4)
    Signed-off-by: Wentao Guan <guanwentao@uniontech.com>

--- arch/x86/include/asm/fpu/api.h
+++ arch/x86/include/asm/fpu/api.h
@@ -73,18 +73,6 @@ static inline int kernel_fpu_begin_nonatomic(void)
 #endif
 }
 
-/*
- * It means we call kernel_fpu_end after kernel_fpu_begin_nonatomic
- * func, but before kernel_fpu_end_nonatomic
- */
-static inline void check_using_kernel_fpu(void)
-{
-	WARN_ON_ONCE(test_thread_flag(TIF_USING_FPU_NONATOMIC));
-}
-
-#else
-static inline void check_using_kernel_fpu(void) { }
-
 #endif
 
 /*
