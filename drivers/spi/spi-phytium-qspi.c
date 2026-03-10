c6e9de22cc5a7165d9c602183eb673b4e01d0729
Cui Fulong <cuifulong2112@phytium.com.cn>
2026-03-10 17:11:10 +0800

    qspi: phytium: Slove the capacity register configured error issue
    Functions that configure capacity registers should shift left
    instead of right, which will lead to capacity register configured
    error.
    
    Mainline: Open-Source
    Signed-off-by: Cui Fulong <cuifulong2112@phytium.com.cn>
    Signed-off-by: Peng Min <pengmin1540@phytium.com.cn>
    Signed-off-by: Wang Yinfeng <wangyinfeng@phytium.com.cn>

--- drivers/spi/spi-phytium-qspi.c
+++ drivers/spi/spi-phytium-qspi.c
@@ -246,6 +246,44 @@ static int phytium_spi_nor_protocol_encode(const struct spi_mem_op *op, u32 *cod
 	return ret;
 }
 
+static int phytium_qspi_flash_capacity_encode_new(u32 size,
+		u32 *cap, int i)
+{
+	int ret = 0;
+
+	switch (size) {
+	case SZ_4M:
+		*cap |= (0x0 << (4 * i));
+		break;
+	case SZ_8M:
+		*cap |= (0x1 << (4 * i));
+		break;
+	case SZ_16M:
+		*cap |= (0x2 << (4 * i));
+		break;
+	case SZ_32M:
+		*cap |= (0x3 << (4 * i));
+		break;
+	case SZ_64M:
+		*cap |= (0x4 << (4 * i));
+		break;
+	case SZ_128M:
+		*cap |= (0x5 << (4 * i));
+		break;
+	case SZ_256M:
+		*cap |= (0x6 << (4 * i));
+		break;
+	case SZ_512M:
+		*cap |= (0x7 << (4 * i));
+		break;
+	default:
+		ret = -EINVAL;
+		break;
+	}
+
+	return ret;
+}
+
 static int phytium_qspi_flash_capacity_encode(u32 size, u32 *cap)
 {
 	int ret = 0;
