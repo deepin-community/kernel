df0310c470a7a58b91502e805f6b58736ac4bd7f
Hyunchul Lee <hyc.lee@gmail.com>
2026-03-17 14:13:44 +0900

    ntfs: remove redundant out-of-bound checks
    Remove redundant out-of-bounds validations.
    Since ntfs_attr_find and ntfs_external_attr_find
    now validate the attribute value offsets and
    lengths against the bounds of the MFT record block,
    performing subsequent bounds checking in caller
    functions like ntfs_attr_lookup is no longer necessary.
    
    Signed-off-by: Hyunchul Lee <hyc.lee@gmail.com>
    Signed-off-by: Namjae Jeon <linkinjeon@kernel.org>
    (cherry picked from commit 14f0a13ec79dfa63e143ea45e6530d80bec6e291)
    Signed-off-by: Wentao Guan <guanwentao@uniontech.com>

--- fs/ntfs/inode.c
+++ fs/ntfs/inode.c
@@ -773,12 +773,6 @@ static int ntfs_read_locked_inode(struct inode *vi)
 	}
 	a = ctx->attr;
 	/* Get the standard information attribute value. */
-	if ((u8 *)a + le16_to_cpu(a->data.resident.value_offset)
-			+ le32_to_cpu(a->data.resident.value_length) >
-			(u8 *)ctx->mrec + vol->mft_record_size) {
-		ntfs_error(vi->i_sb, "Corrupt standard information attribute in inode.");
-		goto unm_err_out;
-	}
 	si = (struct standard_information *)((u8 *)a +
 			le16_to_cpu(a->data.resident.value_offset));
 
@@ -865,13 +859,6 @@ static int ntfs_read_locked_inode(struct inode *vi)
 				goto unm_err_out;
 			}
 		} else /* if (!a->non_resident) */ {
-			if ((u8 *)a + le16_to_cpu(a->data.resident.value_offset)
-					+ le32_to_cpu(
-					a->data.resident.value_length) >
-					(u8 *)ctx->mrec + vol->mft_record_size) {
-				ntfs_error(vi->i_sb, "Corrupt attribute list in inode.");
-				goto unm_err_out;
-			}
 			/* Now copy the attribute list. */
 			memcpy(ni->attr_list, (u8 *)a + le16_to_cpu(
 					a->data.resident.value_offset),
@@ -970,10 +957,6 @@ view_index_meta:
 		ir = (struct index_root *)((u8 *)a +
 				le16_to_cpu(a->data.resident.value_offset));
 		ir_end = (u8 *)ir + le32_to_cpu(a->data.resident.value_length);
-		if (ir_end > (u8 *)ctx->mrec + vol->mft_record_size) {
-			ntfs_error(vi->i_sb, "$INDEX_ROOT attribute is corrupt.");
-			goto unm_err_out;
-		}
 		index_end = (u8 *)&ir->index +
 				le32_to_cpu(ir->index.index_length);
 		if (index_end > ir_end) {
@@ -1568,10 +1551,6 @@ static int ntfs_read_locked_index_inode(struct inode *base_vi, struct inode *vi)
 
 	ir = (struct index_root *)((u8 *)a + le16_to_cpu(a->data.resident.value_offset));
 	ir_end = (u8 *)ir + le32_to_cpu(a->data.resident.value_length);
-	if (ir_end > (u8 *)ctx->mrec + vol->mft_record_size) {
-		ntfs_error(vi->i_sb, "$INDEX_ROOT attribute is corrupt.");
-		goto unm_err_out;
-	}
 	index_end = (u8 *)&ir->index + le32_to_cpu(ir->index.index_length);
 	if (index_end > ir_end) {
 		ntfs_error(vi->i_sb, "Index is corrupt.");
@@ -2015,13 +1994,6 @@ int ntfs_read_inode_mount(struct inode *vi)
 				goto put_err_out;
 			}
 		} else /* if (!ctx.attr->non_resident) */ {
-			if ((u8 *)a + le16_to_cpu(
-					a->data.resident.value_offset) +
-					le32_to_cpu(a->data.resident.value_length) >
-					(u8 *)ctx->mrec + vol->mft_record_size) {
-				ntfs_error(sb, "Corrupt attribute list attribute.");
-				goto put_err_out;
-			}
 			/* Now copy the attribute list. */
 			memcpy(ni->attr_list, (u8 *)a + le16_to_cpu(
 					a->data.resident.value_offset),
