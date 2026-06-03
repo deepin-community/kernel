fdfa64a303d8415547e92ba3acf702f2f116bc1c
Xie XiuQi <xiexiuqi@huawei.com>
2026-06-03 10:03:27 +0800

    GHES: Add a notify chain for process memory section.
    Add a notify chain for process memory section, with which
    other modules might do error recovery.
    
    Signed-off-by: Xie XiuQi <xiexiuqi@huawei.com>
    Tested-by: Wang Xiongfeng <wangxiongfeng2@huawei.com>
    Tested-by: Tyler Baicar <tbaicar@codeaurora.org>
    Reviewed-by: Hanjun Guo <guohanjun@huawei.com>
    Signed-off-by: Yang Yingliang <yangyingliang@huawei.com>
    Tested-by: Peng Min <pengmin1540@phytium.com.cn>
    Signed-off-by: Wang Yinfeng <wangyinfeng@phytium.com.cn>
    Signed-off-by: Wang Chenlu <wangchenlu2236@phytium.com.cn>

--- include/acpi/ghes.h
+++ include/acpi/ghes.h
@@ -131,6 +131,14 @@ int ghes_notify_sea(void);
 static inline int ghes_notify_sea(void) { return -ENOENT; }
 #endif
 
+struct ghes_mem_err {
+	int notify_type;
+	int severity;
+	struct cper_sec_mem_err *mem_err;
+};
+
+extern struct atomic_notifier_head ghes_mem_err_chain;
+
 struct notifier_block;
 extern void ghes_register_report_chain(struct notifier_block *nb);
 extern void ghes_unregister_report_chain(struct notifier_block *nb);
