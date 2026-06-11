85dc1cd5db844e0f6de34d77e3d130dec71be76a
Binbin Zhou <zhoubinbin@loongson.cn>
2026-06-11 14:21:54 +0800

    ipmi: Add Loongson-2K BMC support
    commit d46651d4e3c0caab554c4c591c0b6c3b026b1e93 upstream.
    
    This patch adds Loongson-2K BMC IPMI support.
    
    According to the existing design, we use software simulation to
    implement the KCS interface registers: Stauts/Command/Data_Out/Data_In.
    
    Also since both host side and BMC side read and write kcs status, fifo flag
    is used to ensure data consistency.
    
    The single KCS message block is as follows:
    
    +-------------------------------------------------------------------------+
    |FIFO flags| KCS register data | CMD data | KCS version | WR REQ | WR ACK |
    +-------------------------------------------------------------------------+
    
    Co-developed-by: Chong Qiao <qiaochong@loongson.cn>
    Signed-off-by: Chong Qiao <qiaochong@loongson.cn>
    Reviewed-by: Huacai Chen <chenhuacai@loongson.cn>
    Acked-by: Corey Minyard <corey@minyard.net>
    Signed-off-by: Binbin Zhou <zhoubinbin@loongson.cn>
    Message-ID: <8f9ffb6f0405345af8f04193ce1510aacd075e72.1756987761.git.zhoubinbin@loongson.cn>
    Signed-off-by: Corey Minyard <corey@minyard.net>
    (cherry picked from commit d46651d4e3c0caab554c4c591c0b6c3b026b1e93)
    Signed-off-by: zhuyunfei <zhuyunfei@loongson.cn>

--- drivers/char/ipmi/ipmi_si_intf.c
+++ drivers/char/ipmi/ipmi_si_intf.c
@@ -2149,6 +2149,8 @@ static int __init init_ipmi_si(void)
 
 	ipmi_si_pci_init();
 
+	ipmi_si_ls2k_init();
+
 	ipmi_si_parisc_init();
 
 	/* We prefer devices with interrupts, but in the case of a machine
@@ -2329,6 +2331,8 @@ static void cleanup_ipmi_si(void)
 		return;
 
 	ipmi_si_pci_shutdown();
+
+	ipmi_si_ls2k_shutdown();
 
 	ipmi_si_parisc_shutdown();
 
