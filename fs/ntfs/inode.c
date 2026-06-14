3ede2c73fc30731d5bf5ef48666b9e6927b60e65
Hyunchul Lee <hyc.lee@gmail.com>
2026-06-15 08:49:52 +0900

    ntfs: fix incorrect size of symbolic link
    This patch fixes the issue where a symbolic link size is displayed as 0.
    
    Cc: stable@vger.kernel.org # v7.1
    Signed-off-by: Hyunchul Lee <hyc.lee@gmail.com>
    Signed-off-by: Namjae Jeon <linkinjeon@kernel.org>
    (cherry picked from commit 05a5ff86a7f12c861e3516d3dc4d092ce620742d)
    Signed-off-by: Wentao Guan <guanwentao@uniontech.com>

--- fs/ntfs/inode.c
+++ fs/ntfs/inode.c
@@ -1215,6 +1215,9 @@ no_data_attr_special_case:
 	else
 		vi->i_blocks = ni->allocated_size >> 9;
 
+	if (S_ISLNK(vi->i_mode) && ni->target)
+		vi->i_size = strlen(ni->target);
+
 	ntfs_debug("Done.");
 	return 0;
 unm_err_out:
