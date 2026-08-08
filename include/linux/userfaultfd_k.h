cd3ceee35b00ee446f41bc02643535585185a84d
Chunyan Zhang <zhangchunyan@iscas.ac.cn>
2026-08-08 07:12:48 -0400

    mm: userfaultfd: add pgtable_supports_uffd_wp()
    [ Upstream commit f59c0924d61aa2a2bb85936a593140f327112787 ]
    
    Some platforms can customize the PTE/PMD entry uffd-wp bit making it
    unavailable even if the architecture provides the resource.  This patch
    adds a macro API pgtable_supports_uffd_wp() that allows architectures to
    define their specific implementations to check if the uffd-wp bit is
    available on which device the kernel is running.
    
    Also this patch is removing "ifdef CONFIG_HAVE_ARCH_USERFAULTFD_WP" and
    "ifdef CONFIG_PTE_MARKER_UFFD_WP" in favor of pgtable_supports_uffd_wp()
    and uffd_supports_wp_marker() checks respectively that default to
    IS_ENABLED(CONFIG_HAVE_ARCH_USERFAULTFD_WP) and
    "IS_ENABLED(CONFIG_HAVE_ARCH_USERFAULTFD_WP) &&
    IS_ENABLED(CONFIG_PTE_MARKER_UFFD_WP)" if not overridden by the
    architecture, no change in behavior is expected.
    
    Link: https://lkml.kernel.org/r/20251113072806.795029-3-zhangchunyan@iscas.ac.cn
    Signed-off-by: Chunyan Zhang <zhangchunyan@iscas.ac.cn>
    Acked-by: David Hildenbrand <david@redhat.com>
    Cc: Albert Ou <aou@eecs.berkeley.edu>
    Cc: Alexandre Ghiti <alex@ghiti.fr>
    Cc: Alexandre Ghiti <alexghiti@rivosinc.com>
    Cc: Al Viro <viro@zeniv.linux.org.uk>
    Cc: Andrew Jones <ajones@ventanamicro.com>
    Cc: Arnd Bergmann <arnd@arndb.de>
    Cc: Axel Rasmussen <axelrasmussen@google.com>
    Cc: Christian Brauner <brauner@kernel.org>
    Cc: Conor Dooley <conor.dooley@microchip.com>
    Cc: Conor Dooley <conor@kernel.org>
    Cc: Deepak Gupta <debug@rivosinc.com>
    Cc: Jan Kara <jack@suse.cz>
    Cc: Liam Howlett <liam.howlett@oracle.com>
    Cc: Lorenzo Stoakes <lorenzo.stoakes@oracle.com>
    Cc: Michal Hocko <mhocko@suse.com>
    Cc: Mike Rapoport <rppt@kernel.org>
    Cc: Palmer Dabbelt <palmer@dabbelt.com>
    Cc: Paul Walmsley <paul.walmsley@sifive.com>
    Cc: Peter Xu <peterx@redhat.com>
    Cc: Rob Herring <robh@kernel.org>
    Cc: Suren Baghdasaryan <surenb@google.com>
    Cc: Vlastimil Babka <vbabka@suse.cz>
    Cc: Yuanchu Xie <yuanchu@google.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Stable-dep-of: 3c58f641e813 ("userfaultfd: prevent registration of special VMAs")
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Conflicts:
    	fs/userfaultfd.c
    	include/linux/userfaultfd_k.h
    (cherry picked from commit 896e8884fc2cc25cdc13589c0b47663b968dc08d)
    Signed-off-by: Wentao Guan <guanwentao@uniontech.com>

--- include/linux/userfaultfd_k.h
+++ include/linux/userfaultfd_k.h
@@ -177,15 +177,14 @@ static inline bool vma_can_userfault(struct vm_area_struct *vma,
 	if (wp_async && (vm_flags == VM_UFFD_WP))
 		return true;
 
-#ifndef CONFIG_PTE_MARKER_UFFD_WP
 	/*
 	 * If user requested uffd-wp but not enabled pte markers for
 	 * uffd-wp, then shmem & hugetlbfs are not supported but only
 	 * anonymous.
 	 */
-	if ((vm_flags & VM_UFFD_WP) && !vma_is_anonymous(vma))
+	if (!uffd_supports_wp_marker() && (vm_flags & VM_UFFD_WP) &&
+	    !vma_is_anonymous(vma))
 		return false;
-#endif
 
 	/* By default, allow any of anon|shmem|hugetlb */
 	return vma_is_anonymous(vma) || is_vm_hugetlb_page(vma) ||
