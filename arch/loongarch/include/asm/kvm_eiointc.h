ceb03d20eb50e9cf179bd8f3b1c3fc8e2afce088
Xianglai Li <lixianglai@loongson.cn>
2026-08-25 15:09:35 +0800

    LoongArch: KVM: sync eiointc to upstream
    Synchronize the upstream eiointc implementation logic to keep
    intc/eiointc.c consistent with the upstream
    
    Signed-off-by: Xianglai Li <lixianglai@loongson.cn>
    Signed-off-by: Binbin Zhou <zhoubinbin@loongson.cn>

--- arch/loongarch/include/asm/kvm_eiointc.h
+++ arch/loongarch/include/asm/kvm_eiointc.h
@@ -3,16 +3,17 @@
  * Copyright (C) 2024 Loongson Technology Corporation Limited
  */
 
-#ifndef LOONGARCH_EIOINTC_H
-#define LOONGARCH_EIOINTC_H
+#ifndef __ASM_KVM_EIOINTC_H
+#define __ASM_KVM_EIOINTC_H
 
 #include <kvm/iodev.h>
 
 #define EIOINTC_IRQS			256
 #define EIOINTC_ROUTE_MAX_VCPUS		256
 #define EIOINTC_IRQS_U8_NUMS		(EIOINTC_IRQS / 8)
+#define EIOINTC_IRQS_U16_NUMS		(EIOINTC_IRQS_U8_NUMS / 2)
 #define EIOINTC_IRQS_U32_NUMS		(EIOINTC_IRQS_U8_NUMS / 4)
-#define EIOINTC_IRQS_U64_NUMS		(EIOINTC_IRQS_U32_NUMS / 2)
+#define EIOINTC_IRQS_U64_NUMS		(EIOINTC_IRQS_U8_NUMS / 8)
 /* map to ipnum per 32 irqs */
 #define EIOINTC_IRQS_NODETYPE_COUNT	16
 
@@ -51,7 +52,7 @@
 #define EIOINTC_ENABLE_INT_ENCODE	(2)
 #define EIOINTC_ENABLE_CPU_ENCODE	(3)
 
-#define LS3A_INTC_IP			8
+#define LOONGSON_IP_NUM			8
 
 #define EIOINTC_SW_COREMAP_FLAG		(1 << 0)
 
@@ -68,7 +69,7 @@ struct loongarch_eiointc {
 	union nodetype {
 		u64 reg_u64[EIOINTC_IRQS_NODETYPE_COUNT / 4];
 		u32 reg_u32[EIOINTC_IRQS_NODETYPE_COUNT / 2];
-		uint16_t reg_u16[EIOINTC_IRQS_NODETYPE_COUNT];
+		u16 reg_u16[EIOINTC_IRQS_NODETYPE_COUNT];
 		u8 reg_u8[EIOINTC_IRQS_NODETYPE_COUNT * 2];
 	} nodetype;
 
@@ -76,22 +77,26 @@ struct loongarch_eiointc {
 	union bounce {
 		u64 reg_u64[EIOINTC_IRQS_U64_NUMS];
 		u32 reg_u32[EIOINTC_IRQS_U32_NUMS];
+		u16 reg_u16[EIOINTC_IRQS_U16_NUMS];
 		u8 reg_u8[EIOINTC_IRQS_U8_NUMS];
 	} bounce;
 
 	union isr {
 		u64 reg_u64[EIOINTC_IRQS_U64_NUMS];
 		u32 reg_u32[EIOINTC_IRQS_U32_NUMS];
+		u16 reg_u16[EIOINTC_IRQS_U16_NUMS];
 		u8 reg_u8[EIOINTC_IRQS_U8_NUMS];
 	} isr;
 	union coreisr {
 		u64 reg_u64[EIOINTC_ROUTE_MAX_VCPUS][EIOINTC_IRQS_U64_NUMS];
 		u32 reg_u32[EIOINTC_ROUTE_MAX_VCPUS][EIOINTC_IRQS_U32_NUMS];
+		u16 reg_u16[EIOINTC_ROUTE_MAX_VCPUS][EIOINTC_IRQS_U16_NUMS];
 		u8 reg_u8[EIOINTC_ROUTE_MAX_VCPUS][EIOINTC_IRQS_U8_NUMS];
 	} coreisr;
 	union enable {
 		u64 reg_u64[EIOINTC_IRQS_U64_NUMS];
 		u32 reg_u32[EIOINTC_IRQS_U32_NUMS];
+		u16 reg_u16[EIOINTC_IRQS_U16_NUMS];
 		u8 reg_u8[EIOINTC_IRQS_U8_NUMS];
 	} enable;
 
@@ -99,20 +104,23 @@ struct loongarch_eiointc {
 	union ipmap {
 		u64 reg_u64;
 		u32 reg_u32[EIOINTC_IRQS_U32_NUMS / 4];
+		u16 reg_u16[EIOINTC_IRQS_U16_NUMS / 4];
 		u8 reg_u8[EIOINTC_IRQS_U8_NUMS / 4];
 	} ipmap;
 	/* use one byte to config coremap for one irq */
 	union coremap {
 		u64 reg_u64[EIOINTC_IRQS / 8];
 		u32 reg_u32[EIOINTC_IRQS / 4];
+		u16 reg_u16[EIOINTC_IRQS / 2];
 		u8 reg_u8[EIOINTC_IRQS];
 	} coremap;
 
-	DECLARE_BITMAP(sw_coreisr[EIOINTC_ROUTE_MAX_VCPUS][LS3A_INTC_IP], EIOINTC_IRQS);
+	DECLARE_BITMAP(sw_coreisr[EIOINTC_ROUTE_MAX_VCPUS][LOONGSON_IP_NUM], EIOINTC_IRQS);
 	uint8_t  sw_coremap[EIOINTC_IRQS];
 };
 
-void eiointc_set_irq(struct loongarch_eiointc *s, int irq, int level);
 int kvm_loongarch_register_eiointc_device(void);
+void eiointc_set_irq(struct loongarch_eiointc *s, int irq, int level);
 int kvm_loongarch_reset_eiointc(struct kvm *kvm);
+
 #endif /* LOONGARCH_EIOINTC_H */
