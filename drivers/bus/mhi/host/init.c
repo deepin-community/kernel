telit/linux

drivers: bus: mhi: let userspace manage xfp fw update states
After performing the firmware update in SBL through the XFP
protocol, FN980 resets and is not detected anymore by the host.

Add a new controller file to be used when performing the firmware update
in order to:

- Make the mhi stack aware of the process and not to reset the
function during the update.
- Force a reset of the mhi stack at the end of the process in order to
detect the modem again.

This file should be used both when performing the update through the USB
composition 0x1055 (FN980 old hw revision) that through PCIe (both FN980
and FN990).

For PCIe the flow is the following:

- Disable runtime power management on the mhi pci device
- Write 1 to the fw_update sysfs file to races between
  the recovery work and other mhi operations
- Start the firmware update
- Write 0 to the fw_update sysfs file to reset the mhi stack

Firmware update requires also upstream commit
d651ce8e917fa1bf6cfab8dca74c512edffc35d3
bus: mhi: core: Fix race while handling SYS_ERR at power up

Signed-off-by: Daniele Palmas <dnlplm@gmail.com>

drivers/bus/mhi/host/init.c
@@ -79,6 +79,32 @@ const char *to_mhi_pm_state_str(u32 state)
 	return mhi_pm_state_str[index];
 }
 
+static ssize_t fw_update_show(struct device *dev, struct device_attribute *attr, char *buf)
+{
+	struct mhi_device *mhi_dev = to_mhi_device(dev);
+	struct mhi_controller *mhi_cntrl = mhi_dev->mhi_cntrl;
+
+	return sprintf(buf, "%u\n", mhi_cntrl->xfp);
+}
+
+static ssize_t fw_update_store(struct device *dev,  struct device_attribute *attr, const char *buf, size_t len)
+{
+	struct mhi_device *mhi_dev = to_mhi_device(dev);
+	struct mhi_controller *mhi_cntrl = mhi_dev->mhi_cntrl;
+	bool enable;
+
+	if (strtobool(buf, &enable))
+		return -EINVAL;
+
+	if (enable)
+		mhi_cntrl->xfp = XFP_STATE_FLASHING;
+	else
+		mhi_cntrl->xfp = XFP_STATE_NEED_RESET;
+
+	return len;
+}
+static DEVICE_ATTR_RW(fw_update);
+
 static ssize_t serial_number_show(struct device *dev,
 				  struct device_attribute *attr,
 				  char *buf)
@@ -124,6 +150,7 @@ static struct attribute *mhi_dev_attrs[] = {
 	&dev_attr_serial_number.attr,
 	&dev_attr_oem_pk_hash.attr,
 	&dev_attr_soc_reset.attr,
+	&dev_attr_fw_update.attr,
 	NULL,
 };
 ATTRIBUTE_GROUPS(mhi_dev);

deepin-community/kernel

d651ce8e917fa("bus: mhi: core: Fix race while handling SYS_ERR at power up")
deepin-Intel-SIG: drivers: bus: mhi: let userspace manage xfp fw update states.
Signed-off-by: Wei Qiao <wei.qiao@intel.com>
Signed-off-by: WangYuli <wangyuli@uniontech.com>
(cherry picked from commit be3b98592fca26d86a212f1e3800457cfe8b12e5)
[linux-6.18: change strtobool to kstrtobool]
Link: https://github.com/deepin-community/kernel/pull/64
Signed-off-by: Wentao Guan <guanwentao@uniontech.com>

drivers/bus/mhi/host/init.c
@@ -77,6 +77,32 @@ const char *to_mhi_pm_state_str(u32 state)
 	return mhi_pm_state_str[index];
 }
 
+static ssize_t fw_update_show(struct device *dev, struct device_attribute *attr, char *buf)
+{
+	struct mhi_device *mhi_dev = to_mhi_device(dev);
+	struct mhi_controller *mhi_cntrl = mhi_dev->mhi_cntrl;
+
+	return sprintf(buf, "%u\n", mhi_cntrl->xfp);
+}
+
+static ssize_t fw_update_store(struct device *dev,  struct device_attribute *attr, const char *buf, size_t len)
+{
+	struct mhi_device *mhi_dev = to_mhi_device(dev);
+	struct mhi_controller *mhi_cntrl = mhi_dev->mhi_cntrl;
+	bool enable;
+
+	if (kstrtobool(buf, &enable))
+		return -EINVAL;
+
+	if (enable)
+		mhi_cntrl->xfp = XFP_STATE_FLASHING;
+	else
+		mhi_cntrl->xfp = XFP_STATE_NEED_RESET;
+
+	return len;
+}
+static DEVICE_ATTR_RW(fw_update);
+
 static ssize_t serial_number_show(struct device *dev,
 				  struct device_attribute *attr,
 				  char *buf)
@@ -154,6 +180,7 @@ static struct attribute *mhi_dev_attrs[] = {
 	&dev_attr_serial_number.attr,
 	&dev_attr_oem_pk_hash.attr,
 	&dev_attr_soc_reset.attr,
+	&dev_attr_fw_update.attr,
 	NULL,
 };
 ATTRIBUTE_GROUPS(mhi_dev);