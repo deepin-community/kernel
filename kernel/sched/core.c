2a38ce703f874c4cee396f39a545716fe2ccbc24
Hui Su <sh_def@163.com>
2026-09-02 23:02:09 +0800

    sched/core: Call wq_worker_tick() for the execution context
    [ Upstream commit f5741d2b34519d387edf6e9798fc7030c20a35f3 ]
    
    wq_worker_tick() accounts CPU time and detects CPU-intensive work for
    the kworker that is actually running. With proxy execution, rq->donor
    is the scheduling context while rq->curr is the execution context.
    
    Calling the hook with rq->donor can skip workqueue accounting when a
    kworker is executing on behalf of a donor task. It can also account a
    blocked kworker when the donor is a worker but rq->curr is the task
    actually executing. The former can delay WORKER_CPU_INTENSIVE handling
    and pool concurrency management, which can delay pending kernel work
    and userspace operations depending on it.
    
    Use rq->curr for the workqueue tick hook while retaining rq->donor for
    scheduler accounting.
    
    Fixes: af0c8b2bf67b ("sched: Split scheduler and execution contexts")
    Signed-off-by: Hui Su <sh_def@163.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Acked-by: Tejun Heo <tj@kernel.org>
    Link: https://patch.msgid.link/20260902150208.1209922-2-sh_def@163.com
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    (cherry picked from commit a7bdefb1e00966832b4ca19d9aa0385939d03d5e)
    Signed-off-by: Wentao Guan <guanwentao@uniontech.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -5606,8 +5606,8 @@ void sched_tick(void)
 {
 	int cpu = smp_processor_id();
 	struct rq *rq = cpu_rq(cpu);
-	/* accounting goes to the donor task */
-	struct task_struct *donor;
+	/* scheduler accounting goes to the donor task */
+	struct task_struct *curr, *donor;
 	struct rq_flags rf;
 	unsigned long hw_pressure;
 	u64 resched_latency;
@@ -5618,6 +5618,7 @@ void sched_tick(void)
 	sched_clock_tick();
 
 	rq_lock(rq, &rf);
+	curr = rq->curr;
 	donor = rq->donor;
 
 	psi_account_irqtime(rq, donor, NULL);
@@ -5644,8 +5645,8 @@ void sched_tick(void)
 
 	perf_event_task_tick();
 
-	if (donor->flags & PF_WQ_WORKER)
-		wq_worker_tick(donor);
+	if (curr->flags & PF_WQ_WORKER)
+		wq_worker_tick(curr);
 
 	if (!scx_switched_all()) {
 		rq->idle_balance = idle_cpu(cpu);
