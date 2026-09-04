deepin-community/kernel

commit cac5daf0672731b42080a77786f6418c13c19f65

Revert "LoongArch: Old BPI compatibility"
This reverts commit 29da605493460800aec33f5e012ad47a17ee887e.
Link: https://github.com/deepin-community/kernel/pull/356

arch/loongarch/kernel/reset.c
@@ -49,8 +49,7 @@ void machine_power_off(void)
 #endif
 	do_kernel_power_off();
 #ifdef CONFIG_EFI
-	if (efi.reset_system)
-		efi.reset_system(EFI_RESET_SHUTDOWN, EFI_SUCCESS, 0, NULL);
+	efi.reset_system(EFI_RESET_SHUTDOWN, EFI_SUCCESS, 0, NULL);
 #endif
 
 	while (true) {

arch/loongarch/include/asm/addrspace.h
@@ -142,7 +142,6 @@ extern unsigned long vm_map_base;
 #define PCI_IOSIZE	SZ_32M
 #define ISA_IOSIZE	SZ_16K
 #define IO_SPACE_LIMIT	(PCI_IOSIZE - 1)
-#define ISA_PHY_IOBASE  LOONGSON_LIO_BASE
 
 #define PHYS_LINK_KADDR	PHYSADDR(VMLINUX_LOAD_ADDRESS)
 

arch/loongarch/include/asm/efi.h
@@ -6,7 +6,6 @@
 #define _ASM_LOONGARCH_EFI_H
 
 #include <linux/efi.h>
-#include <asm/early_ioremap.h>
 
 void __init efi_init(void);
 void __init efi_runtime_init(void);

arch/loongarch/kernel/Makefile
@@ -11,7 +11,6 @@ obj-y		+= head.o cpu-probe.o cacheinfo.o env.o setup.o entry.o genex.o \
 		   traps.o irq.o idle.o process.o dma.o mem.o reset.o switch.o \
 		   elf.o syscall.o signal.o time.o topology.o inst.o ptrace.o vdso.o \
 		   alternative.o kdebugfs.o unwind.o
-obj-y		+= legacy_boot.o
 
 obj-$(CONFIG_ACPI)		+= acpi.o
 obj-$(CONFIG_EFI) 		+= efi.o

arch/loongarch/kernel/acpi.c
@@ -20,7 +20,6 @@
 #include <asm/io.h>
 #include <asm/numa.h>
 #include <asm/loongson.h>
-#include "legacy_boot.h"
 
 int acpi_disabled;
 EXPORT_SYMBOL(acpi_disabled);
@@ -89,7 +88,7 @@ void acpi_remove_early_pio(void)
 }
 
 #ifdef CONFIG_SMP
-int set_processor_mask(u32 id, u32 pass)
+static int set_processor_mask(u32 id, u32 pass)
 {
 	int cpu = -1, cpuid = id;
 
@@ -189,10 +188,6 @@ static void __init acpi_process_madt(void)
 		__cpu_logical_map[i] = -1;
 	}
 #endif
-
-	if (efi_bp && bpi_version <= BPI_VERSION_V1)
-		legacy_madt_table_init();
-
 	acpi_table_parse_madt(ACPI_MADT_TYPE_CORE_PIC,
 			acpi_parse_p1_processor, MAX_CORE_PIC);
 

arch/loongarch/kernel/efi.c
@@ -26,7 +26,6 @@
 #include <asm/tlb.h>
 #include <asm/loongson.h>
 #include <asm/pgalloc.h>
-#include "legacy_boot.h"
 
 static unsigned long efi_nr_tables;
 static unsigned long efi_config_table;
@@ -196,9 +195,6 @@ static int __init set_virtual_map(void)
 	efi_set_virtual_address_map_t *svam;
 	efi_memory_desc_t *in, runtime_map[32];
 
-	if (efi_bp)
-		return EFI_SUCCESS;
-
 	size = sizeof(efi_memory_desc_t);
 
 	for_each_efi_memory_desc(in) {
@@ -293,12 +289,10 @@ void __init efi_init(void)
 	void *config_tables;
 	struct efi_boot_memmap *tbl;
 
-	if (efi_system_table)
-		efi_systab = (efi_system_table_t *)early_memremap_ro(efi_system_table,
-						sizeof(*efi_systab));
-	else
-		efi_systab = (efi_system_table_t *)efi_bp->systemtable;
+	if (!efi_system_table)
+		return;
 
+	efi_systab = (efi_system_table_t *)early_memremap_ro(efi_system_table, sizeof(*efi_systab));
 	if (!efi_systab) {
 		pr_err("Can't find EFI system table.\n");
 		return;

arch/loongarch/kernel/env.c
@@ -15,7 +15,6 @@
 #include <asm/loongson.h>
 #include <asm/setup.h>
 #include <asm/time.h>
-#include "legacy_boot.h"
 
 u64 efi_system_table;
 struct loongson_system_configuration loongson_sysconf;
@@ -24,19 +23,13 @@ EXPORT_SYMBOL(loongson_sysconf);
 void __init init_environ(void)
 {
 	int efi_boot = fw_arg0;
-	char *cmdline;
-
-	legacy_boot_init(fw_arg0, fw_arg1, fw_arg2);
-
-	if (efi_bp)
-		return;
+	char *cmdline = early_memremap_ro(fw_arg1, COMMAND_LINE_SIZE);
 
 	if (efi_boot)
 		set_bit(EFI_BOOT, &efi.flags);
 	else
 		clear_bit(EFI_BOOT, &efi.flags);
 
-	cmdline = early_memremap_ro(fw_arg1, COMMAND_LINE_SIZE);
 	strscpy(boot_command_line, cmdline, COMMAND_LINE_SIZE);
 	strscpy(init_command_line, cmdline, COMMAND_LINE_SIZE);
 	early_memunmap(cmdline, COMMAND_LINE_SIZE);

arch/loongarch/kernel/irq.c
@@ -21,7 +21,6 @@
 #include <asm/irq.h>
 #include <asm/loongson.h>
 #include <asm/setup.h>
-#include "legacy_boot.h"
 
 DEFINE_PER_CPU(unsigned long, irq_stack);
 DEFINE_PER_CPU_SHARED_ALIGNED(irq_cpustat_t, irq_stat);
@@ -63,12 +62,6 @@ static int __init early_pci_mcfg_parse(struct acpi_table_header *header)
 	if (header->length < sizeof(struct acpi_table_mcfg))
 		return -EINVAL;
 
-	for (i = 0; i < MAX_IO_PICS; i++) {
-		msi_group[i].pci_segment = -1;
-		msi_group[i].node = -1;
-		pch_group[i].node = -1;
-	}
-
 	n = (header->length - sizeof(struct acpi_table_mcfg)) /
 					sizeof(struct acpi_mcfg_allocation);
 	mcfg = (struct acpi_table_mcfg *)header;
@@ -84,6 +77,14 @@ static int __init early_pci_mcfg_parse(struct acpi_table_header *header)
 
 static void __init init_vec_parent_group(void)
 {
+	int i;
+
+	for (i = 0; i < MAX_IO_PICS; i++) {
+		msi_group[i].pci_segment = -1;
+		msi_group[i].node = -1;
+		pch_group[i].node = -1;
+	}
+
 	acpi_table_parse(ACPI_SIG_MCFG, early_pci_mcfg_parse);
 }
 
@@ -106,7 +107,7 @@ unsigned int arch_dynirq_lower_bound(unsigned int from)
 
 void __init init_IRQ(void)
 {
-	int i, ret;
+	int i;
 	unsigned int order = get_order(IRQ_STACK_SIZE);
 	struct page *page;
 	unsigned long node;
@@ -121,13 +122,7 @@ void __init init_IRQ(void)
 	clear_csr_estat(ESTATF_IP);
 
 	init_vec_parent_group();
-	if (efi_bp && bpi_version <= BPI_VERSION_V1) {
-		ret = setup_legacy_IRQ();
-		if (ret)
-			panic("IRQ domain init error!\n");
-	} else {
-		irqchip_init();
-	}
+	irqchip_init();
 #ifdef CONFIG_SMP
 	mp_ops.init_ipi();
 #endif

commit d924c8eb4aa0ca3d4b94c83449d3940973705faf

LoongArch: Guard EFI shutdown callback
CONFIG_EFI only says EFI support was built in. It does not guarantee
that EFI runtime services were initialized for this boot, so
reset_system() can still be NULL.

Restore the pointer guard before invoking reset_system() during
poweroff.

Assisted-by: Codex:gpt-5.5-xhigh
Assisted-by: GithubCopilot:gpt-5.6-sol # Reported-by
Fixes: cac5daf06727 ("Revert "LoongArch: Old BPI compatibility"")
Signed-off-by: Wentao Guan <guanwentao@uniontech.com>

arch/loongarch/kernel/reset.c
@@ -49,7 +49,8 @@ void machine_power_off(void)
 #endif
 	do_kernel_power_off();
 #ifdef CONFIG_EFI
-	efi.reset_system(EFI_RESET_SHUTDOWN, EFI_SUCCESS, 0, NULL);
+	if (efi.reset_system)
+		efi.reset_system(EFI_RESET_SHUTDOWN, EFI_SUCCESS, 0, NULL);
 #endif
 
 	while (true) {