deepin-community/kernel

commit 3674931621b22397f51e6da94a957516d159baef

arm64: spi: Phytium: Adapt SPI driver to use DDMA interface
Code has been modified and added in the SPI driver section
to adapt to the DDMA controller.

Signed-off-by: yuanxia <yuanxia2073@phytium.com.cn>
Signed-off-by: Liu Tianyu <liutianyu1250@phytium.com.cn>
Signed-off-by: Wang Hanmo <wanghanmo2242@phytium.com.cn>
Signed-off-by: Wang Yinfeng <wangyinfeng@phytium.com.cn>
Link: https://github.com/deepin-community/kernel/pull/136
[ Wentao Guan: by e289df82344f ("spi: Rework per message DMA mapped flag to be per transfer") ]
(cherry picked from commit de1e289ec4840bfb970616654cc1768599b78cf7)
Signed-off-by: Wentao Guan <guanwentao@uniontech.com>

drivers/spi/spi-phytium-dma.c
@@ -1,0 +1,553 @@
+// SPDX-License-Identifier: GPL-2.0-only
+/*
+ * Special handling for phytium DMA core
+ *
+ * Copyright (c) 2019-2023, Phytium Technology Co., Ltd..
+ */
+#include <linux/completion.h>
+#include <linux/dma-mapping.h>
+#include <linux/dmaengine.h>
+#include <linux/irqreturn.h>
+#include <linux/jiffies.h>
+#include <linux/pci.h>
+#include <linux/spi/spi.h>
+#include <linux/types.h>
+#include <linux/delay.h>
+#include "spi-phytium.h"
+
+#define RX_BUSY		0
+#define RX_BURST_LEVEL	16
+#define TX_BUSY		1
+#define TX_BURST_LEVEL	16
+
+#define DMA_MAX_BUF_SIZE	4096
+
+static void phytium_spi_dma_maxburst_init(struct phytium_spi *fts)
+{
+	struct dma_slave_caps caps;
+	u32 max_burst, def_burst;
+	int ret;
+
+	def_burst = fts->fifo_len / 2;
+
+	ret = dma_get_slave_caps(fts->rxchan, &caps);
+	if (!ret && caps.max_burst)
+		max_burst = caps.max_burst;
+	else
+		max_burst = RX_BURST_LEVEL;
+
+	fts->rxburst = min(max_burst, def_burst);
+	phytium_writel(fts, DMARDLR, 0x0);
+
+	ret = dma_get_slave_caps(fts->txchan, &caps);
+	if (!ret && caps.max_burst)
+		max_burst = caps.max_burst;
+	else
+		max_burst = TX_BURST_LEVEL;
+
+	/*
+	 * Having a Rx DMA channel serviced with higher priority than a Tx DMA
+	 * channel might not be enough to provide a well balanced DMA-based
+	 * SPI transfer interface. There might still be moments when the Tx DMA
+	 * channel is occasionally handled faster than the Rx DMA channel.
+	 * That in its turn will eventually cause the SPI Rx FIFO overflow if
+	 * SPI bus speed is high enough to fill the SPI Rx FIFO in before it's
+	 * cleared by the Rx DMA channel. In order to fix the problem the Tx
+	 * DMA activity is intentionally slowed down by limiting the SPI Tx
+	 * FIFO depth with a value twice bigger than the Tx burst length.
+	 */
+	fts->txburst = min(max_burst, def_burst);
+	/* set dmatdlr to 0 + 1 */
+	phytium_writel(fts, DMATDLR, 0);
+}
+
+static int phytium_spi_dma_init(struct device *dev,
+		struct phytium_spi *fts)
+{
+	fts->rxchan = dma_request_chan(dev, "rx");
+	if (IS_ERR_OR_NULL(fts->rxchan))
+		return -ENODEV;
+
+	fts->txchan = dma_request_chan(dev, "tx");
+	if (IS_ERR_OR_NULL(fts->txchan)) {
+		dev_err(dev, "can't request chan\n");
+		dma_release_channel(fts->rxchan);
+		fts->rxchan = NULL;
+		return -ENODEV;
+	}
+
+	fts->master->dma_rx = fts->rxchan;
+	fts->master->dma_tx = fts->txchan;
+	init_completion(&fts->dma_completion);
+
+	phytium_spi_dma_maxburst_init(fts);
+	fts->dma_sg_burst = 0;
+
+	return 0;
+}
+
+static void phytium_spi_dma_exit(struct phytium_spi *fts)
+{
+	if (fts->txchan) {
+		dmaengine_terminate_sync(fts->txchan);
+		dma_release_channel(fts->txchan);
+	}
+
+	if (fts->rxchan) {
+		dmaengine_terminate_sync(fts->rxchan);
+		dma_release_channel(fts->rxchan);
+	}
+}
+
+static irqreturn_t phytium_spi_dma_transfer_handler
+(struct phytium_spi *fts)
+{
+	phytium_spi_check_status(fts, false);
+
+	complete(&fts->dma_completion);
+
+	return IRQ_HANDLED;
+}
+
+static bool phytium_spi_can_dma(struct spi_controller *master,
+			   struct spi_device *spi, struct spi_transfer *xfer)
+{
+	struct phytium_spi *fts = spi_controller_get_devdata(master);
+
+	return xfer->len > fts->fifo_len;
+}
+
+static enum dma_slave_buswidth phytium_spi_dma_convert_width(u8 n_bytes)
+{
+	if (n_bytes == 1)
+		return DMA_SLAVE_BUSWIDTH_1_BYTE;
+	else if (n_bytes == 2)
+		return DMA_SLAVE_BUSWIDTH_2_BYTES;
+
+	return DMA_SLAVE_BUSWIDTH_UNDEFINED;
+}
+
+static int phytium_spi_dma_wait(struct phytium_spi *fts, unsigned int len,
+					u32 speed)
+{
+	unsigned long long ms;
+
+	ms = len * MSEC_PER_SEC * BITS_PER_BYTE;
+	do_div(ms, speed);
+	ms += ms + 200;
+
+	if (ms > UINT_MAX)
+		ms = UINT_MAX;
+
+	ms = wait_for_completion_timeout(&fts->dma_completion,
+					 msecs_to_jiffies(ms));
+
+	if (ms == 0) {
+		dev_err(&fts->master->cur_msg->spi->dev,
+			"DMA transaction timed out\n");
+		return -ETIMEDOUT;
+	}
+
+	return 0;
+}
+
+static inline bool phytium_spi_dma_tx_busy(struct phytium_spi *fts)
+{
+	return !(phytium_readl(fts, SR) & SR_TF_EMPT);
+}
+
+static void spi_transfer_delay_ns(u32 ns)
+{
+	if (!ns)
+		return;
+	if (ns <= 1000) {
+		ndelay(ns);
+	} else {
+		u32 us = DIV_ROUND_UP(ns, 1000);
+
+		if (us <= 10)
+			udelay(us);
+		else
+			usleep_range(us, us + DIV_ROUND_UP(us, 10));
+	}
+}
+
+static int phytium_spi_dma_wait_tx_done(struct phytium_spi *fts,
+				   struct spi_transfer *xfer)
+{
+	int retry = SPI_WAIT_RETRIES;
+	u32 ns = 0;
+	u32 nents = 0;
+
+	nents = phytium_readl(fts, TXFLR);
+	ns = nents * fts->n_bytes * BITS_PER_BYTE;
+	ns *= DIV_ROUND_UP(1000000000, xfer->speed_hz / 2);
+
+	while (phytium_spi_dma_tx_busy(fts) && retry--)
+		spi_transfer_delay_ns(ns);
+
+	if (retry < 0) {
+		dev_err(&fts->master->dev, "Tx hanged up\n");
+		return -EIO;
+	}
+
+	return 0;
+}
+
+/*
+ * fts->dma_chan_busy is set before the dma transfer starts,
+ * callback for tx
+ * channel will clear a corresponding bit.
+ */
+static void phytium_spi_dma_tx_done(void *arg)
+{
+	struct phytium_spi *fts = arg;
+
+	clear_bit(TX_BUSY, &fts->dma_chan_busy);
+	if (test_bit(RX_BUSY, &fts->dma_chan_busy))
+		return;
+
+	complete(&fts->dma_completion);
+}
+
+static int phytium_spi_dma_config_tx(struct phytium_spi *fts)
+{
+	struct dma_slave_config txconf;
+
+	memset(&txconf, 0, sizeof(txconf));
+	txconf.direction = DMA_MEM_TO_DEV;
+	txconf.dst_addr = fts->dma_addr;
+	txconf.dst_maxburst = fts->txburst;
+	txconf.src_addr_width = DMA_SLAVE_BUSWIDTH_4_BYTES;
+	txconf.dst_addr_width = phytium_spi_dma_convert_width(fts->n_bytes);
+	txconf.device_fc = false;
+
+	return dmaengine_slave_config(fts->txchan, &txconf);
+}
+
+static int phytium_spi_dma_submit_tx(struct phytium_spi *fts,
+	struct scatterlist *sgl, unsigned int nents)
+{
+	struct dma_async_tx_descriptor *txdesc;
+	dma_cookie_t cookie;
+	int ret;
+
+	txdesc = dmaengine_prep_slave_sg(fts->txchan, sgl, nents,
+					 DMA_MEM_TO_DEV,
+					 DMA_PREP_INTERRUPT | DMA_CTRL_ACK);
+	if (!txdesc)
+		return -ENOMEM;
+
+	txdesc->callback = phytium_spi_dma_tx_done;
+	txdesc->callback_param = fts;
+
+	cookie = dmaengine_submit(txdesc);
+	ret = dma_submit_error(cookie);
+	if (ret) {
+		dmaengine_terminate_sync(fts->txchan);
+		return ret;
+	}
+
+	set_bit(TX_BUSY, &fts->dma_chan_busy);
+
+	return 0;
+}
+
+static inline bool phytium_spi_dma_rx_busy(struct phytium_spi *fts)
+{
+	return !!(phytium_readl(fts, SR) & SR_RF_NOT_EMPT);
+}
+
+static int phytium_spi_dma_wait_rx_done(struct phytium_spi *fts)
+{
+	int retry = SPI_WAIT_RETRIES;
+	unsigned long ns = 0;
+	u32 nents = 0;
+
+	/*
+	 * It's unlikely that DMA engine is still doing the data fetching, but
+	 * if it's let's give it some reasonable time. The timeout calculation
+	 * is based on the synchronous APB/SSI reference clock rate, on a
+	 * number of data entries left in the Rx FIFO, times a number of clock
+	 * periods normally needed for a single APB read/write transaction
+	 * without PREADY signal utilized (which is true for the phytium APB SSI
+	 * controller).
+	 */
+	nents = phytium_readl(fts, RXFLR);
+	ns = 4U * NSEC_PER_SEC / fts->max_freq * nents;
+
+	while (phytium_spi_dma_rx_busy(fts) && retry--)
+		spi_transfer_delay_ns(ns);
+
+	if (retry < 0) {
+		dev_err(&fts->master->dev, "Rx hanged up, nents = %d\n", nents);
+		return -EIO;
+	}
+
+	return 0;
+}
+
+/*
+ * fts->dma_chan_busy is set before the dma transfer starts,
+ * callback for rx
+ * channel will clear a corresponding bit.
+ */
+static void phytium_spi_dma_rx_done(void *arg)
+{
+	struct phytium_spi *fts = arg;
+
+	clear_bit(RX_BUSY, &fts->dma_chan_busy);
+	if (test_bit(TX_BUSY, &fts->dma_chan_busy))
+		return;
+
+	complete(&fts->dma_completion);
+}
+
+static int phytium_spi_dma_config_rx(struct phytium_spi *fts)
+{
+	struct dma_slave_config rxconf;
+
+	memset(&rxconf, 0, sizeof(rxconf));
+	rxconf.direction = DMA_DEV_TO_MEM;
+	rxconf.src_addr = fts->dma_addr;
+	rxconf.src_maxburst = fts->rxburst;
+	rxconf.dst_addr_width = DMA_SLAVE_BUSWIDTH_4_BYTES;
+	rxconf.src_addr_width = phytium_spi_dma_convert_width(fts->n_bytes);
+	rxconf.device_fc = false;
+
+	return dmaengine_slave_config(fts->rxchan, &rxconf);
+}
+
+static int phytium_spi_dma_submit_rx(struct phytium_spi *fts,
+	struct scatterlist *sgl, unsigned int nents)
+{
+	struct dma_async_tx_descriptor *rxdesc;
+	dma_cookie_t cookie;
+	int ret;
+
+	rxdesc = dmaengine_prep_slave_sg(fts->rxchan, sgl, nents,
+					 DMA_DEV_TO_MEM,
+					 DMA_PREP_INTERRUPT | DMA_CTRL_ACK);
+	if (!rxdesc)
+		return -ENOMEM;
+
+	rxdesc->callback = phytium_spi_dma_rx_done;
+	rxdesc->callback_param = fts;
+
+	cookie = dmaengine_submit(rxdesc);
+	ret = dma_submit_error(cookie);
+	if (ret) {
+		dmaengine_terminate_sync(fts->rxchan);
+		return ret;
+	}
+
+	set_bit(RX_BUSY, &fts->dma_chan_busy);
+
+	return 0;
+}
+
+static int phytium_spi_dma_setup(struct phytium_spi *fts,
+		struct spi_transfer *xfer)
+{
+	u16 imr, dma_ctrl;
+	int ret;
+
+	if (!xfer->tx_buf)
+		return -EINVAL;
+
+	/* Setup DMA channels */
+	ret = phytium_spi_dma_config_tx(fts);
+	if (ret)
+		return ret;
+
+	if (xfer->rx_buf) {
+		ret = phytium_spi_dma_config_rx(fts);
+		if (ret)
+			return ret;
+	}
+
+	/* Set the DMA handshaking interface */
+	dma_ctrl = SPI_DMA_TDMAE;
+	if (xfer->rx_buf)
+		dma_ctrl |= SPI_DMA_RDMAE;
+	phytium_writel(fts, DMACR, dma_ctrl);
+
+	/* Set the interrupt mask */
+	imr = INT_TXOI;
+	if (xfer->rx_buf)
+		imr |= INT_RXUI | INT_RXOI;
+
+	spi_umask_intr(fts, imr);
+
+	reinit_completion(&fts->dma_completion);
+
+	fts->transfer_handler = phytium_spi_dma_transfer_handler;
+
+	return 0;
+}
+
+static int phytium_spi_dma_transfer_all(struct phytium_spi *fts,
+				   struct spi_transfer *xfer)
+{
+	int ret;
+
+	/* Submit the DMA Tx transfer */
+	ret = phytium_spi_dma_submit_tx(fts, xfer->tx_sg.sgl,
+			xfer->tx_sg.nents);
+	if (ret)
+		goto err_clear_dmac;
+
+	/* Submit the DMA Rx transfer if required */
+	if (xfer->rx_buf) {
+		ret = phytium_spi_dma_submit_rx(fts, xfer->rx_sg.sgl,
+					   xfer->rx_sg.nents);
+		if (ret)
+			goto err_clear_dmac;
+
+		/* rx must be started before tx due to spi instinct */
+		dma_async_issue_pending(fts->rxchan);
+	}
+
+	dma_async_issue_pending(fts->txchan);
+
+	ret = phytium_spi_dma_wait(fts, xfer->len, xfer->speed_hz);
+
+err_clear_dmac:
+	phytium_writel(fts, DMACR, 0);
+
+	return ret;
+}
+
+static int phytium_spi_dma_transfer_one(struct phytium_spi *fts,
+				   struct spi_transfer *xfer)
+{
+	struct scatterlist *tx_sg = NULL, *rx_sg = NULL, tx_tmp, rx_tmp;
+	unsigned int tx_len = 0, rx_len = 0;
+	unsigned int base, len;
+	int ret;
+
+	sg_init_table(&tx_tmp, 1);
+	sg_init_table(&rx_tmp, 1);
+
+	for (base = 0, len = 0; base < xfer->len; base += len) {
+		/* Fetch next Tx DMA data chunk */
+		if (!tx_len) {
+			tx_sg = !tx_sg ? &xfer->tx_sg.sgl[0] : sg_next(tx_sg);
+			sg_dma_address(&tx_tmp) = sg_dma_address(tx_sg);
+			tx_len = sg_dma_len(tx_sg);
+		}
+
+		/* Fetch next Rx DMA data chunk */
+		if (!rx_len) {
+			rx_sg = !rx_sg ? &xfer->rx_sg.sgl[0] : sg_next(rx_sg);
+			sg_dma_address(&rx_tmp) = sg_dma_address(rx_sg);
+			rx_len = sg_dma_len(rx_sg);
+		}
+
+		if ((base + DMA_MAX_BUF_SIZE) > xfer->len)
+			len = xfer->len - base;
+		else
+			len = DMA_MAX_BUF_SIZE;
+
+		len = min3(len, tx_len, rx_len);
+
+		sg_dma_len(&tx_tmp) = len;
+		sg_dma_len(&rx_tmp) = len;
+
+		/* Submit DMA Tx transfer */
+		ret = phytium_spi_dma_submit_tx(fts, &tx_tmp, 1);
+		if (ret)
+			break;
+
+		/* Submit DMA Rx transfer */
+		ret = phytium_spi_dma_submit_rx(fts, &rx_tmp, 1);
+		if (ret)
+			break;
+
+		/* Rx must be started before Tx due to SPI instinct */
+		dma_async_issue_pending(fts->rxchan);
+
+		dma_async_issue_pending(fts->txchan);
+
+		/*
+		 * Here we only need to wait for the DMA transfer to be
+		 * finished since SPI controller is kept enabled during the
+		 * procedure this loop implements and there is no risk to lose
+		 * data left in the Tx/Rx FIFOs.
+		 */
+		ret = phytium_spi_dma_wait(fts, len, xfer->speed_hz);
+		if (ret)
+			break;
+
+		reinit_completion(&fts->dma_completion);
+
+		sg_dma_address(&tx_tmp) += len;
+		sg_dma_address(&rx_tmp) += len;
+		tx_len -= len;
+		rx_len -= len;
+	}
+
+	phytium_writel(fts, DMACR, 0);
+
+	return ret;
+}
+
+static int phytium_spi_dma_transfer(struct phytium_spi *fts,
+		struct spi_transfer *xfer)
+{
+	unsigned int nents;
+	int ret;
+
+	nents = max(xfer->tx_sg.nents, xfer->rx_sg.nents);
+
+	/*
+	 * large transfer length caused spi RX FIFO full event
+	 * transfer 4096 bytes each time
+	 */
+	if (xfer->len <= DMA_MAX_BUF_SIZE)
+		ret = phytium_spi_dma_transfer_all(fts, xfer);
+	else
+		ret = phytium_spi_dma_transfer_one(fts, xfer);
+	if (ret)
+		return ret;
+
+	if (fts->master->cur_msg->status == -EINPROGRESS) {
+		ret = phytium_spi_dma_wait_tx_done(fts, xfer);
+		if (ret)
+			return ret;
+	}
+
+	if (xfer->rx_buf && fts->master->cur_msg->status == -EINPROGRESS)
+		ret = phytium_spi_dma_wait_rx_done(fts);
+
+	return ret;
+}
+
+static void phytium_spi_dma_stop(struct phytium_spi *fts)
+{
+	if (test_bit(TX_BUSY, &fts->dma_chan_busy)) {
+		dmaengine_terminate_sync(fts->txchan);
+		clear_bit(TX_BUSY, &fts->dma_chan_busy);
+	}
+	if (test_bit(RX_BUSY, &fts->dma_chan_busy)) {
+		dmaengine_terminate_sync(fts->rxchan);
+		clear_bit(RX_BUSY, &fts->dma_chan_busy);
+	}
+}
+
+static const struct phytium_spi_dma_ops phytium_spi_dma_generic_ops = {
+	.dma_init	= phytium_spi_dma_init,
+	.dma_exit	= phytium_spi_dma_exit,
+	.dma_setup	= phytium_spi_dma_setup,
+	.can_dma	= phytium_spi_can_dma,
+	.dma_transfer	= phytium_spi_dma_transfer,
+	.dma_stop	= phytium_spi_dma_stop,
+};
+
+void phytium_spi_dmaops_set(struct phytium_spi *fts)
+{
+	fts->dma_ops = &phytium_spi_dma_generic_ops;
+}
+EXPORT_SYMBOL_GPL(phytium_spi_dmaops_set);
+
+MODULE_LICENSE("GPL");

drivers/spi/Makefile
@@ -116,6 +116,7 @@ obj-$(CONFIG_SPI_PHYTIUM)		+= spi-phytium.o
 obj-$(CONFIG_SPI_PHYTIUM_PLAT)		+= spi-phytium-plat.o
 obj-$(CONFIG_SPI_PHYTIUM_PCI)		+= spi-phytium-pci.o
 obj-$(CONFIG_SPI_PHYTIUM_QSPI)		+= spi-phytium-qspi.o
+obj-$(CONFIG_SPI_PHYTIUM)               += spi-phytium-dma.o
 obj-$(CONFIG_SPI_PIC32)			+= spi-pic32.o
 obj-$(CONFIG_SPI_PIC32_SQI)		+= spi-pic32-sqi.o
 obj-$(CONFIG_SPI_PL022)			+= spi-pl022.o

drivers/spi/spi-phytium-plat.c
@@ -56,6 +56,7 @@ static int phytium_spi_probe(struct platform_device *pdev)
 		return -EINVAL;
 	}
 
+	fts->paddr = mem->start;
 	fts->regs = devm_ioremap_resource(&pdev->dev, mem);
 	if (IS_ERR(fts->regs)) {
 		dev_err(&pdev->dev, "SPI region map failed\n");
@@ -86,13 +87,19 @@ static int phytium_spi_probe(struct platform_device *pdev)
 	device_property_read_u32(&pdev->dev, "reg-io-width", &fts->reg_io_width);
 
 	num_cs = 4;
-
 	device_property_read_u32(&pdev->dev, "num-cs", &num_cs);
-
 	fts->num_cs = num_cs;
 
 	device_property_read_u32(&pdev->dev, "global-cs", &global_cs);
 	fts->global_cs = global_cs;
+
+	/* check is use dma transfer */
+	if ((device_property_read_string_array(&pdev->dev, "dma-names",
+			NULL, 0) > 0) &&
+		device_property_present(&pdev->dev, "dmas")) {
+		fts->dma_en = true;
+		phytium_spi_dmaops_set(fts);
+	}
 
 	ret = phytium_spi_add_host(&pdev->dev, fts);
 	if (ret)

drivers/spi/spi-phytium.c
@@ -25,102 +25,6 @@
 #include <linux/acpi.h>
 #include "spi-phytium.h"
 
-static inline u32 phytium_readl(struct phytium_spi *fts, u32 offset)
-{
-	return __raw_readl(fts->regs + offset);
-}
-
-static inline u16 phytium_readw(struct phytium_spi *fts, u32 offset)
-{
-	return __raw_readw(fts->regs + offset);
-}
-
-static inline void phytium_writel(struct phytium_spi *fts, u32 offset, u32 val)
-{
-	__raw_writel(val, fts->regs + offset);
-}
-
-static inline void phytium_writew(struct phytium_spi *fts, u32 offset, u16 val)
-{
-	__raw_writew(val, fts->regs + offset);
-}
-
-static inline u32 phytium_read_io_reg(struct phytium_spi *fts, u32 offset)
-{
-	switch (fts->reg_io_width) {
-	case 2:
-		return phytium_readw(fts, offset);
-	case 4:
-	default:
-		return phytium_readl(fts, offset);
-	}
-}
-
-static inline void phytium_write_io_reg(struct phytium_spi *fts, u32 offset, u32 val)
-{
-	switch (fts->reg_io_width) {
-	case 2:
-		phytium_writew(fts, offset, val);
-		break;
-	case 4:
-	default:
-		phytium_writel(fts, offset, val);
-		break;
-	}
-}
-
-static inline void spi_enable_chip(struct phytium_spi *fts, int enable)
-{
-	phytium_writel(fts, SSIENR, (enable ? 1 : 0));
-}
-
-static inline void spi_set_clk(struct phytium_spi *fts, u16 div)
-{
-	phytium_writel(fts, BAUDR, div);
-}
-
-static inline void spi_mask_intr(struct phytium_spi *fts, u32 mask)
-{
-	u32 new_mask;
-
-	new_mask = phytium_readl(fts, IMR) & ~mask;
-	phytium_writel(fts, IMR, new_mask);
-}
-
-static inline void spi_umask_intr(struct phytium_spi *fts, u32 mask)
-{
-	u32 new_mask;
-
-	new_mask = phytium_readl(fts, IMR) | mask;
-	phytium_writel(fts, IMR, new_mask);
-}
-
-static inline void spi_global_cs(struct phytium_spi *fts)
-{
-	u32 global_cs_en, mask, setmask;
-
-	mask = GENMASK(fts->num_cs-1, 0) << fts->num_cs;
-	setmask = ~GENMASK(fts->num_cs-1, 0);
-	global_cs_en = (phytium_readl(fts, GCSR) | mask) & setmask;
-
-	phytium_writel(fts, GCSR, global_cs_en);
-}
-
-static inline void spi_reset_chip(struct phytium_spi *fts)
-{
-	spi_enable_chip(fts, 0);
-	if (fts->global_cs)
-		spi_global_cs(fts);
-	spi_mask_intr(fts, 0xff);
-	spi_enable_chip(fts, 1);
-}
-
-static inline void spi_shutdown_chip(struct phytium_spi *fts)
-{
-	spi_enable_chip(fts, 0);
-	spi_set_clk(fts, 0);
-}
-
 struct phytium_spi_chip {
 	u8 poll_mode;
 	u8 type;
@@ -215,6 +119,40 @@ static void phytium_reader(struct phytium_spi *fts)
 		fts->rx += fts->n_bytes;
 	}
 }
+int phytium_spi_check_status(struct phytium_spi *fts, bool raw)
+{
+	u32 irq_status;
+	int ret = 0;
+
+	if (raw)
+		irq_status = phytium_readl(fts, RISR);
+	else
+		irq_status = phytium_readl(fts, ISR);
+
+	if (irq_status & INT_RXOI) {
+		dev_err(&fts->master->dev, "RX FIFO overflow detected\n");
+		ret = -EIO;
+	}
+
+	if (irq_status & INT_RXUI) {
+		dev_err(&fts->master->dev, "RX FIFO underflow detected\n");
+		ret = -EIO;
+	}
+
+	if (irq_status & INT_TXOI) {
+		dev_err(&fts->master->dev, "TX FIFO overflow detected\n");
+		ret = -EIO;
+	}
+
+	/* Generically handle the erroneous situation */
+	if (ret) {
+		spi_reset_chip(fts);
+		if (fts->master->cur_msg)
+			fts->master->cur_msg->status = ret;
+	}
+	return ret;
+}
+EXPORT_SYMBOL_GPL(phytium_spi_check_status);
 
 static void int_error_stop(struct phytium_spi *fts, const char *msg)
 {
@@ -290,7 +228,9 @@ static int phytium_spi_transfer_one(struct spi_controller *master,
 	u16 txlevel = 0;
 	u16 clk_div;
 	u32 cr0;
+	int ret = 0;
 
+	fts->dma_mapped = 0;
 	fts->tx = (void *)transfer->tx_buf;
 	fts->tx_end = fts->tx + transfer->len;
 	fts->rx = transfer->rx_buf;
@@ -299,12 +239,15 @@ static int phytium_spi_transfer_one(struct spi_controller *master,
 
 	spi_enable_chip(fts, 0);
 
-	if (transfer->speed_hz != chip->speed_hz) {
-		clk_div = (fts->max_freq / transfer->speed_hz + 1) & 0xfffe;
+	if (transfer->speed_hz != fts->current_freq) {
+		if (transfer->speed_hz != chip->speed_hz) {
+			clk_div = (fts->max_freq / transfer->speed_hz + 1) &
+				0xfffe;
 
-		chip->speed_hz = transfer->speed_hz;
-		chip->clk_div = clk_div;
-
+			chip->speed_hz = transfer->speed_hz;
+			chip->clk_div = clk_div;
+		}
+		fts->current_freq = transfer->speed_hz;
 		spi_set_clk(fts, chip->clk_div);
 	}
 
@@ -332,11 +275,22 @@ static int phytium_spi_transfer_one(struct spi_controller *master,
 		cr0 |= (chip->tmode << TMOD_OFFSET);
 	}
 
-	phytium_writel(fts, CTRL0, cr0);
+	phytium_writel(fts, CTRLR0, cr0);
+
+	/* check if current transfer is a DMA transcation */
+	if (master->can_dma && master->can_dma(master, spi, transfer))
+		fts->dma_mapped = (transfer->tx_sg_mapped || transfer->rx_sg_mapped);
 
 	spi_mask_intr(fts, 0xff);
 
-	if (!chip->poll_mode) {
+	/* DMA setup */
+	if (fts->dma_mapped) {
+		ret = fts->dma_ops->dma_setup(fts, transfer);
+		if (ret)
+			return ret;
+	}
+	/* interrupt transfer mode setup */
+	if (!chip->poll_mode && !fts->dma_mapped) {
 		txlevel = min_t(u16, fts->fifo_len / 2, fts->len / fts->n_bytes);
 		phytium_writel(fts, TXFLTR, txlevel);
 
@@ -349,6 +303,9 @@ static int phytium_spi_transfer_one(struct spi_controller *master,
 
 	spi_enable_chip(fts, 1);
 
+	if (fts->dma_mapped)
+		return fts->dma_ops->dma_transfer(fts, transfer);
+
 	if (chip->poll_mode)
 		return poll_transfer(fts);
 
@@ -359,6 +316,9 @@ static void phytium_spi_handle_err(struct spi_controller *master,
 		struct spi_message *msg)
 {
 	struct phytium_spi *fts = spi_controller_get_devdata(master);
+
+	if (fts->dma_mapped)
+		fts->dma_ops->dma_stop(fts);
 
 	spi_reset_chip(fts);
 }
@@ -396,7 +356,7 @@ static int phytium_spi_setup(struct spi_device *spi)
 	cr0 = (spi->bits_per_word - 1) | (chip->type << FRF_OFFSET) |
 	      (spi->mode << MODE_OFFSET) | (chip->tmode << TMOD_OFFSET);
 
-	phytium_writel(fts, CTRL0, cr0);
+	phytium_writel(fts, CTRLR0, cr0);
 
 	spi_enable_chip(fts, 1);
 
@@ -442,9 +402,11 @@ int phytium_spi_add_host(struct device *dev, struct phytium_spi *fts)
 		return -ENOMEM;
 
 	fts->master = master;
+	fts->dma_addr = (dma_addr_t)(fts->paddr + DR);
 	snprintf(fts->name, sizeof(fts->name), "phytium_spi%d", fts->bus_num);
 
-	ret = request_irq(fts->irq, phytium_spi_irq, IRQF_SHARED, fts->name, master);
+	ret = request_irq(fts->irq, phytium_spi_irq, IRQF_SHARED,
+			fts->name, master);
 	if (ret < 0) {
 		dev_err(dev, "can not get IRQ\n");
 		goto err_free_master;
@@ -467,6 +429,16 @@ int phytium_spi_add_host(struct device *dev, struct phytium_spi *fts)
 
 	spi_hw_init(dev, fts);
 
+	if (fts->dma_ops && fts->dma_ops->dma_init) {
+		ret = fts->dma_ops->dma_init(dev, fts);
+		if (ret) {
+			dev_warn(dev, "DMA init failed\n");
+		} else {
+			master->can_dma = fts->dma_ops->can_dma;
+			master->flags |= SPI_CONTROLLER_MUST_TX;
+		}
+	}
+
 	spi_controller_set_devdata(master, fts);
 	ret = spi_register_controller(master);
 	if (ret) {
@@ -477,6 +449,8 @@ int phytium_spi_add_host(struct device *dev, struct phytium_spi *fts)
 	return 0;
 
 err_exit:
+	if (fts->dma_ops && fts->dma_ops->dma_exit)
+		fts->dma_ops->dma_exit(fts);
 	spi_enable_chip(fts, 0);
 	free_irq(fts->irq, master);
 err_free_master:
@@ -487,6 +461,8 @@ EXPORT_SYMBOL_GPL(phytium_spi_add_host);
 
 void phytium_spi_remove_host(struct phytium_spi *fts)
 {
+	if (fts->dma_ops && fts->dma_ops->dma_exit)
+		fts->dma_ops->dma_exit(fts);
 	spi_shutdown_chip(fts);
 
 	spi_unregister_controller(fts->master);

drivers/spi/spi-phytium.h
@@ -11,16 +11,21 @@
 #include <linux/scatterlist.h>
 #include <linux/gpio.h>
 
-#define CTRL0			0x00
+#define CTRLR0			0x00
 #define SSIENR			0x08
 #define SER			0x10
 #define BAUDR			0x14
 #define TXFLTR			0x18
 #define TXFLR			0x20
 #define RXFLR			0x24
+#define SR			0x28
 #define IMR			0x2c
 #define ISR			0x30
+#define RISR			0x34
 #define ICR			0x48
+#define DMACR			0x4C
+#define DMATDLR			0x50
+#define DMARDLR			0x54
 #define DR			0x60
 #define GCSR			0x100
 
@@ -37,6 +42,29 @@
 #define INT_TXOI		(1 << 1)
 #define INT_RXUI		(1 << 2)
 #define INT_RXOI		(1 << 3)
+/* Bit fields in SR, 7 bits */
+#define SR_MASK			0x7f		/* cover 7 bits */
+#define SR_BUSY			(1 << 0)
+#define SR_TF_NOT_FULL		(1 << 1)
+#define SR_TF_EMPT		(1 << 2)
+#define SR_RF_NOT_EMPT		(1 << 3)
+#define SR_RF_FULL		(1 << 4)
+#define SR_TX_ERR		(1 << 5)
+#define SR_DCOL			(1 << 6)
+/* Bit fields in DMACR */
+#define SPI_DMA_RDMAE		(1 << 0)
+#define SPI_DMA_TDMAE		(1 << 1)
+#define SPI_WAIT_RETRIES	5
+struct phytium_spi;
+struct phytium_spi_dma_ops {
+	int (*dma_init)(struct device *dev, struct phytium_spi *fts);
+	void (*dma_exit)(struct phytium_spi *fts);
+	int (*dma_setup)(struct phytium_spi *fts, struct spi_transfer *xfer);
+	bool (*can_dma)(struct spi_controller *master, struct spi_device *spi,
+			struct spi_transfer *xfer);
+	int (*dma_transfer)(struct phytium_spi *fts, struct spi_transfer *xfer);
+	void (*dma_stop)(struct phytium_spi *fts);
+};
 
 struct phytium_spi {
 	struct spi_controller	*master;
@@ -44,6 +72,7 @@ struct phytium_spi {
 
 	void __iomem		*regs;
 	bool			global_cs;
+	bool			dma_en;
 	unsigned long		paddr;
 	int			irq;
 	u32			fifo_len;
@@ -60,12 +89,109 @@ struct phytium_spi {
 	void			*rx;
 	void			*rx_end;
 	u8			n_bytes;
+	int			dma_mapped;
 	irqreturn_t		(*transfer_handler)(struct phytium_spi *fts);
+	/* DMA info */
+	u32			current_freq; /* frequency in hz */
+	struct dma_chan		*txchan;
+	u32			txburst;
+	struct dma_chan		*rxchan;
+	u32			rxburst;
+	u32			dma_sg_burst;
+	unsigned long		dma_chan_busy;
+	dma_addr_t		dma_addr; /* phy address of the Data register */
+	const struct phytium_spi_dma_ops *dma_ops;
+	struct completion	dma_completion;
 };
 
+static inline u32 phytium_readl(struct phytium_spi *fts, u32 offset)
+{
+	return __raw_readl(fts->regs + offset);
+}
+static inline u16 phytium_readw(struct phytium_spi *fts, u32 offset)
+{
+	return __raw_readw(fts->regs + offset);
+}
+static inline void phytium_writel(struct phytium_spi *fts, u32 offset, u32 val)
+{
+	__raw_writel(val, fts->regs + offset);
+}
+static inline void phytium_writew(struct phytium_spi *fts, u32 offset, u16 val)
+{
+	__raw_writew(val, fts->regs + offset);
+}
+static inline u32 phytium_read_io_reg(struct phytium_spi *fts, u32 offset)
+{
+	switch (fts->reg_io_width) {
+	case 2:
+		return phytium_readw(fts, offset);
+	case 4:
+	default:
+		return phytium_readl(fts, offset);
+	}
+}
+static inline void phytium_write_io_reg(struct phytium_spi *fts,
+				u32 offset, u32 val)
+{
+	switch (fts->reg_io_width) {
+	case 2:
+		phytium_writew(fts, offset, val);
+		break;
+	case 4:
+	default:
+		phytium_writel(fts, offset, val);
+		break;
+	}
+}
+static inline void spi_enable_chip(struct phytium_spi *fts, int enable)
+{
+	phytium_writel(fts, SSIENR, (enable ? 1 : 0));
+}
+static inline void spi_set_clk(struct phytium_spi *fts, u16 div)
+{
+	phytium_writel(fts, BAUDR, div);
+}
+static inline void spi_mask_intr(struct phytium_spi *fts, u32 mask)
+{
+	u32 new_mask;
+
+	new_mask = phytium_readl(fts, IMR) & ~mask;
+	phytium_writel(fts, IMR, new_mask);
+}
+static inline void spi_umask_intr(struct phytium_spi *fts, u32 mask)
+{
+	u32 new_mask;
+
+	new_mask = phytium_readl(fts, IMR) | mask;
+	phytium_writel(fts, IMR, new_mask);
+}
+static inline void spi_global_cs(struct phytium_spi *fts)
+{
+	u32 global_cs_en, mask, setmask;
+
+	mask = GENMASK(fts->num_cs-1, 0) << fts->num_cs;
+	setmask = ~GENMASK(fts->num_cs-1, 0);
+	global_cs_en =	(phytium_readl(fts, GCSR) | mask) & setmask;
+	phytium_writel(fts, GCSR, global_cs_en);
+}
+static inline void spi_reset_chip(struct phytium_spi *fts)
+{
+	spi_enable_chip(fts, 0);
+	if (fts->global_cs)
+		spi_global_cs(fts);
+	spi_mask_intr(fts, 0xff);
+	spi_enable_chip(fts, 1);
+}
+static inline void spi_shutdown_chip(struct phytium_spi *fts)
+{
+	spi_enable_chip(fts, 0);
+	spi_set_clk(fts, 0);
+	fts->current_freq = 0;
+}
 extern int phytium_spi_add_host(struct device *dev, struct phytium_spi *fts);
 extern void phytium_spi_remove_host(struct phytium_spi *fts);
 extern int phytium_spi_suspend_host(struct phytium_spi *fts);
 extern int phytium_spi_resume_host(struct phytium_spi *fts);
-
+extern void phytium_spi_dmaops_set(struct phytium_spi *fts);
+extern int phytium_spi_check_status(struct phytium_spi *fts, bool raw);
 #endif /* PHYTIUM_SPI_HEADER_H */

commit 8529907d96ee0da4903c3eabee4a42f3312e3635

spi: phytium-dma: Do not leave error pointers in the DMA channels
When dma_request_chan() fails, phytium_spi_dma_init() returns with
the ERR_PTR still stored in fts->rxchan or fts->txchan (only the
tx-failure path cleared rxchan). DMA init failure is non-fatal - the
host keeps running in PIO mode - so phytium_spi_dma_exit() can later
hit the stale pointer and crash in dmaengine_terminate_sync() or
dma_release_channel().

Clear both channel pointers on every failure path.

Fixes: 3674931621b2 ("arm64: spi: Phytium: Adapt SPI driver to use DDMA interface")
Signed-off-by: Wentao Guan <guanwentao@uniontech.com>

drivers/spi/spi-phytium-dma.c
@@ -71,8 +71,10 @@ static int phytium_spi_dma_init(struct device *dev,
 		fts->rxchan = acpi_dma_request_slave_chan_by_index(dev, 0);
 	else
 		fts->rxchan = dma_request_chan(dev, "rx");
-	if (IS_ERR_OR_NULL(fts->rxchan))
+	if (IS_ERR_OR_NULL(fts->rxchan)) {
+		fts->rxchan = NULL;
 		return -ENODEV;
+	}
 
 	if (has_acpi_companion(dev))
 		fts->txchan = acpi_dma_request_slave_chan_by_index(dev, 1);
@@ -82,6 +84,7 @@ static int phytium_spi_dma_init(struct device *dev,
 		dev_err(dev, "can't request chan\n");
 		dma_release_channel(fts->rxchan);
 		fts->rxchan = NULL;
+		fts->txchan = NULL;
 		return -ENODEV;
 	}
 