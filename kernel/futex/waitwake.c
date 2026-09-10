7e8842f0feb499bfea7e7ca8990a96e459413a4b
Peter Zijlstra <peterz@infradead.org>
2026-09-10 10:06:21 -0400

    futex: Optimize futex hash bucket access patterns
    [ Upstream commit a734d9fca84e1d4fa0cb442ef5f84c88f8212d32 ]
    
    Breno reported significant c2c HITM in a futex hash heavy workload.
    
    It turns out that the hash bucket to private hash table reverse pointer
    (futex_hash_bucket::priv) was to blame. Notably when the hash buckets are
    heavily contended, the: 'fph = bh->priv;' load in futex_hash() will typically
    miss and consequently become quite expensive.
    
    Since this load in particular is quite superfluous, removing it is fairly
    straight forward. However, removing it does not in fact achieve anything much.
    The pain moves to the next user, notably: futex_hash_put().
    
    Therefore rework the whole private hash refcounting to avoid needing this back
    pointer (and removing it). Instead of passing around 'struct futex_hash_bucket
    *hb', pass around a new structure that contains it and the related 'struct
    futex_private_hash *fph' pointer in tandem.
    
    Funnily this turns out to remove more code than it adds and significantly
    improves futex hash performance (as measured by 'perf bench futex hash'):
    
    SKL dual socket 112 threads:
    
    		  Baseline        Patched
      shared (16k)    1571857         1641435         + 4.4%
      autosize (512)   646390          903371         +39.7%
      -b 256           464395          587014         +26.4%
      -b 512           715687          995943         +39.2%
      -b 1024          995085         1396328         +40.3%
      -b 2048         1293114         1668395         +29.0%
      -b 4096         2124438         2240228         + 5.5%
    
    Zen3 dual socket 256 threads:
    
                      Baseline        Patched
      shared  (16k)   1275840         1381279         + 8.2%
      autosize (512)  1252745         1482179         +18.3%
      -b 256           856274          955455         +11.5%
      -b 512          1267490         1544010         +21.8%
      -b 1024         1424013         1625424         +14.1%
      -b 2048         1505181         1669342         +10.9%
      -b 4096         1465993         1688932         +15.2%
    
    AMD EPYC 9D64 (Zen4, single socket) 176 threads:
    
                           Baseline       Patched      Delta
      shared (16k)         1,230,599      1,368,655    +11.2%
      autosize (1024)      1,285,440      1,556,946    +21.1%
      -b 256               1,341,471      1,520,303    +13.3%
      -b 512               1,438,330      1,599,319    +11.2%
      -b 1024              1,443,772      1,622,493    +12.4%
      -b 2048              1,472,108      1,643,975    +11.7%
      -b 4096              1,333,098      1,570,897    +17.8%
    
    Reported-by: Breno Leitao <leitao@debian.org>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Tested-by: Breno Leitao <leitao@debian.org>
    Tested-by: Thomas Gleixner <tglx@kernel.org>
    Link: https://patch.msgid.link/20260610135510.GB1430057@noisy.programming.kicks-ass.net
    
    [ Stable adaptation: use mm->futex_phash in __futex_hash(), since this
      branch does not contain the mm_struct futex field consolidation.
      Preserve the final-put use-after-free fix by saving fph->mm before
      futex_ref_put(), and restore the NULL guard needed by the direct
      private-hash put callers. futex_key_is_private() is already in futex.h;
      no new functions are introduced. Keep the bucket-reference conversion
      so 912edebe8501 applies without changes. ]
    
    Stable-dep-of: 912edebe8501 ("futex: Provide rt_mutex_.*_schedule() equivalents for futex scheduling")
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    (cherry picked from commit 884e8484c5fe827d299d49d0400423c3da94965d)
    Signed-off-by: Wentao Guan <guanwentao@uniontech.com>

--- kernel/futex/waitwake.c
+++ kernel/futex/waitwake.c
@@ -172,7 +172,8 @@ int futex_wake(u32 __user *uaddr, unsigned int flags, int nr_wake, u32 bitset)
 	if ((flags & FLAGS_STRICT) && !nr_wake)
 		return 0;
 
-	CLASS(hb, hb)(&key);
+	CLASS(hbr, hbr)(&key);
+	auto hb = hbr.hb;
 
 	/* Make sure we really have tasks to wakeup */
 	if (!futex_hb_waiters_pending(hb))
@@ -269,8 +270,10 @@ retry:
 
 retry_private:
 	if (1) {
-		CLASS(hb, hb1)(&key1);
-		CLASS(hb, hb2)(&key2);
+		CLASS(hbr, hbr1)(&key1);
+		CLASS(hbr, hbr2)(&key2);
+		auto hb1 = hbr1.hb;
+		auto hb2 = hbr2.hb;
 
 		double_lock_hb(hb1, hb2);
 		op_ret = futex_atomic_op_inuser(op, uaddr2);
@@ -420,7 +423,7 @@ int futex_wait_multiple_setup(struct futex_vector *vs, int count, int *woken)
 	 * Make sure to have a reference on the private_hash such that we
 	 * don't block on rehash after changing the task state below.
 	 */
-	guard(private_hash)();
+	guard(private_hash)(current->mm);
 
 	/*
 	 * Enqueuing multiple futexes is tricky, because we need to enqueue
@@ -457,7 +460,8 @@ retry:
 		u32 val = vs[i].w.val;
 
 		if (1) {
-			CLASS(hb, hb)(&q->key);
+			CLASS(hbr, hbr)(&q->key);
+			auto hb = hbr.hb;
 
 			futex_q_lock(q, hb);
 			ret = futex_get_value_locked(&uval, uaddr);
@@ -631,7 +635,8 @@ retry:
 
 retry_private:
 	if (1) {
-		CLASS(hb, hb)(&q->key);
+		CLASS(hbr, hbr)(&q->key);
+		auto hb = hbr.hb;
 
 		futex_q_lock(q, hb);
 
